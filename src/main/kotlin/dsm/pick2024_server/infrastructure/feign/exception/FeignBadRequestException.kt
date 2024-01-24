39e08cce8a5215fc686074527657bc861fd44609
도경 <dokyungkim2926@gmail.com>
2024-01-25 04:18:26 +0900

    refactor :: ktlint

--- src/main/kotlin/dsm/pick2024_server/infrastructure/feign/exception/FeignBadRequestException.kt
+++ src/main/kotlin/dsm/pick2024_server/infrastructure/feign/exception/FeignBadRequestException.kt
@@ -3,6 +3,6 @@ package dsm.pick2024_server.infrastructure.feign.exception
 import dsm.pick2024_server.global.error.exception.ErrorCode
 import dsm.pick2024_server.global.error.exception.PickException
 
-object FeignBadRequestException: PickException (
+object FeignBadRequestException : PickException(
     ErrorCode.FEIGN_BAD_REQUEST
 )
--- src/main/kotlin/dsm/pick2024_server/infrastructure/feign/exception/FeignForbiddenException.kt
+++ src/main/kotlin/dsm/pick2024_server/infrastructure/feign/exception/FeignForbiddenException.kt
@@ -6,4 +6,3 @@ import dsm.pick2024_server.global.error.exception.PickException
 object FeignForbiddenException : PickException(
     ErrorCode.FEIGN_FORBIDDEN
 )
-
