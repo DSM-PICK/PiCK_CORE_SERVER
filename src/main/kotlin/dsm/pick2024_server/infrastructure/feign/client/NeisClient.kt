7ec11c64e531a1824b3126de3a14002dbde7de1a
도경 <dokyungkim2926@gmail.com>
2024-01-25 04:18:43 +0900

    add :: feingclient schoolMeal

---
+++ src/main/kotlin/dsm/pick2024_server/infrastructure/feign/client/NeisClient.kt
@@ -0,0 +1,20 @@
+package dsm.pick2024_server.infrastructure.feign.client
+
+import dsm.pick2024_server.infrastructure.feign.client.property.NeisFeignProperty
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@FeignClient(name = "neis-feign-client", url = "https://open.neis.go.kr/hub")
+interface NeisFeignClient {
+
+    @GetMapping("/mealServiceDietInfo")
+    fun getMealServiceDietInfo(
+        @RequestParam(name = NeisFeignProperty.KEY) key: String,
+        @RequestParam(name = NeisFeignProperty.TYPE) type: String,
+        @RequestParam(name = NeisFeignProperty.PAGE_INDEX) pageIndex: Int,
+        @RequestParam(name = NeisFeignProperty.PAGE_SIZE) pageSize: Int,
+        @RequestParam(name = NeisFeignProperty.SD_SCHUL_CODE)schoolCode: String,
+        @RequestParam(name = NeisFeignProperty.ATPT_OFCDC_SC_CODE)atptCode: String
+    ): String
+}
---
+++ src/main/kotlin/dsm/pick2024_server/infrastructure/feign/client/property/NeisFeignProperty.kt
@@ -0,0 +1,10 @@
+package dsm.pick2024_server.infrastructure.feign.client.property
+
+object NeisFeignProperty {
+    const val KEY = "KEY"
+    const val TYPE = "Type"
+    const val PAGE_INDEX = "pIndex"
+    const val PAGE_SIZE = "pSize"
+    const val SD_SCHUL_CODE = "SD_SCHUL_CODE"
+    const val ATPT_OFCDC_SC_CODE = "ATPT_OFCDC_SC_CODE"
+}
