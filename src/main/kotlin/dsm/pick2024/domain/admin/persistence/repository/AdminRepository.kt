00734464eaa52723e50f7d49eab295e057ea071e
도경 <dokyungkim2926@gmail.com>
2024-01-26 01:34:01 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/domain/admin/persistence/repository/AdminRepository.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/persistence/repository/AdminRepository.kt
@@ -4,5 +4,4 @@ import dsm.pick2024.domain.admin.entity.AdminJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface AdminRepository : JpaRepository<AdminJpaEntity, UUID> {
-}
+interface AdminRepository : JpaRepository<AdminJpaEntity, UUID>
