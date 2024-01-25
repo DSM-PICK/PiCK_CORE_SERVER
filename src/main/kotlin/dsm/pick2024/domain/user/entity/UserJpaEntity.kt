4d3b7491a425291fccdf2d2b2b483ffe93b0706d
도경 <dokyungkim2926@gmail.com>
2024-01-26 00:21:15 +0900

    add :: entity

--- src/main/kotlin/dsm/pick2024/domain/user/entity/UserJpaEntity.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/entity/UserJpaEntity.kt
@@ -1,12 +1,32 @@
 package dsm.pick2024.domain.user.entity
 
+import dsm.pick2024.domain.user.entity.enums.Role
 import dsm.pick2024.global.base.BaseUUIDEntity
 import java.util.UUID
+import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 
 @Entity(name = "tbl_user")
 class UserJpaEntity(
+
     id: UUID?,
 
-    val name: String
-) : dsm.pick2024.global.base.BaseUUIDEntity(id)
+    @Column(name = "name", columnDefinition = "CHAR(4)", nullable = false, unique = true)
+    val name: String,
+
+    @Column(name = "grade", nullable = false)
+    val grade: Int,
+
+    @Column(name = "class_num", nullable = false)
+    val classNum: Int,
+
+    @Column(name = "num", nullable = false)
+    val num: Int,
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    val role: Role
+
+) : BaseUUIDEntity(id)
