c1fb1b5bf8b768a363331609d07b73f80a10fcb9
도경 <dokyungkim2926@gmail.com>
2024-01-26 01:40:13 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/domain/meal/persistence/repository/MealRepository.kt
+++ src/main/kotlin/dsm/pick2024/domain/meal/persistence/repository/MealRepository.kt
@@ -4,5 +4,4 @@ import dsm.pick2024.domain.meal.entity.MealJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface MealRepository : JpaRepository<MealJpaEntity, UUID> {
-}
+interface MealRepository : JpaRepository<MealJpaEntity, UUID>
