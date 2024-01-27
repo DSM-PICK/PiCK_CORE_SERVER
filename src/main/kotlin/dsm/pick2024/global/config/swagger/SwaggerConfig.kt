056fd846f812ce7195d9c8f7cd404acc021fc688
도경 <dokyungkim2926@gmail.com>
2024-01-28 02:18:55 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/global/config/swagger/SwaggerConfig.kt
+++ src/main/kotlin/dsm/pick2024/global/config/swagger/SwaggerConfig.kt
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-
 @Configuration
 class SwaggerConfig {
     @Bean
