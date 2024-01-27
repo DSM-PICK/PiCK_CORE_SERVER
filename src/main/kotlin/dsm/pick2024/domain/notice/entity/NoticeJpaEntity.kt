9f018938abbf881200c15ff014800d44af9d4277
도경 <dokyungkim2926@gmail.com>
2024-01-28 01:56:42 +0900

    add :: noticeJpaEntity

---
+++ src/main/kotlin/dsm/pick2024/domain/notice/entity/NoticeJpaEntity.kt
@@ -0,0 +1,22 @@
+package dsm.pick2024.domain.notice.entity
+
+import dsm.pick2024.domain.admin.entity.AdminJpaEntity
+import dsm.pick2024.global.base.BaseUUIDEntity
+import java.util.UUID
+import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+
+@Entity(name = "tbl_notice")
+class NoticeJpaEntity (
+    id: UUID,
+
+    val title: String,
+
+    val content: String,
+
+    @ManyToOne
+    @JoinColumn(name = "admin")
+    val admin: AdminJpaEntity
+
+): BaseUUIDEntity(id)
