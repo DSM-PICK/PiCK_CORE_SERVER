a1af2e8a2e9800f48a54d2f1fed3d42ed4c39a2d
도경 <dokyungkim2926@gmail.com>
2024-01-31 00:40:55 +0900

    add :: admin controller

---
+++ src/main/kotlin/dsm/pick2024/domain/admin/presentation/AdminController.kt
@@ -0,0 +1,22 @@
+package dsm.pick2024.domain.admin.presentation
+
+import dsm.pick2024.domain.admin.port.`in`.AdminLoginUseCase
+import dsm.pick2024.domain.admin.presentation.dto.request.AdminLoginRequest
+import dsm.pick2024.global.security.jwt.dto.TokenResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Tag(name = "admin API")
+@RestController
+@RequestMapping("/admin")
+class AdminController (
+    private val adminLoginUseCase: AdminLoginUseCase
+) {
+    @Operation(summary = "어드민 로그인 API")
+    @PostMapping
+    fun login(adminLoginRequest: AdminLoginRequest): TokenResponse =
+        adminLoginUseCase.adminLogin(adminLoginRequest)
+}
