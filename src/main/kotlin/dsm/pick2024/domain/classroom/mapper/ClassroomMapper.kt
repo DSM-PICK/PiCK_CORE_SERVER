bebf133daf7645d7398d266eaaacfe101ec3d241
도경 <dokyungkim2926@gmail.com>
2024-02-04 02:49:25 +0900

    add :: classroom mapper

---
+++ src/main/kotlin/dsm/pick2024/domain/classroom/mapper/ClassroomMapper.kt
@@ -0,0 +1,27 @@
+package dsm.pick2024.domain.classroom.mapper
+
+import dsm.pick2024.domain.classroom.domain.Classroom
+import dsm.pick2024.domain.classroom.entity.ClassroomJpaEntity
+import dsm.pick2024.global.base.GenericMapper
+import org.springframework.stereotype.Component
+
+@Component
+class ClassroomMapper : GenericMapper<ClassroomJpaEntity, Classroom> {
+
+    override fun toEntity(domain: Classroom) = domain.run {
+        ClassroomJpaEntity(
+            id = id,
+            classroomName = classroomName,
+            floor = floor
+        )
+    }
+
+
+    override fun toDomain(entity: ClassroomJpaEntity?) = entity?.run {
+        Classroom(
+            id = id,
+            classroomName = classroomName,
+            floor = floor
+        )
+    }
+}
