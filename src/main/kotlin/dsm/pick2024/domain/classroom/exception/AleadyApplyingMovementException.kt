8616c461e3998f086f48a8b25ad9f6cb19a6a599
도경 <dokyungkim2926@gmail.com>
2024-02-05 04:13:04 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/domain/classroom/exception/AleadyApplyingMovementException.kt
+++ src/main/kotlin/dsm/pick2024/domain/classroom/exception/AleadyApplyingMovementException.kt
@@ -3,6 +3,6 @@ package dsm.pick2024.domain.classroom.exception
 import dsm.pick2024.global.error.exception.ErrorCode
 import dsm.pick2024.global.error.exception.PickException
 
-object AleadyApplyingMovementException : PickException (
+object AleadyApplyingMovementException : PickException(
     ErrorCode.ALREADY_APPLYING_MOVEMENT
 )
--- src/main/kotlin/dsm/pick2024/domain/classroom/service/UserMoveClassroomService.kt
+++ src/main/kotlin/dsm/pick2024/domain/classroom/service/UserMoveClassroomService.kt
@@ -5,7 +5,6 @@ import dsm.pick2024.domain.classroom.exception.AleadyApplyingMovementException
 import dsm.pick2024.domain.classroom.port.`in`.UserMoveClassroomUseCase
 import dsm.pick2024.domain.classroom.port.out.ClassroomSavePort
 import dsm.pick2024.domain.classroom.port.out.ExistsByUsernamePort
-import dsm.pick2024.domain.classroom.port.out.FindByUsernamePort
 import dsm.pick2024.domain.classroom.presentation.dto.request.UserMoveClassroomRequest
 import dsm.pick2024.domain.user.port.`in`.UserFacadeUseCase
 import org.springframework.stereotype.Service
@@ -29,7 +28,7 @@ class UserMoveClassroomService(
         val classroom = Classroom(
             username = user,
             classroomName = request.classroomName,
-            floor = request.floor,
+            floor = request.floor
         )
         classroomSavePort.save(classroom)
     }
