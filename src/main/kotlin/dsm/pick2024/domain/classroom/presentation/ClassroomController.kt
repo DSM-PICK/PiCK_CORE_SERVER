604e75c5824c06eb276faf8639138ef414720f77
도경 <dokyungkim2926@gmail.com>
2024-02-04 21:48:09 +0900

    refactor :: logic

--- src/main/kotlin/dsm/pick2024/domain/classroom/presentation/ClassroomController.kt
+++ src/main/kotlin/dsm/pick2024/domain/classroom/presentation/ClassroomController.kt
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -20,6 +21,6 @@ class ClassroomController(
     @Operation(summary = "교실이동 API")
     @PostMapping("/move")
     @ResponseStatus(value = HttpStatus.OK)
-    fun moveClassroom(userMoveClassroomRequest: UserMoveClassroomRequest) =
+    fun moveClassroom(@RequestBody userMoveClassroomRequest: UserMoveClassroomRequest) =
         userMoveClassroomUseCase.moveClassroom(userMoveClassroomRequest)
 }
