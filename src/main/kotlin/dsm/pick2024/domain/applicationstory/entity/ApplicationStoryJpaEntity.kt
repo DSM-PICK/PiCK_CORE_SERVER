6d01f1efa016b0153bfb0d01c39921dea0045580
도경 <dokyungkim2926@gmail.com>
2024-02-09 00:52:09 +0900

    add :: entity

---
+++ src/main/kotlin/dsm/pick2024/domain/applicationstory/entity/ApplicationStoryJpaEntity.kt
@@ -0,0 +1,26 @@
+package dsm.pick2024.domain.applicationstory.entity
+
+import dsm.pick2024.global.base.BaseUUIDEntity
+import java.time.LocalTime
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+
+@Entity
+class ApplicationStoryJpaEntity (
+
+    id: UUID?,
+
+    @Column(nullable = false)
+    val reason: String,
+
+    @Column(name = "start_time", nullable = false)
+    val startTime: LocalTime,
+
+    @Column(name = "end_time")
+    val endTime: LocalTime ?= null,
+
+    @Column(nullable = false)
+    val username: String,
+
+): BaseUUIDEntity(id)
