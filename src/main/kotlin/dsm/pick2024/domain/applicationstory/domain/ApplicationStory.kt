97b0cafa6f17c3c131f1ed6af030933a106916a4
도경 <dokyungkim2926@gmail.com>
2024-02-09 00:52:17 +0900

    add :: domain

---
+++ src/main/kotlin/dsm/pick2024/domain/applicationstory/domain/ApplicationStory.kt
@@ -0,0 +1,12 @@
+package dsm.pick2024.domain.applicationstory.domain
+
+import java.time.LocalTime
+import java.util.*
+
+data class ApplicationStory (
+    val id: UUID? = null,
+    val reason: String,
+    val startTime: LocalTime,
+    val endTime: LocalTime? = null,
+    val username: String,
+)
