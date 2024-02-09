1c911676f0077b8c421646eaff2e5208a3a29300
soohyeon <meltapplee@gmail.com>
2024-02-09 14:08:02 +0900

    add :: mapper

---
+++ src/main/kotlin/dsm/pick2024/domain/application/mapper/WeekendMealMapper.kt
@@ -0,0 +1,28 @@
+package dsm.pick2024.domain.application.mapper
+
+import dsm.pick2024.domain.application.domain.WeekendMeal
+import dsm.pick2024.domain.application.entity.WeekendMealJpaEntity
+import org.springframework.stereotype.Component
+
+@Component
+class WeekendMealMapper {
+    fun toEntity(domain: WeekendMeal) =
+        domain.run {
+            WeekendMealJpaEntity(
+                id = id,
+                username = username,
+                status = status,
+                isCheck = isCheck,
+            )
+        }
+
+    fun toDomain(entity: WeekendMealJpaEntity) =
+        entity.run {
+            WeekendMeal(
+                id = id,
+                username = username,
+                status = status,
+                isCheck = isCheck,
+            )
+        }
+}
