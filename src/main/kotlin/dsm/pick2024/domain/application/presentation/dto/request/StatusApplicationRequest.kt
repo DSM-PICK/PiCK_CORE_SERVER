6e53df8caa9dfa545c310c14ed41a1b1f953fe82
도경 <dokyungkim2926@gmail.com>
2024-02-10 02:11:47 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/domain/application/presentation/dto/request/StatusApplicationRequest.kt
+++ src/main/kotlin/dsm/pick2024/domain/application/presentation/dto/request/StatusApplicationRequest.kt
@@ -1,9 +1,7 @@
 package dsm.pick2024.domain.application.presentation.dto.request
 
-import dsm.pick2024.domain.application.enums.Status
 import java.util.UUID
 
 data class StatusApplicationRequest(
-    val applicationIds: List<UUID>,
-    val status: Status
+    val applicationIds: List<UUID>
 )
