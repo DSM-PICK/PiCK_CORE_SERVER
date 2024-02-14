cb84af36adf113a702ffe4826e818f2052dd61d8
도경 <dokyungkim2926@gmail.com>
2024-02-14 22:47:20 +0900

    add :: user refresh token api

--- src/main/kotlin/dsm/pick2024/domain/user/presentation/UserController.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/presentation/UserController.kt
@@ -3,12 +3,14 @@ package dsm.pick2024.domain.user.presentation
 import dsm.pick2024.domain.user.port.`in`.LoginUseCase
 import dsm.pick2024.domain.user.port.`in`.QueryUserDetailsInfoUseCase
 import dsm.pick2024.domain.user.port.`in`.QueryUserSimpleInfoUseCase
+import dsm.pick2024.domain.user.port.`in`.UserTokenRefreshUseCase
 import dsm.pick2024.domain.user.presentation.dto.request.UserLoginRequest
 import dsm.pick2024.global.security.jwt.dto.TokenResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/user")
 class UserController(
     private val loginUseCase: LoginUseCase,
+    private val userTokenRefreshUseCase: UserTokenRefreshUseCase,
     private val queryUserSimpleInfoUseCase: QueryUserSimpleInfoUseCase,
     private val queryUserDetailsInfoUseCase: QueryUserDetailsInfoUseCase
 ) {
@@ -26,6 +29,12 @@ class UserController(
     @PostMapping("/login")
     fun login(@RequestBody userLoginRequest: UserLoginRequest): TokenResponse =
         loginUseCase.login(userLoginRequest)
+
+
+    @Operation(summary = "유저 토큰 재발급 API")
+    @PutMapping("/refresh")
+    fun userTokenRefresh(token: String): TokenResponse =
+        userTokenRefreshUseCase.userTokenRefresh(token)
 
     @Operation(summary = "내 정보 간편조회 API")
     @GetMapping("/simple")
