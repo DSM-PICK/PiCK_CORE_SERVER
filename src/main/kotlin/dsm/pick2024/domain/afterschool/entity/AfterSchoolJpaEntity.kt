e3ec395d866f6502cb2a4235eb5b1b185c3c0acb
도경 <dokyungkim2926@gmail.com>
2024-02-16 00:11:11 +0900

    add :: entity

---
+++ src/main/kotlin/dsm/pick2024/domain/afterschool/entity/AfterSchoolJpaEntity.kt
@@ -0,0 +1,24 @@
+package dsm.pick2024.domain.afterschool.entity
+
+import dsm.pick2024.global.base.BaseUUIDEntity
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+
+@Entity(name = "tbl_after_school")
+class AfterSchoolJpaEntity(
+
+    id: UUID?,
+
+    @Column(nullable = false)
+    val name: String,
+
+    @Column(nullable = false)
+    val grade: Int,
+
+    @Column(name = "class_num", nullable = false)
+    val classNum: Int,
+
+    @Column(nullable = false)
+    val num: Int,
+): BaseUUIDEntity(id)
