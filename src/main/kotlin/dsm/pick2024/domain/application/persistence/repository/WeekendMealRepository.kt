3d9bef6ff29dc9b8e61a13c39c62532dace4bd9d
도경 <dokyungkim2926@gmail.com>
2024-02-25 00:24:39 +0900

    error

--- src/main/kotlin/dsm/pick2024/domain/application/persistence/repository/WeekendMealRepository.kt
+++ src/main/kotlin/dsm/pick2024/domain/application/persistence/repository/WeekendMealRepository.kt
@@ -1,11 +1,10 @@
 package dsm.pick2024.domain.application.persistence.repository
 
-import dsm.pick2024.domain.application.domain.WeekendMeal
 import dsm.pick2024.domain.application.entity.WeekendMealJpaEntity
 import org.springframework.data.repository.Repository
 import java.util.UUID
 
-interface WeekendMealRepository : Repository<WeekendMeal, UUID> {
+interface WeekendMealRepository : Repository<WeekendMealJpaEntity, UUID> {
     fun save(entity: WeekendMealJpaEntity)
 
     fun findByUserId(id: UUID): WeekendMealJpaEntity
