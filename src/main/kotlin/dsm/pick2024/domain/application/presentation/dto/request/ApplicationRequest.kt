b907abf79517a4efb9ddb1641c963d236ec15382
도경 <dokyungkim2926@gmail.com>
2024-02-27 02:53:49 +0900

    .

--- src/main/kotlin/dsm/pick2024/domain/application/presentation/dto/request/ApplicationRequest.kt
+++ src/main/kotlin/dsm/pick2024/domain/application/presentation/dto/request/ApplicationRequest.kt
@@ -1,9 +1,12 @@
 package dsm.pick2024.domain.application.presentation.dto.request
 
+import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalTime
 
 data class ApplicationRequest(
     val reason: String,
+    @DateTimeFormat(pattern = "MM-dd")
     val startTime: LocalTime,
+    @DateTimeFormat(pattern = "MM-dd")
     val endTime: LocalTime
 )
