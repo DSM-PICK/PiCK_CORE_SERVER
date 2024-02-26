2b9e0e96005dfed5578052ff19a9700e6d575dd3
도경 <dokyungkim2926@gmail.com>
2024-02-27 01:30:25 +0900

    modify :: logic

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/port/in/CreateWeekendMealUseCase.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/port/in/CreateWeekendMealUseCase.kt
@@ -1,6 +1,7 @@
 package dsm.pick2024.domain.weekendmeal.port.`in`
 
-import dsm.pick2024.domain.application.enums.Status
+import dsm.pick2024.domain.weekendmeal.enums.Status
+
 
 interface CreateWeekendMealUseCase {
     fun changeWeekendMeal(status: Status)
