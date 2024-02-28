c50ecb28d8ce17b96507c1efb9ddb34d22aa5eb8
도경 <dokyungkim2926@gmail.com>
2024-02-28 23:25:53 +0900

    modify : userQuery

--- src/main/kotlin/dsm/pick2024/domain/user/presentation/dto/response/QueryUserDetailsInfoResponse.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/presentation/dto/response/QueryUserDetailsInfoResponse.kt
@@ -3,7 +3,7 @@ package dsm.pick2024.domain.user.presentation.dto.response
 import java.time.LocalDate
 
 data class QueryUserDetailsInfoResponse(
-    val profile: String,
+    val profile: String ?= null,
     val name: String,
     val birthDay: LocalDate,
     val grade: Int,
