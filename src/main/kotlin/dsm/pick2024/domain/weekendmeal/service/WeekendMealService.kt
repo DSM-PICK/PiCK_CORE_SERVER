d53a3e609cde8105c26230b436034c3f08046322
도경 <dokyungkim2926@gmail.com>
2024-03-04 08:40:50 +0900

    update :: changeWeekendMeal logic

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/service/WeekendMealService.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/service/WeekendMealService.kt
@@ -1,7 +1,7 @@
 package dsm.pick2024.domain.weekendmeal.service
 
-import dsm.pick2024.domain.weekendmeal.exception.WeekendMealNotFoundException
 import dsm.pick2024.domain.user.port.`in`.UserFacadeUseCase
+import dsm.pick2024.domain.weekendmeal.domain.WeekendMeal
 import dsm.pick2024.domain.weekendmeal.enums.Status
 import dsm.pick2024.domain.weekendmeal.port.`in`.CreateWeekendMealUseCase
 import dsm.pick2024.domain.weekendmeal.port.out.FindWeekendMealByUserIdPort
@@ -18,15 +18,20 @@ class WeekendMealService(
     @Transactional
     override fun changeWeekendMeal(status: Status) {
         val user = userFacadeUseCase.currentUser()
-
         val weekendMeal = findWeekendMealByUserIdPort.findByUserId(user.id!!)
-            ?: throw WeekendMealNotFoundException
 
-        val update =
-            weekendMeal.copy(
+        if (weekendMeal != null) {
+            val updatedWeekendMeal = weekendMeal.copy(status = status)
+            weekendMealPort.save(updatedWeekendMeal)
+        } else {
+            val newWeekendMeal = WeekendMeal(
+                userId = user.id,
+                grade = user.grade,
+                classNum = user.classNum,
+                username = user.name,
                 status = status
             )
-
-        weekendMealPort.save(update)
+            weekendMealPort.save(newWeekendMeal)
+        }
     }
 }
