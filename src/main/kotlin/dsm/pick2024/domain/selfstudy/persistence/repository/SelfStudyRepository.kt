93ebb635ee543417f5d6aba6b59c49fa9fad8c47
도경 <dokyungkim2926@gmail.com>
2024-03-06 01:19:31 +0900

    modify :: save

--- src/main/kotlin/dsm/pick2024/domain/selfstudy/persistence/repository/SelfStudyRepository.kt
+++ src/main/kotlin/dsm/pick2024/domain/selfstudy/persistence/repository/SelfStudyRepository.kt
@@ -2,10 +2,13 @@ package dsm.pick2024.domain.selfstudy.persistence.repository
 
 import dsm.pick2024.domain.selfstudy.entity.SelfStudyJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.Repository
 import java.time.LocalDate
 import java.util.UUID
 
-interface SelfStudyRepository : JpaRepository<SelfStudyJpaEntity, UUID> {
+interface SelfStudyRepository : Repository<SelfStudyJpaEntity, UUID> {
+
+    fun save(entity: SelfStudyJpaEntity): SelfStudyJpaEntity
 
     fun findByDateAndFloor(date: LocalDate, floor: Int): SelfStudyJpaEntity
 
