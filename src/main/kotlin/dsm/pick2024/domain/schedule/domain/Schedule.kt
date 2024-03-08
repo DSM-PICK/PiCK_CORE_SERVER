4cd9a21098f88ae9538f8bef4637d4b78aebfba4
soohyeon <meltapplee@gmail.com>
2024-03-08 14:45:21 +0900

    refactor :: delete blank

--- src/main/kotlin/dsm/pick2024/domain/schedule/domain/Schedule.kt
+++ src/main/kotlin/dsm/pick2024/domain/schedule/domain/Schedule.kt
@@ -3,7 +3,7 @@ package dsm.pick2024.domain.schedule.domain
 import java.time.LocalDate
 import java.util.*
 
-data class Schedule (
+data class Schedule(
     val id: UUID? = null,
     val eventName: String,
     val isGrade1Event: Boolean,
