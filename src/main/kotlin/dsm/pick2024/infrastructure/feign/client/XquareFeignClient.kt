0f49bdd88abe7ac812d721b2670877686b175368
도경 <dokyungkim2926@gmail.com>
2024-03-10 23:04:21 +0900

    add :: xquare feign

---
+++ src/main/kotlin/dsm/pick2024/infrastructure/feign/client/XquareFeignClient.kt
@@ -0,0 +1,20 @@
+package dsm.pick2024.infrastructure.feign.client
+
+import dsm.pick2024.infrastructure.feign.client.response.XquareResponse
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@FeignClient(name = "xquare-login", url = "\${url.dsmlogin}")
+interface XquareFeignClient {
+
+    @GetMapping("/user-data")
+    fun xquareUser(
+        @RequestParam("account_id")
+        accountId: String,
+        @RequestParam("password")
+        password: String
+    ): XquareResponse
+
+}
---
+++ src/main/kotlin/dsm/pick2024/infrastructure/feign/client/response/XquareResponse.kt
@@ -0,0 +1,14 @@
+package dsm.pick2024.infrastructure.feign.client.response
+
+import java.util.*
+
+data class XquareResponse (
+    val id: UUID,
+    val accountId: String,
+    val password: String,
+    val name: String,
+    val grade: Int?,
+    val classNum: Int?,
+    val num: Int?,
+    val userRole: String
+)
