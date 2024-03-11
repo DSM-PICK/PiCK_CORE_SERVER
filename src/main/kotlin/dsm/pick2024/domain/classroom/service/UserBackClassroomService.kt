7ae7948eca511eabd4d95a58544b3b262e2d1952
도경 <dokyungkim2926@gmail.com>
2024-03-12 01:35:29 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/domain/classroom/service/UserBackClassroomService.kt
+++ src/main/kotlin/dsm/pick2024/domain/classroom/service/UserBackClassroomService.kt
@@ -2,7 +2,6 @@ package dsm.pick2024.domain.classroom.service
 
 import dsm.pick2024.domain.classroom.port.`in`.UserBackClassroomUseCase
 import dsm.pick2024.domain.classroom.port.out.ClassroomDeletePort
-import dsm.pick2024.domain.classroom.port.out.FindByUserIdPort
 import dsm.pick2024.domain.user.port.`in`.UserFacadeUseCase
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,8 +9,7 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class UserBackClassroomService(
     private val classroomDeletePort: ClassroomDeletePort,
-    private val userFacadeUseCase: UserFacadeUseCase,
-    private val findByUserIdPort: FindByUserIdPort
+    private val userFacadeUseCase: UserFacadeUseCase
 ) : UserBackClassroomUseCase {
 
     @Transactional
