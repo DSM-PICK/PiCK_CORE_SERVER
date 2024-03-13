b5bd22ffa850a0b5f018ee7b49ef2ce1fea1fc10
도경 <dokyungkim2926@gmail.com>
2024-03-13 09:22:38 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/presentation/dto/response/QueryStatusResponse.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/presentation/dto/response/QueryStatusResponse.kt
@@ -3,5 +3,5 @@ package dsm.pick2024.domain.weekendmeal.presentation.dto.response
 import dsm.pick2024.domain.weekendmeal.enums.Status
 
 data class QueryStatusResponse(
-    val status: Status? = null
+    val status: Status
 )
