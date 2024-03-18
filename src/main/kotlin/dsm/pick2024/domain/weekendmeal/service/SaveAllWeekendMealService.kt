c84b0aebc3c0cda28d166f7490df57f49e89cc23
soohyeon <meltapplee@gmail.com>
2024-03-18 20:01:23 +0900

    modify :: saveAllWeekendMealService

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/service/SaveAllWeekendMealService.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/service/SaveAllWeekendMealService.kt
@@ -3,6 +3,7 @@ package dsm.pick2024.domain.weekendmeal.service
 import dsm.pick2024.domain.weekendmeal.entity.WeekendMealJpaEntity
 import dsm.pick2024.domain.weekendmeal.enums.Status
 import dsm.pick2024.domain.weekendmeal.persistence.repository.WeekendMealRepository
+import dsm.pick2024.domain.weekendmeal.port.`in`.SaveAllWeekendMealUseCase
 import dsm.pick2024.infrastructure.feign.client.XquareFeignClient
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,10 +12,10 @@ import org.springframework.transaction.annotation.Transactional
 class SaveAllWeekendMealService(
     private val xquareFeignClient: XquareFeignClient,
     private val weekendMealRepository: WeekendMealRepository
-) {
+) : SaveAllWeekendMealUseCase {
 
     @Transactional
-    fun saveAll(key: String) {
+    override fun saveAll(key: String) {
         val xquareWeekendMealUserInfo = xquareFeignClient.userAll(key)
         val weekendMealEntities = xquareWeekendMealUserInfo.map { user ->
             WeekendMealJpaEntity(
