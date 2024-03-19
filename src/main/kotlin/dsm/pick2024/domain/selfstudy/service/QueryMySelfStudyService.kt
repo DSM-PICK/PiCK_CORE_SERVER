251c5ded434455ed8f19e700a0f93f9e3d9c1df0
도경 <dokyungkim2926@gmail.com>
2024-03-19 17:20:01 +0900

    return

--- src/main/kotlin/dsm/pick2024/domain/selfstudy/service/QueryMySelfStudyService.kt
+++ src/main/kotlin/dsm/pick2024/domain/selfstudy/service/QueryMySelfStudyService.kt
@@ -20,7 +20,7 @@ class QueryMySelfStudyService(
         val floor = selfStudy?.floor
 
         return if (floor != null) {
-            "${admin.name} 선생님은 $floor 층 자습감독 입니다"
+            "${admin.name} 선생님은 ${floor}층 자습감독 입니다"
         } else {
             "오늘 ${admin.name} 선생님은 자습감독이 아닙니다."
         }
