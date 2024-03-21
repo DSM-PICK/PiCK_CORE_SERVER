b97e94581a06356f28b0b27a8b43e6c0befa86a9
soohyeon <meltapplee@gmail.com>
2024-03-21 19:24:31 +0900

    modify :: query

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/persistence/WeekendMealPersistenceAdapter.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/persistence/WeekendMealPersistenceAdapter.kt
@@ -1,7 +1,6 @@
 package dsm.pick2024.domain.weekendmeal.persistence
 
 import com.querydsl.jpa.impl.JPAQueryFactory
-import dsm.pick2024.domain.user.entity.QUserJpaEntity
 import dsm.pick2024.domain.weekendmeal.domain.WeekendMeal
 import dsm.pick2024.domain.weekendmeal.entity.QWeekendMealJpaEntity
 import dsm.pick2024.domain.weekendmeal.entity.WeekendMealJpaEntity
@@ -16,7 +15,7 @@ import java.util.UUID
 class WeekendMealPersistenceAdapter(
     private val weekendMealRepository: WeekendMealRepository,
     private val weekendMealMapper: WeekendMealMapper,
-    private val jpaQueryFactory: JPAQueryFactory
+    private val jpaQueryFactory: JPAQueryFactory,
 ) : WeekendMealPort {
     override fun save(weekendMeal: WeekendMeal) {
         weekendMealRepository.save(weekendMealMapper.toEntity(weekendMeal))
@@ -32,31 +31,27 @@ class WeekendMealPersistenceAdapter(
 
     override fun findByGradeAndClassNum(
         grade: Int,
-        classNum: Int
+        classNum: Int,
     ) = jpaQueryFactory
         .selectFrom(QWeekendMealJpaEntity.weekendMealJpaEntity)
-        .innerJoin(QUserJpaEntity.userJpaEntity)
-        .on(QWeekendMealJpaEntity.weekendMealJpaEntity.username.eq(QUserJpaEntity.userJpaEntity.name))
         .where(
             QWeekendMealJpaEntity.weekendMealJpaEntity.grade.eq(grade),
             QWeekendMealJpaEntity.weekendMealJpaEntity.classNum.eq(classNum),
             QWeekendMealJpaEntity.weekendMealJpaEntity.status.eq(Status.OK)
-                .or(QWeekendMealJpaEntity.weekendMealJpaEntity.status.eq(Status.NO))
+                .or(QWeekendMealJpaEntity.weekendMealJpaEntity.status.eq(Status.NO)),
         )
         .fetch()
         .map { weekendMealMapper.toDomain(it) }
 
     override fun findQuitByGradeAndClassNum(
         grade: Int,
-        classNum: Int
+        classNum: Int,
     ) = jpaQueryFactory
         .selectFrom(QWeekendMealJpaEntity.weekendMealJpaEntity)
-        .innerJoin(QUserJpaEntity.userJpaEntity)
-        .on(QWeekendMealJpaEntity.weekendMealJpaEntity.username.eq(QUserJpaEntity.userJpaEntity.name))
         .where(
             QWeekendMealJpaEntity.weekendMealJpaEntity.grade.eq(grade),
             QWeekendMealJpaEntity.weekendMealJpaEntity.classNum.eq(classNum),
-            QWeekendMealJpaEntity.weekendMealJpaEntity.status.eq(Status.QUIET)
+            QWeekendMealJpaEntity.weekendMealJpaEntity.status.eq(Status.QUIET),
         )
         .fetch()
         .map { weekendMealMapper.toDomain(it) }
