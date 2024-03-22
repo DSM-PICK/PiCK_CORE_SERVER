8732348fc488e0d3181db9a4cdeec8d348e0653f
soohyeon <meltapplee@gmail.com>
2024-03-22 19:34:44 +0900

    modify

--- src/main/kotlin/dsm/pick2024/domain/status/service/ChangeStatusService.kt
+++ src/main/kotlin/dsm/pick2024/domain/status/service/ChangeStatusService.kt
@@ -3,24 +3,24 @@ package dsm.pick2024.domain.status.service
 import dsm.pick2024.domain.status.exception.StatusNotFoundException
 import dsm.pick2024.domain.status.persistence.StatusPersistenceAdapter
 import dsm.pick2024.domain.status.port.`in`.ChangeStatusUseCase
-import dsm.pick2024.domain.status.present.dto.request.ChangeStatusRequest
+import dsm.pick2024.domain.status.present.dto.response.QueryClassResponse
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ChangeStatusService(
-    private val statusPersistenceAdapter: StatusPersistenceAdapter
+    private val statusPersistenceAdapter: StatusPersistenceAdapter,
 ) : ChangeStatusUseCase {
     @Transactional
-    override fun changeStatus(request: List<ChangeStatusRequest>) {
-        request.forEach { request ->
+    override fun changeStatus(request: QueryClassResponse) {
+        request.students.forEach { request ->
             val status =
-                statusPersistenceAdapter.findStatusByUserId(request.id)
+                statusPersistenceAdapter.findStatusByUserId(request.userId)
                     ?: throw StatusNotFoundException
 
             val update =
                 status?.copy(
-                    type = request.status
+                    type = request.status,
                 )
 
             update?.let { statusPersistenceAdapter.save(it) }
