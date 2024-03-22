7de6bf157f20da6d7ef89e3b186830cbbb6956d5
도경 <dokyungkim2926@gmail.com>
2024-03-22 14:47:04 +0900

    add :: Transactional

--- src/main/kotlin/dsm/pick2024/domain/status/service/QueryClassStatusService.kt
+++ src/main/kotlin/dsm/pick2024/domain/status/service/QueryClassStatusService.kt
@@ -4,12 +4,14 @@ import dsm.pick2024.domain.status.port.`in`.QueryClassStatusUseCase
 import dsm.pick2024.domain.status.port.out.QueryClassStatusPort
 import dsm.pick2024.domain.status.present.dto.response.QueryClassStatusResponse
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QueryClassStatusService(
     private val queryClassStatusPort: QueryClassStatusPort
 ) : QueryClassStatusUseCase {
 
+    @Transactional(readOnly = true)
     override fun queryClasStatus(grade: Int, classNum: Int) =
         queryClassStatusPort.findByGradeAndClassNum(grade, classNum)
             .map { it ->
