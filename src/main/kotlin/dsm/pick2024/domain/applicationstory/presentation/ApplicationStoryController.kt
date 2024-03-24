3198dce26806cddef3fb9c55c886b70a5ea18ed0
soohyeon <meltapplee@gmail.com>
2024-03-24 22:41:50 +0900

    add :: swegger

--- src/main/kotlin/dsm/pick2024/domain/applicationstory/presentation/ApplicationStoryController.kt
+++ src/main/kotlin/dsm/pick2024/domain/applicationstory/presentation/ApplicationStoryController.kt
@@ -16,17 +16,18 @@ import java.util.UUID
 @RequestMapping("/story")
 class ApplicationStoryController(
     private val queryUserApplicationUseCase: QueryUserApplicationUseCase,
-    private val queryApplicationStoryUseCase: QueryApplicationStoryUseCase
+    private val queryApplicationStoryUseCase: QueryApplicationStoryUseCase,
 ) {
     @Operation(summary = "외출기록 조회 기능")
     @GetMapping("/query/{userId}")
     fun queryUserApplicationStory(
-        @PathVariable(name = "userId") userId: UUID
+        @PathVariable(name = "userId") userId: UUID,
     ) = queryUserApplicationUseCase.queryUserApplicationStory(userId)
 
+    @Operation(summary = "외출기록 반별 학생 조회 기능")
     @GetMapping("/grade")
     fun queryClassApplication(
         @RequestParam(name = "grade") grade: Int,
-        @RequestParam(name = "class_num") classNum: Int
+        @RequestParam(name = "class_num") classNum: Int,
     ) = queryApplicationStoryUseCase.queryClassApplication(grade, classNum)
 }
