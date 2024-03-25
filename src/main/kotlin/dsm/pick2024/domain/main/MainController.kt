6a541914cc424f0a80bcc038f72d7429e637cfce
도경 <dokyungkim2926@gmail.com>
2024-03-25 22:10:45 +0900

    add :: swagger

--- src/main/kotlin/dsm/pick2024/domain/main/MainController.kt
+++ src/main/kotlin/dsm/pick2024/domain/main/MainController.kt
@@ -1,5 +1,6 @@
 package dsm.pick2024.domain.main
 
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 class MainController(
     private val mainService: MainService
 ) {
+    @Operation(summary = "main")
     @GetMapping
     fun main() = mainService.main()
 }
