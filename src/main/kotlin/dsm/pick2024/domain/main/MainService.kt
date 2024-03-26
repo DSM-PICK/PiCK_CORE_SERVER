a96ccd5e4a970cd2527224d3fca03d0d35e07173
도경 <dokyungkim2926@gmail.com>
2024-03-26 20:16:02 +0900

    add :: service

--- src/main/kotlin/dsm/pick2024/domain/main/MainService.kt
+++ src/main/kotlin/dsm/pick2024/domain/main/MainService.kt
@@ -1,11 +1,9 @@
 package dsm.pick2024.domain.main
 
-import dsm.pick2024.domain.application.exception.ApplicationNotFoundException
 import dsm.pick2024.domain.application.port.out.FindApplicationByNamePort
 import dsm.pick2024.domain.application.presentation.dto.response.QuerySimpleMyApplicationResponse
 import dsm.pick2024.domain.classroom.port.out.FindByUserIdPort
 import dsm.pick2024.domain.classroom.presentation.dto.response.UserMoveClassroomResponse
-import dsm.pick2024.domain.earlyreturn.exception.EarlyReturnApplicationNotFoundException
 import dsm.pick2024.domain.earlyreturn.port.out.FindEarlyReturnByUserIdPort
 import dsm.pick2024.domain.earlyreturn.presentation.dto.response.QuerySimpleMyEarlyResponse
 import dsm.pick2024.domain.user.port.`in`.UserFacadeUseCase
@@ -20,45 +18,45 @@ class MainService(
     private val findEarlyReturnByUserIdPort: FindEarlyReturnByUserIdPort,
     private val findByUserIdPort: FindByUserIdPort
 ) {
-    @Transactional(readOnly = true)
-    fun main(): Any {
-        val userId = userFacadeUseCase.currentUser().id ?: throw IllegalStateException("User ID not found")
 
-        return findApplication(userId)
-            ?: findEarlyReturn(userId)
-            ?: findClassroom(userId)
+    @Transactional(readOnly = true)
+    fun main(): Any? {
+        val userId = userFacadeUseCase.currentUser().id!!
+
+        return when {
+            findApplication(userId) != null -> findApplication(userId)
+            findEarlyReturn(userId) != null -> findEarlyReturn(userId)
+            else -> findClassroom(userId)
+        }
     }
 
     private fun findApplication(userId: UUID): QuerySimpleMyApplicationResponse? {
-        val application = findApplicationByNamePort.findByUserId(userId)
-            ?: throw ApplicationNotFoundException
-
-        return QuerySimpleMyApplicationResponse(
-            userId = application.userId,
-            startTime = application.startTime,
-            username = application.username,
-            endTime = application.endTime
-        )
+        return findApplicationByNamePort.findByUserId(userId)?.run {
+            QuerySimpleMyApplicationResponse(
+                userId = userId,
+                startTime = startTime,
+                username = username,
+                endTime = endTime
+            )
+        }
     }
 
     private fun findEarlyReturn(userId: UUID): QuerySimpleMyEarlyResponse? {
-        val earlyReturn = findEarlyReturnByUserIdPort.findByUserId(userId)
-            ?: throw EarlyReturnApplicationNotFoundException
-
-        return QuerySimpleMyEarlyResponse(
-            userId = earlyReturn.userId,
-            startTime = earlyReturn.startTime,
-            username = earlyReturn.username
-        )
+        return findEarlyReturnByUserIdPort.findByUserId(userId)?.run {
+            QuerySimpleMyEarlyResponse(
+                userId = userId,
+                startTime = startTime,
+                username = username
+            )
+        }
     }
 
-    private fun findClassroom(userId: UUID): UserMoveClassroomResponse {
-        val classroom = findByUserIdPort.findByUserId(userId)
-            ?: throw IllegalStateException("Classroom data not found")
-
-        return UserMoveClassroomResponse(
-            username = classroom.username,
-            classroom = classroom.classroomName
-        )
+    private fun findClassroom(userId: UUID): UserMoveClassroomResponse? {
+        return findByUserIdPort.findByUserId(userId)?.run {
+            UserMoveClassroomResponse(
+                username = username,
+                classroom = classroomName
+            )
+        }
     }
 }
