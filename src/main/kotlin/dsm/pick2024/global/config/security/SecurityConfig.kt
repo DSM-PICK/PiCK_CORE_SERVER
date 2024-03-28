c3e33bbe94b0298e25bda17d710da65a1aecf7e5
soohyeon <meltapplee@gmail.com>
2024-03-29 00:52:02 +0900

    modify :: securityConfig

--- src/main/kotlin/dsm/pick2024/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/dsm/pick2024/global/config/security/SecurityConfig.kt
@@ -36,15 +36,13 @@ class SecurityConfig(
                 HttpMethod.POST,
                 "/dsm-pick/after/**",
                 "/dsm-pick/meal",
-                "/dsm-pick/notice/",
+                "/dsm-pick/notice",
                 "/dsm-pick/schedule/create",
-                "/dsm-pick/self-study/",
-                "/dsm-pick/timetable/",
-                "/dsm-pick/weekend-meal/status"
-            ).hasRole(Role.SCH.toString())
-            .antMatchers(
-                HttpMethod.PUT,
-                "/dsm-pick/after/**"
+                "/dsm-pick/self-study/register",
+                "/dsm-pick/timetable",
+                "/dsm-pick/weekend-meal/saveAll",
+                "dsm-pick/status/saveAll",
+                "dsm-pick/schedule/**"
             ).hasRole(Role.SCH.toString())
             .antMatchers(
                 HttpMethod.GET,
@@ -53,56 +51,66 @@ class SecurityConfig(
                 "/dsm-pick/application/reason/all",
                 "/dsm-pick/application/non-return",
                 "/dsm-pick/application/status",
+                "/dsm-pick/application/floor",
+                "dsm-pick/application/grade",
                 "/dsm-pick/story/**",
                 "/dsm-pick/class-room/floor",
                 "/dsm-pick/class-room/grade",
                 "/dsm-pick/early-return/grade",
                 "/dsm-pick/early-return/floor",
                 "/dsm-pick/early-return/reason/ok-all",
-                "/dsm-pick/schedule/modify",
+                "/dsm-pick/early-return/ok",
                 "/dsm-pick/self-study/month",
                 "/dsm-pick/self-study/date",
                 "/dsm-pick/self-study/admin",
                 "/dsm-pick/weekend-meal/all",
                 "/dsm-pick/weekend-meal/quit",
-                "/dsm-pick/weekend-meal/excel"
+                "/dsm-pick/weekend-meal/excel",
+                "/dsm-pick/weekend-meal/hey",
+                "/dsm-pick/status/**",
+                "/dsm-pick/user/all"
             ).hasRole(Role.SCH.toString())
             .antMatchers(
                 HttpMethod.PATCH,
                 "/dsm-pick/application/**",
                 "/dsm-pick/early-return/**",
-                "/dsm-pick/notice/**",
-                "/dsm-pick/status/change"
+                "/dsm-pick/notice/modify",
+                "/dsm-pick/status/change",
+                "/dsm-pick/weekend-meal/status",
+                "dsm-pick/schedule/modify",
+                "dsm-pick/after/change"
             ).hasRole(Role.SCH.toString())
             .antMatchers(
                 HttpMethod.DELETE,
                 "/dsm-pick/after/**",
                 "/dsm-pick/notice/delete/",
-                "/dsm-pick/schedule/delete/"
+                "/dsm-pick/schedule/delete/",
+                "/dsm-pick/after/delete"
             )
             .hasRole(Role.SCH.toString())
             .antMatchers(
                 HttpMethod.POST,
-                "/dsm-pick/application/",
+                "/dsm-pick/application",
                 "/dsm-pick/class-room/move",
                 "/dsm-pick/early-return/create"
             ).hasRole(Role.STU.toString())
             .antMatchers(
                 HttpMethod.PATCH,
                 "/dsm-pick/application/status",
-                "/dsm-pick/application/change/**",
                 "/dsm-pick/weekend-meal/my-status"
             ).hasRole(Role.STU.toString())
             .antMatchers(
                 HttpMethod.GET,
+                "dsm-pick/user/simple",
+                "dsm-pick/user/details",
                 "/dsm-pick/application/my",
                 "/dsm-pick/application/simple",
                 "/dsm-pick/class-room/move",
                 "/dsm-pick/early-return/my",
                 "/dsm-pick/meal/date",
                 "/dsm-pick/timetable/**",
-                "/dsm-pick/user/simple",
-                "/dsm-pick/weekend-meal/my"
+                "/dsm-pick/weekend-meal/my",
+                "/dsm-pick/main"
             ).hasRole(Role.STU.toString())
             .antMatchers(
                 HttpMethod.DELETE,
