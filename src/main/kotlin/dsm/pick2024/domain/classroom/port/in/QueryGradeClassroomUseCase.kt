bb448139f6959ba7c9ed082007dad42461e514bc
soohyeon <meltapplee@gmail.com>
2024-04-02 15:24:01 +0900

    refactor :: name -> username

--- src/main/kotlin/dsm/pick2024/domain/classroom/port/in/QueryGradeClassroomUseCase.kt
+++ src/main/kotlin/dsm/pick2024/domain/classroom/port/in/QueryGradeClassroomUseCase.kt
@@ -3,5 +3,8 @@ package dsm.pick2024.domain.classroom.port.`in`
 import dsm.pick2024.domain.classroom.presentation.dto.response.QueryClassroomResponse
 
 interface QueryGradeClassroomUseCase {
-    fun queryGradeClassroom(grade: Int, classNum: Int): List<QueryClassroomResponse>
+    fun queryGradeClassroom(
+        grade: Int,
+        classNum: Int
+    ): List<QueryClassroomResponse>
 }
--- src/main/kotlin/dsm/pick2024/domain/classroom/presentation/dto/response/QueryClassroomResponse.kt
+++ src/main/kotlin/dsm/pick2024/domain/classroom/presentation/dto/response/QueryClassroomResponse.kt
@@ -1,7 +1,7 @@
 package dsm.pick2024.domain.classroom.presentation.dto.response
 
 data class QueryClassroomResponse(
-    val name: String,
+    val username: String,
     val classroomName: String,
     val move: String,
     val grade: Int,
