26d6e651022ac07de146e0b124a1d8d424a8ba8e
soohyeon <meltapplee@gmail.com>
2024-04-03 16:16:03 +0900

    modify :: localDate zone

--- src/main/kotlin/dsm/pick2024/domain/timetable/service/QueryWeekTimetableService.kt
+++ src/main/kotlin/dsm/pick2024/domain/timetable/service/QueryWeekTimetableService.kt
@@ -5,31 +5,32 @@ import dsm.pick2024.domain.timetable.port.out.FindTimetableByDayWeekPort
 import dsm.pick2024.domain.timetable.presentation.dto.DayTimetableResponse
 import dsm.pick2024.domain.timetable.presentation.dto.PeriodTimetableResponse
 import dsm.pick2024.domain.user.port.`in`.UserFacadeUseCase
-import java.time.LocalDate
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
+import java.time.ZoneId
 
 @Service
 class QueryWeekTimetableService(
     private val userFacadeUseCase: UserFacadeUseCase,
     private val findTimetableByDayWeekPort: FindTimetableByDayWeekPort
 ) : QueryWeekTimetableUseCase {
-
     @Transactional(readOnly = true)
     override fun queryWeekTimetable(): List<DayTimetableResponse> {
         val user = userFacadeUseCase.currentUser()
-        val day = LocalDate.now()
+        val day = LocalDate.now(ZoneId.of("Asia/Seoul"))
         val mon = day.minusDays(day.dayOfWeek.value.toLong() - 1)
 
         val timetableResponses = mutableListOf<DayTimetableResponse>()
 
         for (i in 0 until 5) {
             val date = mon.plusDays(i.toLong())
-            val tables = findTimetableByDayWeekPort.findTimetableByDayWeekPort(
-                date.dayOfWeek.value,
-                user.grade,
-                user.classNum
-            )
+            val tables =
+                findTimetableByDayWeekPort.findTimetableByDayWeekPort(
+                    date.dayOfWeek.value,
+                    user.grade,
+                    user.classNum
+                )
             val response = mutableListOf<PeriodTimetableResponse>()
 
             DayTimetableResponse(date, response).addTimetable(tables, response)
