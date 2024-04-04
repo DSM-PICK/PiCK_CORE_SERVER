606f635310fea559b701049069ef44a0ffa45571
도경 <dokyungkim2926@gmail.com>
2024-04-04 16:59:53 +0900

    modify :: logic

--- src/main/kotlin/dsm/pick2024/domain/selfstudy/service/QuerySelfStudyTeacherService.kt
+++ src/main/kotlin/dsm/pick2024/domain/selfstudy/service/QuerySelfStudyTeacherService.kt
@@ -14,10 +14,14 @@ class QuerySelfStudyTeacherService(
 ) : QueryDateSelfStudyUseCase {
 
     @Transactional(readOnly = true)
-    override fun queryDateSelfStudy(date: LocalDate): QuerySelfStudyTeacherResponse {
-        val selfStudy = findByDatePort.findByDate(date)
-            ?: throw SelfStudyNotFoundException
-
-        return QuerySelfStudyTeacherResponse(selfStudy.floor, selfStudy.teacher, selfStudy.date)
-    }
+    override fun queryDateSelfStudy(date: LocalDate) =
+        findByDatePort.findByDateList(date)
+            .map {
+                it ->
+                QuerySelfStudyTeacherResponse(
+                    it!!.floor,
+                    it.teacher,
+                    it.date
+                )
+            }
 }
