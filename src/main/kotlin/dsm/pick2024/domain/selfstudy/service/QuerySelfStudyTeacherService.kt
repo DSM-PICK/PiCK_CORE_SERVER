9fcdebd28b4361eb3f984649862aa3fca594d3f3
도경 <dokyungkim2926@gmail.com>
2024-04-04 17:01:30 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/domain/selfstudy/service/QuerySelfStudyTeacherService.kt
+++ src/main/kotlin/dsm/pick2024/domain/selfstudy/service/QuerySelfStudyTeacherService.kt
@@ -1,6 +1,5 @@
 package dsm.pick2024.domain.selfstudy.service
 
-import dsm.pick2024.domain.selfstudy.exception.SelfStudyNotFoundException
 import dsm.pick2024.domain.selfstudy.port.`in`.QueryDateSelfStudyUseCase
 import dsm.pick2024.domain.selfstudy.port.out.FindByDatePort
 import dsm.pick2024.domain.selfstudy.presentation.dto.response.QuerySelfStudyTeacherResponse
@@ -17,7 +16,7 @@ class QuerySelfStudyTeacherService(
     override fun queryDateSelfStudy(date: LocalDate) =
         findByDatePort.findByDateList(date)
             .map {
-                it ->
+                    it ->
                 QuerySelfStudyTeacherResponse(
                     it!!.floor,
                     it.teacher,
