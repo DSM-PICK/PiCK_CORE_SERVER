4c3ef22932adaa6b792f2a59b93aa69b6565eb04
도경 <dokyungkim2926@gmail.com>
2024-04-05 21:33:22 +0900

    add :: status save

--- src/main/kotlin/dsm/pick2024/domain/status/service/ChangeStatusService.kt
+++ src/main/kotlin/dsm/pick2024/domain/status/service/ChangeStatusService.kt
@@ -1,8 +1,10 @@
 package dsm.pick2024.domain.status.service
 
+import dsm.pick2024.domain.status.domain.Status
+import dsm.pick2024.domain.status.exception.StatusNotFoundException
 import dsm.pick2024.domain.status.port.`in`.ChangeStatusUseCase
 import dsm.pick2024.domain.status.port.out.FindStatusByUserId
-import dsm.pick2024.domain.status.port.out.SaveStatusPort
+import dsm.pick2024.domain.status.port.out.SaveAllStatusPort
 import dsm.pick2024.domain.status.presentation.dto.request.ChangeStatusRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,10 +12,24 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class ChangeStatusService(
     private val findStatusByUserId: FindStatusByUserId,
-    private val savePort: SaveStatusPort
+    private val saveAllStatusPort: SaveAllStatusPort
 ) : ChangeStatusUseCase {
     @Transactional
     override fun changeStatus(request: List<ChangeStatusRequest>) {
 
+        val statusUpdate = mutableListOf<Status>()
+
+        request.map {
+            requests ->
+            requests.request.map {
+                it ->
+                val status = findStatusByUserId.findStatusByUserId(it.id)
+                    ?: throw StatusNotFoundException
+                val add = status.copy(userId = it.id, type = it.statusType)
+                statusUpdate.add(add)
+            }
+
+        }
+        saveAllStatusPort.saveAll(statusUpdate)
     }
 }
