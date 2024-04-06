3b208bca85000a4a3eaf1b050da4cd455bce00fb
도경 <dokyungkim2926@gmail.com>
2024-04-06 16:54:08 +0900

    modify :: logic

--- src/main/kotlin/dsm/pick2024/domain/afterschool/service/ChangeStatusAfterSchoolStudentService.kt
+++ src/main/kotlin/dsm/pick2024/domain/afterschool/service/ChangeStatusAfterSchoolStudentService.kt
@@ -1,35 +1,42 @@
 package dsm.pick2024.domain.afterschool.service
 
+import dsm.pick2024.domain.afterschool.domain.AfterSchoolStudent
 import dsm.pick2024.domain.afterschool.port.`in`.ChangeStatusAfterSchoolStudentUseCase
 import dsm.pick2024.domain.afterschool.port.out.FindAfterSchoolStudentByUserIdPort
 import dsm.pick2024.domain.afterschool.port.out.SaveAfterSchoolStudentPort
+import dsm.pick2024.domain.afterschool.port.out.SaveAllAfterSchoolStudentPort
+import dsm.pick2024.domain.afterschool.presentation.dto.request.AfterSchool
 import dsm.pick2024.domain.afterschool.presentation.dto.request.ChangeAfterSchoolStatusRequest
 import dsm.pick2024.domain.user.exception.UserNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import kotlin.collections.List
 
 @Service
 class ChangeStatusAfterSchoolStudentService(
-    private val saveAfterSchoolStudentPort: SaveAfterSchoolStudentPort,
+    private val saveAllAfterSchoolStudentPort: SaveAllAfterSchoolStudentPort,
     private val findAfterSchoolStudentByUserIdPort: FindAfterSchoolStudentByUserIdPort
 ) : ChangeStatusAfterSchoolStudentUseCase {
     @Transactional
-    override fun changeStatusAfterSchoolStudent(request: List<ChangeAfterSchoolStatusRequest>) {
-        request.forEach { changeRequest ->
-            val student =
-                findAfterSchoolStudentByUserIdPort.findByUserId(changeRequest.id)
+    override fun changeStatusAfterSchoolStudent(request: ChangeAfterSchoolStatusRequest) {
+        val update = mutableListOf<AfterSchoolStudent>()
+
+        request.list.map {
+            requests ->
+            val period = requests.period
+            requests.afterSchool.map {
+                it ->
+                val afterSchool = findAfterSchoolStudentByUserIdPort.findByUserId(it.userId)
                     ?: throw UserNotFoundException
-
-            val newStatusList = changeRequest.statusList
-            val updatedStudent =
-                student.copy(
-                    status1 = newStatusList.getOrElse(0) { student.status1 },
-                    status2 = newStatusList.getOrElse(1) { student.status2 },
-                    status3 = newStatusList.getOrElse(2) { student.status3 }
-                )
-
-            saveAfterSchoolStudentPort.save(updatedStudent)
+                val add = when (period) {
+                    8 -> afterSchool.copy(status1 = it.status)
+                    9 -> afterSchool.copy(status2 = it.status)
+                    10 -> afterSchool.copy(status3 = it.status)
+                    else -> throw Exception("adsf")
+                }
+                update.add(add)
+            }
+            saveAllAfterSchoolStudentPort.saveAll(update)
         }
+
     }
 }
