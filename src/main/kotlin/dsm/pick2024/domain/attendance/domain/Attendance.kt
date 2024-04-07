783c3804302b2003cde2100c799a0ca551255cad
도경 <dokyungkim2926@gmail.com>
2024-04-07 16:22:50 +0900

    add :: domain

---
+++ src/main/kotlin/dsm/pick2024/domain/attendance/domain/Attendance.kt
@@ -0,0 +1,19 @@
+package dsm.pick2024.domain.attendance.domain
+
+import dsm.pick2024.domain.afterschool.enums.Status
+import java.util.*
+
+data class Attendance (
+    val id: UUID? = null,
+    val userId: UUID,
+    val grade: Int,
+    val classNum: Int,
+    val num: Int,
+    val name: String,
+    val club: String,
+    val period6: Status,
+    val period7: Status,
+    val period8: Status,
+    val period9: Status,
+    val period10: Status,
+)
