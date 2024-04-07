ebbab7e0f831b3cc2495a78375bee2fa3a559129
도경 <dokyungkim2926@gmail.com>
2024-04-07 22:53:32 +0900

    add :: request body

--- src/main/kotlin/dsm/pick2024/domain/attendance/presentation/AttendanceController.kt
+++ src/main/kotlin/dsm/pick2024/domain/attendance/presentation/AttendanceController.kt
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -26,6 +27,7 @@ class AttendanceController(
 
     @Operation(summary = "자습 or 방과후 상태관리")
     @PatchMapping("/modify")
-    fun changeAttendance(changeAttendanceRequest: List<ChangeAttendanceRequest>) =
+    fun changeAttendance(
+        @RequestBody changeAttendanceRequest: List<ChangeAttendanceRequest>) =
         changeAttendanceUseCase.changeAttendance(changeAttendanceRequest)
 }
