a51e3b8856273e5888a562237479bec749fd145e
soohyeon <meltapplee@gmail.com>
2024-04-10 22:10:07 +0900

    modify :: QueryAdminAllService

--- src/main/kotlin/dsm/pick2024/domain/admin/service/QueryAdminAllService.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/service/QueryAdminAllService.kt
@@ -2,11 +2,12 @@ package dsm.pick2024.domain.admin.service
 
 import dsm.pick2024.domain.admin.port.`in`.QueryAdminAllUseCase
 import dsm.pick2024.domain.admin.port.out.FindAllPort
+import dsm.pick2024.domain.user.entity.enums.Role
 import org.springframework.stereotype.Service
 
 @Service
 class QueryAdminAllService(
     private val findAllPort: FindAllPort
 ) : QueryAdminAllUseCase {
-    override fun queryAdminAll(): List<String> = findAllPort.findAll().map { it.name }
+    override fun queryAdminAll(): List<String> = findAllPort.findAll().filter { it.role == Role.SCH }.map { it.name }
 }
