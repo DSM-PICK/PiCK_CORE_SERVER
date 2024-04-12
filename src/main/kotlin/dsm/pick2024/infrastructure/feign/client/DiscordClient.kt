96a1b6a7694aa26a63ba8c8cb9b28db5913e17fb
도경 <dokyungkim2926@gmail.com>
2024-04-12 18:59:41 +0900

    modify :: client

--- src/main/kotlin/dsm/pick2024/infrastructure/feign/client/DiscordClient.kt
+++ src/main/kotlin/dsm/pick2024/infrastructure/feign/client/DiscordClient.kt
@@ -5,9 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
-@FeignClient(
-name = "discord-client",
-url = "\${discord.webhook.url}")
+@FeignClient(name = "discord-client")
 interface DiscordClient {
     @PostMapping("\${discord.webhook.prod}")
     fun prodSendAlarm(@RequestBody message: DiscordMessage)
