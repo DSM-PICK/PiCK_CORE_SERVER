a9db2a65f85d9710540fae7aeb2c51780a171af5
soohyeon <meltapplee@gmail.com>
2024-04-16 22:03:11 +0900

    delete :: QueryAllApplicationByStatusUseCase

--- src/main/kotlin/dsm/pick2024/domain/application/port/in/QueryAllApplicationByStatusUseCase.kt
+++
@@ -1,8 +0,0 @@
-package dsm.pick2024.domain.application.port.`in`
-
-import dsm.pick2024.domain.application.enums.Status
-import dsm.pick2024.domain.application.presentation.dto.response.QueryApplicationResponse
-
-interface QueryAllApplicationByStatusUseCase {
-    fun queryAllApplicationByStatus(status: Status): List<QueryApplicationResponse>
-}
