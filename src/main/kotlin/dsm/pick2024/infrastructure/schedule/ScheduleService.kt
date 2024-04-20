b379163c19ec0caaad91a0dfd5126dcd11f22adf
soohyeon <meltapplee@gmail.com>
2024-04-20 22:59:01 +0900

    modify :: scheduleService

--- src/main/kotlin/dsm/pick2024/infrastructure/schedule/ScheduleService.kt
+++ src/main/kotlin/dsm/pick2024/infrastructure/schedule/ScheduleService.kt
@@ -1,5 +1,6 @@
 package dsm.pick2024.infrastructure.schedule
 
+import dsm.pick2024.domain.afterschool.port.out.DeleteAllSchoolStudentPort
 import dsm.pick2024.domain.application.port.out.DeleteAllApplicationPort
 import dsm.pick2024.domain.attendance.port.`in`.ResetAttendanceUseCase
 import dsm.pick2024.domain.classroom.port.out.DeleteAllClassRoomPort
@@ -15,6 +16,7 @@ class ScheduleService(
     private val deleteAllClassRoomPort: DeleteAllClassRoomPort,
     private val deleteAllEarlyReturnPort: DeleteAllEarlyReturnPort,
     private val deleteAllApplicationPort: DeleteAllApplicationPort,
+    private val deleteAllSchoolStudentPort: DeleteAllSchoolStudentPort
     private val mealUseCase: MealUseCase,
     private val resetAttendanceUseCase: ResetAttendanceUseCase,
     private val resetWeekendMealUseCase: ResetWeekendMealUseCase,
@@ -25,6 +27,7 @@ class ScheduleService(
         deleteAllClassRoomPort.deleteAll()
         deleteAllEarlyReturnPort.deleteAll()
         deleteAllApplicationPort.deleteAll()
+        deleteAllSchoolStudentPort.deleteAll()
     }
 
     @Scheduled(cron = "0 0 0 25 * ?", zone = "Asia/Seoul")
