6775ba49f12665fd034920d80684a22f2776070d
도경 <dokyungkim2926@gmail.com>
2024-04-21 03:25:34 +0900

    add :: s3

---
+++ src/main/kotlin/dsm/pick2024/global/config/aws/AwsConfig.kt
@@ -0,0 +1,29 @@
+package dsm.pick2024.global.config.aws
+
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AwsConfig(
+    @Value("\${cloud.aws.credentials.accessKey}")
+    private val accessKey: String,
+    @Value("\${cloud.aws.credentials.secretKey}")
+    private val secretKey: String,
+    @Value("\${cloud.aws.region.static}")
+    private val region: String
+) {
+    @Bean
+    fun amazonS3(): AmazonS3 {
+        val awsCredentials: AWSCredentials = BasicAWSCredentials(accessKey, secretKey)
+        return AmazonS3ClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
+            .build()
+    }
+}
