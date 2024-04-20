0a25b8cda6f0bc45f6d6f2082e16baa903683792
도경 <dokyungkim2926@gmail.com>
2024-04-21 01:30:14 +0900

    test

---
+++ src/main/kotlin/dsm/pick2024/global/config/security/WebConfig.kt
@@ -0,0 +1,22 @@
+package dsm.pick2024.global.config.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowedOrigins(
+                "https://pick-admin.xquare.app",
+                "http://localhost:3000",
+                "http://localhost:3001"
+
+            )
+    }
+}
