161f1949ca83f0c4d4beeee1055984e8111cfc6f
soohyeon <meltapplee@gmail.com>
2024-04-20 22:47:37 +0900

    add :: resetStatusServie

--- src/main/kotlin/dsm/pick2024/infrastructure/schedule/ScheduleService.kt
+++ src/main/kotlin/dsm/pick2024/infrastructure/schedule/ScheduleService.kt
@@ -5,6 +5,7 @@ import dsm.pick2024.domain.attendance.port.`in`.ResetAttendanceUseCase
 import dsm.pick2024.domain.classroom.port.out.DeleteAllClassRoomPort
 import dsm.pick2024.domain.earlyreturn.port.out.DeleteAllEarlyReturnPort
 import dsm.pick2024.domain.meal.port.`in`.MealUseCase
+import dsm.pick2024.domain.status.port.`in`.ResetStatusUseCase
 import dsm.pick2024.domain.weekendmeal.port.`in`.ResetWeekendMealUseCase
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -16,7 +17,8 @@ class ScheduleService(
     private val deleteAllApplicationPort: DeleteAllApplicationPort,
     private val mealUseCase: MealUseCase,
     private val resetAttendanceUseCase: ResetAttendanceUseCase,
-    private val resetWeekendMealUseCase: ResetWeekendMealUseCase
+    private val resetWeekendMealUseCase: ResetWeekendMealUseCase,
+    private val resetStatusUseCase: ResetStatusUseCase,
 ) {
     @Scheduled(cron = "0 30 20 * * ?", zone = "Asia/Seoul")
     fun deleteTable() {
@@ -34,5 +36,6 @@ class ScheduleService(
     @Scheduled(cron = "0 00 21 * * ?", zone = "Asia/Seoul")
     fun resetTable() {
         resetAttendanceUseCase.reset()
+        resetStatusUseCase.reset()
     }
 }
