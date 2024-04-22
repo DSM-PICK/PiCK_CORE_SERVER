3be058d61f515c28e78e2791287aca6d8420a46e
soohyeon <meltapplee@gmail.com>
2024-04-22 19:25:29 +0900

    modify :: queryAdminNameResponse

--- src/main/kotlin/dsm/pick2024/domain/admin/presentation/dto/response/QueryAdminNameResponse.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/presentation/dto/response/QueryAdminNameResponse.kt
@@ -1,5 +1,7 @@
 package dsm.pick2024.domain.admin.presentation.dto.response
 
 data class QueryAdminNameResponse(
-    val name: String
+    val name: String,
+    val grade: Int,
+    val classNum: Int
 )
