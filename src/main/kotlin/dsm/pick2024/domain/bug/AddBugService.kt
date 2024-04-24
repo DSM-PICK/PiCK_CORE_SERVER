3c78195a0617f599db32bd3d6d883620fa1bb137
도경 <dokyungkim2926@gmail.com>
2024-04-25 08:56:57 +0900

    add :: api

--- src/main/kotlin/dsm/pick2024/domain/bug/AddBugService.kt
+++ src/main/kotlin/dsm/pick2024/domain/bug/AddBugService.kt
@@ -1,28 +1,42 @@
 package dsm.pick2024.domain.bug
 
 import dsm.pick2024.domain.bug.presentation.dto.request.BugRequest
-import dsm.pick2024.infrastructure.feign.client.DiscordMessageClient
+import dsm.pick2024.domain.user.facade.UserFacade
+import dsm.pick2024.global.discord.DiscordMessage
+import dsm.pick2024.infrastructure.feign.client.DiscordBugClient
 import dsm.pick2024.infrastructure.s3.FileUtil
 import dsm.pick2024.infrastructure.s3.PathList
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 
 @Service
 class AddBugService(
     private val fileUtil: FileUtil,
-    private val discordMessageClient: DiscordMessageClient
+    private val discordBugClient: DiscordBugClient,
+    private val userFacade: UserFacade
 ) {
+    @Async
     fun bugAlarm(request: BugRequest) {
-        val messageTitle = "Bug Report: ${request.title}"
-        val messageContent = buildString {
-            append("Title: ${request.title}\n")
-            append("Content: ${request.content}\n")
-            if (request.fileName != null) {
-                val fileUrl = fileUtil.generateObjectUrl(request.fileName, PathList.BUG)
-                append("File Name: ${request.fileName}\nFile URL: $fileUrl")
-            }
-        }
-
-        val message = mapOf("message" to messageTitle, "content" to messageContent)
-        discordMessageClient.sendMessage(message)
+        val message = DiscordMessage(
+            content = "# 🚨 버그 제보",
+            embeds = listOf(
+                DiscordMessage.Embed(
+                    title = "ℹ️ 버그 정보",
+                    description = """
+                        ### 🕖 버그 제목
+                        ${request.title}
+                        ### 🔗 버그 내용
+                        ${request.content}
+                        ### 📄 이미지
+                        ```
+                        ${request.fileName?.let { fileUtil.generateObjectUrl(request.fileName, PathList.BUG) }}
+                        ```
+                        ### 🧑🏻‍💻 버그 제보자
+                        ${userFacade.currentUser().name}
+                    """.trimIndent()
+                )
+            )
+        )
+        discordBugClient.sendMessage(message)
     }
 }
--- src/main/kotlin/dsm/pick2024/domain/bug/presentation/BugController.kt
+++ src/main/kotlin/dsm/pick2024/domain/bug/presentation/BugController.kt
@@ -3,6 +3,8 @@ package dsm.pick2024.domain.bug.presentation
 import dsm.pick2024.domain.bug.AddBugService
 import dsm.pick2024.domain.bug.UploadBugImageService
 import dsm.pick2024.domain.bug.presentation.dto.request.BugRequest
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
+@Tag(name = "버그제보 api")
 @RestController
 @RequestMapping("/bug")
 class BugController(
@@ -19,6 +22,7 @@ class BugController(
     private val addBugService: AddBugService
 ) {
 
+    @Operation(summary = "버그제보 이미지 업로드 API")
     @ResponseStatus(value = HttpStatus.OK)
     @PostMapping("/upload")
     fun uploadBugImage(
@@ -26,6 +30,7 @@ class BugController(
     ) =
         uploadBugImageService.uploadBugImage(file)
 
+    @Operation(summary = "버그제부 문자 전송 API")
     @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping("/message")
     fun sendBugMessage(
