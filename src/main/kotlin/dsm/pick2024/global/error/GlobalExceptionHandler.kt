def57fce369a7076bbf30e7392238cf8dde2f7e6
도경 <dokyungkim2926@gmail.com>
2024-05-02 19:54:51 +0900

    modify ::  security

--- src/main/kotlin/dsm/pick2024/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/dsm/pick2024/global/error/GlobalExceptionHandler.kt
@@ -1,21 +1,13 @@
 package dsm.pick2024.global.error
 
-import dsm.pick2024.infrastructure.discord.DiscordComponet
 import dsm.pick2024.global.error.exception.PickException
-import org.springframework.context.MessageSource
-import org.springframework.core.env.Environment
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.context.request.WebRequest
-import java.io.Serializable
 
 @RestControllerAdvice
 class GlobalExceptionHandler(
-    private val messageSource: MessageSource,
-    private val discordComponet: DiscordComponet,
-    private val environment: Environment
 ) {
 
     @ExceptionHandler(PickException::class)
@@ -25,17 +17,5 @@ class GlobalExceptionHandler(
             ErrorResponse(code.status, code.message),
             HttpStatus.valueOf(code.status)
         )
-    }
-
-    @ExceptionHandler(Exception::class)
-    fun exception(e: PickException, request: WebRequest): ResponseEntity<Any>? {
-        if (!listOf(environment.activeProfiles).contains<Serializable?>("local")) {
-            discordComponet.sendDiscordAlarm(e, request)
-            return ResponseEntity(
-                ErrorResponse(e.errorCode.status, e.message),
-                HttpStatus.valueOf(e.errorCode.status)
-            )
-        }
-        return null
     }
 }
