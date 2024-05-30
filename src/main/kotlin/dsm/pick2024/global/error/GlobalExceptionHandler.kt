495d1703da2928e97e0059d5acaef6436ace812d
도경 <dokyungkim2926@gmail.com>
2024-05-30 20:27:43 +0900

    modify :: handler

--- src/main/kotlin/dsm/pick2024/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/dsm/pick2024/global/error/GlobalExceptionHandler.kt
@@ -3,8 +3,14 @@ package dsm.pick2024.global.error
 import dsm.pick2024.global.error.exception.PickException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.io.IOException
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @RestControllerAdvice
 class GlobalExceptionHandler() {
@@ -15,6 +21,22 @@ class GlobalExceptionHandler() {
         return ResponseEntity(
             ErrorResponse(code.status, code.message),
             HttpStatus.valueOf(code.status)
+        )
+    }
+
+    @ExceptionHandler(AccessDeniedException::class)
+    fun handleAccessDeniedException(e: AccessDeniedException, response: HttpServletResponse) {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied")
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun validatorExceptionHandler(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(
+            ErrorResponse(
+                400,
+                e.bindingResult.allErrors[0].defaultMessage
+            ),
+            HttpStatus.BAD_REQUEST
         )
     }
 }
