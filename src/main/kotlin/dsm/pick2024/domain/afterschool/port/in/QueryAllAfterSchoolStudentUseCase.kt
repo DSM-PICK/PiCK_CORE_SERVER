ef7a862d5fabcf0e7b20d224d84163343ea9c624
도경 <dokyungkim2926@gmail.com>
2024-06-04 23:45:40 +0900

    refactor

--- src/main/kotlin/dsm/pick2024/domain/afterschool/port/in/QueryAllAfterSchoolStudentUseCase.kt
+++ src/main/kotlin/dsm/pick2024/domain/afterschool/port/in/QueryAllAfterSchoolStudentUseCase.kt
@@ -2,6 +2,6 @@ package dsm.pick2024.domain.afterschool.port.`in`
 
 import dsm.pick2024.domain.afterschool.presentation.dto.response.QueryAfterSchoolStudentAllResponse
 
-interface QueryAfterSchoolStudentAllUseCase {
+interface QueryAllAfterSchoolStudentUseCase {
     fun queryAfterSchoolStudentAll(): List<QueryAfterSchoolStudentAllResponse>
 }
--- src/main/kotlin/dsm/pick2024/domain/afterschool/service/QueryAllAfterSchoolStudentService.kt
+++ src/main/kotlin/dsm/pick2024/domain/afterschool/service/QueryAllAfterSchoolStudentService.kt
@@ -1,15 +1,15 @@
 package dsm.pick2024.domain.afterschool.service
 
-import dsm.pick2024.domain.afterschool.port.`in`.QueryAfterSchoolStudentAllUseCase
+import dsm.pick2024.domain.afterschool.port.`in`.QueryAllAfterSchoolStudentUseCase
 import dsm.pick2024.domain.afterschool.port.out.QueryAfterSchoolStudentPort
 import dsm.pick2024.domain.afterschool.presentation.dto.response.QueryAfterSchoolStudentAllResponse
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class QueryAfterSchoolStudentAllService(
+class QueryAllAfterSchoolStudentService(
     private val queryAfterSchoolPort: QueryAfterSchoolStudentPort
-) : QueryAfterSchoolStudentAllUseCase {
+) : QueryAllAfterSchoolStudentUseCase {
 
     @Transactional(readOnly = true)
     override fun queryAfterSchoolStudentAll() =
