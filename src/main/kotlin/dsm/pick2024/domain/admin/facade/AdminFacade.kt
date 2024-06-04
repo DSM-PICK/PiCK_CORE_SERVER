eb186b65c63aca9a28f497c45a42455835270ea5
도경 <dokyungkim2926@gmail.com>
2024-06-04 22:54:17 +0900

    refactor :: fun name

--- src/main/kotlin/dsm/pick2024/domain/admin/facade/AdminFacade.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/facade/AdminFacade.kt
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Component
 class AdminFacade(
     private val findByAdminIdPort: FindByAdminIdPort
 ) : AdminFacadeUseCase {
-    override fun currentUser(): Admin {
+
+    override fun currentAdmin(): Admin {
         val name = SecurityContextHolder.getContext().authentication.name
         return getAdminByAdminId(name)
     }
--- src/main/kotlin/dsm/pick2024/domain/admin/port/in/AdminFacadeUseCase.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/port/in/AdminFacadeUseCase.kt
@@ -3,7 +3,7 @@ package dsm.pick2024.domain.admin.port.`in`
 import dsm.pick2024.domain.admin.domain.Admin
 
 interface AdminFacadeUseCase {
-    fun currentUser(): Admin
+    fun currentAdmin(): Admin
 
     fun getAdminByAdminId(adminId: String): Admin?
 }
