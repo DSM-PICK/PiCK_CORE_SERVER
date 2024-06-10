362f5ca6b75761b2ba3a8f52ed105fb557af89b3
도경 <dokyungkim2926@gmail.com>
2024-06-10 22:46:25 +0900

    refactor :: 급식 query 함수

--- src/main/kotlin/dsm/pick2024/domain/meal/port/out/MealPort.kt
+++ src/main/kotlin/dsm/pick2024/domain/meal/port/out/MealPort.kt
@@ -2,4 +2,4 @@ package dsm.pick2024.domain.meal.port.out
 
 interface MealPort :
     SaveMealPort,
-    FindMealsByMealDatePort
+    QueryMealPort
--- src/main/kotlin/dsm/pick2024/domain/meal/port/out/QueryMealPort.kt
+++ src/main/kotlin/dsm/pick2024/domain/meal/port/out/QueryMealPort.kt
@@ -3,6 +3,6 @@ package dsm.pick2024.domain.meal.port.out
 import dsm.pick2024.domain.meal.domain.Meal
 import java.time.LocalDate
 
-interface FindMealsByMealDatePort {
+interface QueryMealPort {
     fun findMealsByMealDate(date: LocalDate): List<Meal>
 }
