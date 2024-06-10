4b83905244a112bc45f0b6f65f107d492e3db498
soohyeon <meltapplee@gmail.com>
2024-06-10 23:45:59 +0900

    refactor :: saveFeignSchedule

--- src/main/kotlin/dsm/pick2024/domain/schedule/port/out/SaveSchedulePort.kt
+++ src/main/kotlin/dsm/pick2024/domain/schedule/port/out/SaveSchedulePort.kt
@@ -1,11 +1,9 @@
 package dsm.pick2024.domain.schedule.port.out
 
 import dsm.pick2024.domain.schedule.domain.Schedule
-import dsm.pick2024.domain.schedule.entity.ScheduleJpaEntity
 
 interface SaveSchedulePort {
-
-    fun saveFeignSchedule(schedule: MutableList<ScheduleJpaEntity>)
+    fun saveFeignSchedule(schedule: MutableList<Schedule>)
 
     fun save(schedule: Schedule)
 }
