4e4533ba29b46399283c9aab2b525ff8a1706c54
soohyeon <meltapplee@gmail.com>
2024-06-10 23:46:16 +0900

    refactor :: entity -> domain

--- src/main/kotlin/dsm/pick2024/infrastructure/feign/NeisTimetableFeignClientService.kt
+++ src/main/kotlin/dsm/pick2024/infrastructure/feign/NeisTimetableFeignClientService.kt
@@ -1,14 +1,14 @@
 package dsm.pick2024.infrastructure.feign
 
 import com.google.gson.Gson
-import dsm.pick2024.domain.timetable.entity.TimetableJpaEntity
+import dsm.pick2024.domain.timetable.domain.Timetable
 import dsm.pick2024.infrastructure.feign.client.NeisFeignClient
-import dsm.pick2024.infrastructure.feign.client.property.NeisFeignClientRequestProperty
 import dsm.pick2024.infrastructure.feign.client.dto.response.NeisFeignClientTimetableResponse
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import dsm.pick2024.infrastructure.feign.client.property.NeisFeignClientRequestProperty
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Component
 class NeisTimetableFeignClientService(
@@ -16,24 +16,26 @@ class NeisTimetableFeignClientService(
     private val neisKey: String,
     private val neisFeignClient: NeisFeignClient
 ) {
-    fun getNeisInfoToEntity(): MutableList<TimetableJpaEntity>? {
+    fun getNeisInfoToEntity(): MutableList<Timetable>? {
         val runDay = LocalDate.now()
 
-        val neisTimetableServiceInfoString = neisFeignClient.hisTimetable(
-            key = neisKey,
-            type = NeisFeignClientRequestProperty.TYPE,
-            pageIndex = NeisFeignClientRequestProperty.PAGE_INDEX,
-            pageSize = NeisFeignClientRequestProperty.PAGE_SIZE,
-            schoolCode = NeisFeignClientRequestProperty.SD_SCHUL_CODE,
-            atptCode = NeisFeignClientRequestProperty.ATPT_OFCDC_CODE,
-            startedYmd = runDay.withDayOfMonth(runDay.dayOfMonth).toString().replace("-", ""),
-            endedYmd = runDay.withDayOfMonth(runDay.dayOfMonth).plusDays(7).toString().replace("-", "")
-        )
-        val timetableJson = Gson().fromJson(
-            neisTimetableServiceInfoString,
-            NeisFeignClientTimetableResponse::class.java
-        )
-        val timetableEntities = mutableListOf<TimetableJpaEntity>()
+        val neisTimetableServiceInfoString =
+            neisFeignClient.hisTimetable(
+                key = neisKey,
+                type = NeisFeignClientRequestProperty.TYPE,
+                pageIndex = NeisFeignClientRequestProperty.PAGE_INDEX,
+                pageSize = NeisFeignClientRequestProperty.PAGE_SIZE,
+                schoolCode = NeisFeignClientRequestProperty.SD_SCHUL_CODE,
+                atptCode = NeisFeignClientRequestProperty.ATPT_OFCDC_CODE,
+                startedYmd = runDay.withDayOfMonth(runDay.dayOfMonth).toString().replace("-", ""),
+                endedYmd = runDay.withDayOfMonth(runDay.dayOfMonth).plusDays(7).toString().replace("-", "")
+            )
+        val timetableJson =
+            Gson().fromJson(
+                neisTimetableServiceInfoString,
+                NeisFeignClientTimetableResponse::class.java
+            )
+        val timetables = mutableListOf<Timetable>()
 
         for (timetable in timetableJson.hisTimetable) {
             timetable.row?.let { rows ->
@@ -44,9 +46,8 @@ class NeisTimetableFeignClientService(
                     val period = row.PERIO
                     val subjectName = row.ITRT_CNTNT
 
-                    timetableEntities.add(
-                        TimetableJpaEntity(
-                            id = null,
+                    timetables.add(
+                        Timetable(
                             grade = grade,
                             classNum = classNum,
                             period = period,
@@ -57,8 +58,9 @@ class NeisTimetableFeignClientService(
                 }
             }
         }
-        return timetableEntities
+        return timetables
     }
+
     private fun changeStringToLocalDate(date: String): LocalDate {
         return LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE)
     }
