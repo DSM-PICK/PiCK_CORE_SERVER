c53ce12523eed2acbf6362b15e8c533a2c5b03ca
도경 <dokyungkim2926@gmail.com>
2024-06-11 19:07:09 +0900

    refactor

--- src/main/kotlin/dsm/pick2024/domain/attendance/port/out/FindAttendanceByUserIdPort.kt
+++
@@ -1,8 +0,0 @@
-package dsm.pick2024.domain.attendance.port.out
-
-import dsm.pick2024.domain.attendance.domain.Attendance
-import java.util.UUID
-
-interface FindAttendanceByUserIdPort {
-    fun findByUserId(userId: UUID): Attendance?
-}
--- src/main/kotlin/dsm/pick2024/domain/attendance/service/ResetAttendanceService.kt
+++ src/main/kotlin/dsm/pick2024/domain/attendance/service/ResetAttendanceService.kt
@@ -3,19 +3,19 @@ package dsm.pick2024.domain.attendance.service
 import dsm.pick2024.domain.afterschool.enums.Status
 import dsm.pick2024.domain.attendance.domain.Attendance
 import dsm.pick2024.domain.attendance.port.`in`.ResetAttendanceUseCase
-import dsm.pick2024.domain.attendance.port.out.FindAllAttendancePort
+import dsm.pick2024.domain.attendance.port.out.QueryAttendancePort
 import dsm.pick2024.domain.attendance.port.out.SaveAllPort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ResetAttendanceService(
-    private val findAllAttendancePort: FindAllAttendancePort,
+    private val queryAttendancePort: QueryAttendancePort,
     private val saveAllPort: SaveAllPort
 ) : ResetAttendanceUseCase {
     @Transactional
     override fun reset() {
-        val allStudent = findAllAttendancePort.findAll()
+        val allStudent = queryAttendancePort.findAll()
         val update = mutableListOf<Attendance>()
 
         allStudent.map { attendance ->
