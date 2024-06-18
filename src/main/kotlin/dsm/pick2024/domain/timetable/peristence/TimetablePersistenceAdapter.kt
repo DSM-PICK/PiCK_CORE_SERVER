654b284ddea5c197bf9ddfdca3e93e216a108c11
도경 <dokyungkim2926@gmail.com>
2024-06-18 20:19:41 +0900

    lint

--- src/main/kotlin/dsm/pick2024/domain/timetable/peristence/TimetablePersistenceAdapter.kt
+++ src/main/kotlin/dsm/pick2024/domain/timetable/peristence/TimetablePersistenceAdapter.kt
@@ -3,7 +3,6 @@ package dsm.pick2024.domain.timetable.peristence
 import com.querydsl.jpa.impl.JPAQueryFactory
 import dsm.pick2024.domain.timetable.domain.Timetable
 import dsm.pick2024.domain.timetable.entity.QTimetableJpaEntity
-import dsm.pick2024.domain.timetable.entity.TimetableJpaEntity
 import dsm.pick2024.domain.timetable.mapper.TimetableMapper
 import dsm.pick2024.domain.timetable.peristence.repository.TimetableRepository
 import dsm.pick2024.domain.timetable.port.out.TimetablePort
