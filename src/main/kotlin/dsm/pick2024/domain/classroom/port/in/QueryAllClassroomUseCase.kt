fdb5910f09a5f0a8c072f82c8896b77939b7e7ec
soohyeon <meltapplee@gmail.com>
2024-06-25 19:29:14 +0900

    add :: class-room all api

--- src/main/kotlin/dsm/pick2024/domain/classroom/port/in/QueryAllClassroomUseCase.kt
+++
@@ -1,8 +0,0 @@
-package dsm.pick2024.domain.classroom.port.`in`
-
-import dsm.pick2024.domain.application.enums.Status
-import dsm.pick2024.domain.classroom.presentation.dto.response.QueryClassroomResponse
-
-interface QueryAllClassroomUseCase {
-    fun queryAllClassroom(status: Status): List<QueryClassroomResponse>
-}
