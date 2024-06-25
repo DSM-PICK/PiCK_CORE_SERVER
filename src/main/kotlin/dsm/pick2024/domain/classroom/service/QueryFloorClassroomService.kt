4aa26c5e841313706776ef734f7eac134dc7a327
soohyeon <meltapplee@gmail.com>
2024-06-25 19:31:51 +0900

    modify :: logic

--- src/main/kotlin/dsm/pick2024/domain/classroom/service/QueryFloorClassroomService.kt
+++ src/main/kotlin/dsm/pick2024/domain/classroom/service/QueryFloorClassroomService.kt
@@ -11,30 +11,31 @@ import org.springframework.transaction.annotation.Transactional
 class QueryFloorClassroomService(
     private val queryClassroomPort: QueryClassroomPort
 ) : QueryFloorClassroomUseCase {
+
     @Transactional(readOnly = true)
     override fun queryFloorClassroom(
         floor: Int,
         status: Status
-    ) =
-        queryClassroomPort.queryFloorClassroom(floor)
-            .filter {
-                if (status == Status.QUIET) {
-                    it.status == Status.QUIET
-                } else {
-                    it.status == Status.OK
-                }
-            }
-            .map {
-                QueryClassroomResponse(
-                    id = it.userId,
-                    username = it.userName,
-                    classroomName = it.classroomName,
-                    move = "${it.grade}-${it.classNum}",
-                    grade = it.grade,
-                    classNum = it.classNum,
-                    num = it.num,
-                    startPeriod = it.startPeriod,
-                    endPeriod = it.endPeriod
-                )
-            }
+    ): List<QueryClassroomResponse> {
+        val classrooms = if (floor == 5) {
+            queryClassroomPort.findAllByStatus(status)
+        } else {
+            queryClassroomPort.queryFloorClassroom(floor)
+                .filter { it.status == if (status == Status.QUIET) Status.QUIET else Status.OK }
+        }
+
+        return classrooms.map {
+            QueryClassroomResponse(
+                id = it.userId,
+                username = it.userName,
+                classroomName = it.classroomName,
+                move = "${it.grade}-${it.classNum}",
+                grade = it.grade,
+                classNum = it.classNum,
+                num = it.num,
+                startPeriod = it.startPeriod,
+                endPeriod = it.endPeriod
+            )
+        }.sortedWith(compareBy({ it.grade }, { it.classNum }, { it.num }))
+    }
 }
