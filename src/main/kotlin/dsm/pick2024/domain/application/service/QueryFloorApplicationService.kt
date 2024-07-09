1dd326b68cb751b1b5e3e725122174a0363b66da
soohyeon <meltapplee@gmail.com>
2024-07-09 17:11:25 +0900

    add :: queryFloorApplicationService

--- src/main/kotlin/dsm/pick2024/domain/application/service/QueryFloorApplicationService.kt
+++ src/main/kotlin/dsm/pick2024/domain/application/service/QueryFloorApplicationService.kt
@@ -1,21 +1,42 @@
 package dsm.pick2024.domain.application.service
 
 import dsm.pick2024.domain.application.enums.Status
-import dsm.pick2024.domain.application.port.`in`.QueryFloorApplicationUseCase
+import dsm.pick2024.domain.application.port.`in`.QueryFloorAndStatusApplicationUseCase
+import dsm.pick2024.domain.application.port.out.QueryAllApplicationPort
 import dsm.pick2024.domain.application.port.out.QueryApplicationPort
 import dsm.pick2024.domain.application.presentation.dto.response.QueryApplicationResponse
+import dsm.pick2024.domain.classroom.exception.FloorNotFoundException
+import org.joda.time.LocalDate
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QueryFloorApplicationService(
-    private val queryApplicationPort: QueryApplicationPort
-) : QueryFloorApplicationUseCase {
+    private val queryApplicationPort: QueryApplicationPort,
+    private val queryAllApplicationPort: QueryAllApplicationPort
+) : QueryFloorAndStatusApplicationUseCase {
 
     @Transactional(readOnly = true)
-    override fun queryFloorApplication(floor: Int) =
+    override fun queryFloorAndStatusApplication(floor: Int, status: Status): List<QueryApplicationResponse> {
+        val today = LocalDate.now().dayOfWeek
 
-        queryApplicationPort.findByFloor(floor)
-            .filter { it.status == Status.QUIET }
-            .map { QueryApplicationResponse(it) }
+        val applications = when (floor) {
+            2, 3, 4 -> {
+                val filteredClassrooms = if (today == 2 || today == 5) {
+                    queryApplicationPort.queryApplicationWithAttendance(floor)
+                } else {
+                    queryApplicationPort.findByFloorAndStatus(floor, status)
+                }
+                filteredClassrooms.filter { it.status == status }
+            }
+            5 -> {
+                queryAllApplicationPort.findAllByStatus(status)
+            }
+            else -> throw FloorNotFoundException
+        }
+
+        return applications.map { QueryApplicationResponse(it) }.sortedWith(
+            compareBy({ it.grade }, { it.classNum }, { it.num })
+        )
+    }
 }
