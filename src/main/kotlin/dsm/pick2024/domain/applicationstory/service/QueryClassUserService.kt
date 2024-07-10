a6866909294f2cb0c8e7952745304873b8f40c7a
soohyeon <meltapplee@gmail.com>
2024-07-10 14:03:47 +0900

    refactor :: exception

--- src/main/kotlin/dsm/pick2024/domain/applicationstory/service/QueryClassUserService.kt
+++ src/main/kotlin/dsm/pick2024/domain/applicationstory/service/QueryClassUserService.kt
@@ -5,6 +5,7 @@ import dsm.pick2024.domain.applicationstory.port.`in`.QueryClassUserUseCase
 import dsm.pick2024.domain.applicationstory.port.out.QueryAllApplicationStoryPort
 import dsm.pick2024.domain.applicationstory.presentation.dto.response.QueryUserClassResponse
 import dsm.pick2024.domain.status.port.out.QueryStatusPort
+import dsm.pick2024.domain.user.exception.UserNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -21,7 +22,7 @@ class QueryClassUserService(
         val students = queryStatusPort.findByGradeAndClassNum(grade, classNum)
 
         return students.map { student ->
-            val applicationStory = queryAllApplicationStoryPort.findAllByUserId(student.userId) ?: throw Exception()
+            val applicationStory = queryAllApplicationStoryPort.findAllByUserId(student.userId) ?: throw UserNotFoundException
             val applicationCnt = applicationStory.count { it.type == Type.APPLICATION } ?: 0
             val earlyReturnCnt = applicationStory.count { it.type == Type.EARLY_RETURN } ?: 0
 
