206790e02f5c966bf53872ff54747ccece93c0ab
soohyeon <meltapplee@gmail.com>
2024-08-05 19:56:48 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/domain/user/persistence/repository/UserRepository.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/persistence/repository/UserRepository.kt
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface UserRepository : JpaRepository<UserJpaEntity, UUID> {
+
     fun findByName(name: String): UserJpaEntity?
 
     fun findByAccountId(accountId: String): UserJpaEntity?
