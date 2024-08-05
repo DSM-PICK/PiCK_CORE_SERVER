c5018e36146d4c3b23ca913002212262ad912abf
도경 <dokyungkim2926@gmail.com>
2024-08-05 20:13:59 +0900

    modify :: swagger config

--- src/main/kotlin/dsm/pick2024/global/config/swagger/SwaggerConfig.kt
+++ src/main/kotlin/dsm/pick2024/global/config/swagger/SwaggerConfig.kt
@@ -4,15 +4,21 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.ForwardedHeaderFilter
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @Configuration
-class SwaggerConfig {
+@EnableWebMvc
+class SwaggerConfig : WebMvcConfigurer {
     @Bean
-    fun openAPI(): OpenAPI {
+    fun openApi(): OpenAPI {
         return OpenAPI()
             .info(
-                Info().title("PICK 2024")
-                    .description("PICK 2024")
+                Info().title("PiCK API")
+                    .description("PiCK api 입니다.")
+                    .version("v0.0.1")
             )
     }
 }
