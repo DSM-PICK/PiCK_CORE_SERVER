8d96c8acaa52c9828bde726e0c57e76584ed28dd
도경 <dokyungkim2926@gmail.com>
2024-08-22 21:48:47 +0900

    modify :: domain, entity

--- src/main/kotlin/dsm/pick2024/domain/application/domain/Application.kt
+++ src/main/kotlin/dsm/pick2024/domain/application/domain/Application.kt
@@ -1,5 +1,6 @@
 package dsm.pick2024.domain.application.domain
 
+import dsm.pick2024.domain.application.enums.ApplicationType
 import dsm.pick2024.domain.application.enums.Status
 import dsm.pick2024.global.annotation.Aggregate
 import java.time.LocalDate
@@ -12,12 +13,13 @@ data class Application(
     val userId: UUID,
     val reason: String,
     val date: LocalDate,
-    val startTime: LocalTime,
-    val endTime: LocalTime,
+    val start: String,
+    val end: String,
     val userName: String,
     val teacherName: String? = null,
     val grade: Int,
     val classNum: Int,
     val num: Int,
-    val status: Status
+    val status: Status,
+    val applicationType: ApplicationType
 )
