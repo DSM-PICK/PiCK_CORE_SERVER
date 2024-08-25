50ef5c8bff46ecd730cc069444c5eb9f84183fe8
도경 <dokyungkim2926@gmail.com>
2024-08-25 23:16:02 +0900

    modify :: logic

--- src/main/kotlin/dsm/pick2024/domain/attendance/domain/service/AttendanceService.kt
+++ src/main/kotlin/dsm/pick2024/domain/attendance/domain/service/AttendanceService.kt
@@ -51,29 +51,24 @@ class AttendanceService {
             ApplicationType.TIME -> {
                 val startTime = LocalTime.parse(start)
                 val endTime = LocalTime.parse(end)
-                periods.filter { startTime <= it.second && endTime > it.first }
+                periods.filter { startTime <= it.second && endTime >= it.first }
             }
         }
 
         var updatedAttendance = attendance
 
-        matchingPeriods.forEachIndexed { index, _ ->
-            updatedAttendance = when (index) {
-                0 -> updatedAttendance.copy(period6 = AttendanceStatus.GO_OUT)
-                1 -> updatedAttendance.copy(period7 = AttendanceStatus.GO_OUT)
-                2 -> updatedAttendance.copy(period8 = AttendanceStatus.GO_OUT)
-                3 -> updatedAttendance.copy(period9 = AttendanceStatus.GO_OUT)
-                4 -> updatedAttendance.copy(period10 = AttendanceStatus.GO_OUT)
+        matchingPeriods.forEach { period ->
+            updatedAttendance = when (period) {
+                period6 -> updatedAttendance.copy(period6 = AttendanceStatus.GO_OUT)
+                period7 -> updatedAttendance.copy(period7 = AttendanceStatus.GO_OUT)
+                period8 -> updatedAttendance.copy(period8 = AttendanceStatus.GO_OUT)
+                period9 -> updatedAttendance.copy(period9 = AttendanceStatus.GO_OUT)
+                period10 -> updatedAttendance.copy(period10 = AttendanceStatus.GO_OUT)
                 else -> updatedAttendance
             }
         }
 
         return updatedAttendance
-    }
-
-
-    private fun getPeriodTime(periodName: String): Pair<LocalTime, LocalTime>? {
-        return periodMap[periodName]
     }
 
     private fun getMatchingPeriods(startTime: LocalTime, endTime: LocalTime?): List<String> {
