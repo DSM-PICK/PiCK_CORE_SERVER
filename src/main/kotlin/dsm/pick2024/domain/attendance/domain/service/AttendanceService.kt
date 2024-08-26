6bc86a9efc9211a88717310bc97889e9cbf0237b
도경 <dokyungkim2926@gmail.com>
2024-08-26 09:24:34 +0900

    refactor

--- src/main/kotlin/dsm/pick2024/domain/attendance/domain/service/AttendanceService.kt
+++ src/main/kotlin/dsm/pick2024/domain/attendance/domain/service/AttendanceService.kt
@@ -10,15 +10,22 @@ import java.time.LocalTime
 class AttendanceService {
 
     companion object {
-        val period6 = LocalTime.of(14, 30) to LocalTime.of(15, 30) // 6교시
-        val period7 = LocalTime.of(15, 30) to LocalTime.of(16, 30) // 7교시
-        val period8 = LocalTime.of(16, 40) to LocalTime.of(18, 30) // 8교시
-        val period9 = LocalTime.of(18, 40) to LocalTime.of(19, 30) // 9교시
-        val period10 = LocalTime.of(19, 40) to LocalTime.of(20, 30) // 10교시
-
-        val periods = listOf(period6, period7, period8, period9, period10)
-        val periodNames = listOf("6교시", "7교시", "8교시", "9교시", "10교시")
-        val periodMap = periodNames.zip(periods).toMap()
+        val periods = listOf(
+            LocalTime.of(8, 40) to LocalTime.of(9, 40), // 1교시
+            LocalTime.of(9, 40) to LocalTime.of(10, 40), // 2교시
+            LocalTime.of(10, 40) to LocalTime.of(11, 40), // 3교시
+            LocalTime.of(12, 40) to LocalTime.of(13, 30), // 4교시
+            LocalTime.of(13, 30) to LocalTime.of(14, 40), // 5교시
+            LocalTime.of(14, 30) to LocalTime.of(15, 30), // 6교시
+            LocalTime.of(15, 30) to LocalTime.of(16, 30), // 7교시
+            LocalTime.of(16, 40) to LocalTime.of(18, 40), // 8교시
+            LocalTime.of(18, 40) to LocalTime.of(19, 40), // 9교시
+            LocalTime.of(19, 40) to LocalTime.of(20, 30)  // 10교시
+        )
+        val periodNames = listOf(
+            "1교시", "2교시", "3교시", "4교시", "5교시",
+            "6교시", "7교시", "8교시", "9교시", "10교시"
+        )
     }
 
     // 교시 혹은 시간을 기반으로 교시 목록을 반환하는 함수
@@ -28,7 +35,7 @@ class AttendanceService {
             ApplicationType.TIME -> {
                 val startTime = LocalTime.parse(start)
                 val endTime = end?.let { LocalTime.parse(it) }
-                getMatchingPeriods(startTime, endTime)
+                getMatchPeriods(startTime, endTime)
             }
         }
     }
@@ -40,41 +47,48 @@ class AttendanceService {
         applicationType: ApplicationType,
         attendance: Attendance
     ): Attendance {
-        val matchingPeriods = when (applicationType) {
+        // applicationType에 따라 매칭되는 period을 찾음
+        val matchPeriods = when (applicationType) {
             ApplicationType.PERIOD -> {
-                val startIndex = periodNames.indexOf(start)
-                val endIndex = periodNames.indexOf(end)
-                (startIndex.takeIf { it != -1 } to endIndex.takeIf { it != -1 })
-                    .takeIf { it.first != null && it.second != null && it.first!! <= it.second!! }
-                    ?.let { periods.subList(it.first!!, it.second!! + 1) } ?: emptyList()
+                // 시작 기간의 위치를 찾고, 없으면 기본값 0으로 설정
+                val startIndex = periodNames.indexOf(start).coerceAtLeast(0)
+                // 종료 기간의 위치를 찾고, 없으면 마지막 위치로 설정
+                val endIndex = periodNames.indexOf(end).takeIf { it != -1 } ?: (periodNames.size - 1)
+                periods.subList(startIndex, endIndex + 1)
             }
+
             ApplicationType.TIME -> {
+                // 시간 문자열을 실제 시간으로 변환
                 val startTime = LocalTime.parse(start)
                 val endTime = LocalTime.parse(end)
-                periods.filter { startTime <= it.second && endTime >= it.first }
+                // 시간 범위에 맞는 기간을 필터링
+                periods.filter { it.first.isBefore(endTime) && it.second.isAfter(startTime) }
+                // 예: "10:00" - "12:00" 사이에 있는 모든 기간을 찾음
             }
         }
 
+        // 업데이트된 출석 상태를 저장할 변수, 초기값은 기존 출석 상태
         var updatedAttendance = attendance
 
-        matchingPeriods.forEach { period ->
+        // 매칭된 각 기간에 대해 출석 상태를 "GO_OUT"으로 업데이트
+        matchPeriods.forEach { period ->
             updatedAttendance = when (period) {
-                period6 -> updatedAttendance.copy(period6 = AttendanceStatus.GO_OUT)
-                period7 -> updatedAttendance.copy(period7 = AttendanceStatus.GO_OUT)
-                period8 -> updatedAttendance.copy(period8 = AttendanceStatus.GO_OUT)
-                period9 -> updatedAttendance.copy(period9 = AttendanceStatus.GO_OUT)
-                period10 -> updatedAttendance.copy(period10 = AttendanceStatus.GO_OUT)
+                periods[5] -> updatedAttendance.copy(period6 = AttendanceStatus.GO_OUT)
+                periods[6] -> updatedAttendance.copy(period7 = AttendanceStatus.GO_OUT)
+                periods[7] -> updatedAttendance.copy(period8 = AttendanceStatus.GO_OUT)
+                periods[8] -> updatedAttendance.copy(period9 = AttendanceStatus.GO_OUT)
+                periods[9] -> updatedAttendance.copy(period10 = AttendanceStatus.GO_OUT)
                 else -> updatedAttendance
             }
         }
-
         return updatedAttendance
     }
 
-    private fun getMatchingPeriods(startTime: LocalTime, endTime: LocalTime?): List<String> {
+
+    private fun getMatchPeriods(startTime: LocalTime, endTime: LocalTime?): List<String> {
         return periods
             .mapIndexed { index, period ->
-                if (startTime <= period.second && (endTime == null || endTime >= period.first)) {
+                if ((startTime < period.second) && (endTime == null || endTime > period.first)) {
                     periodNames[index]
                 } else {
                     null
