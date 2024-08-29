33da30693ed77d7262d8f61ced58c7b59c006aba
도경 <dokyungkim2926@gmail.com>
2024-08-29 19:02:02 +0900

    add :: 주석처리

--- src/main/kotlin/dsm/pick2024/global/annotation/ValidFormat.kt
+++ src/main/kotlin/dsm/pick2024/global/annotation/ValidFormat.kt
@@ -10,6 +10,6 @@ import kotlin.reflect.KClass
 @Retention(AnnotationRetention.RUNTIME)
 annotation class ValidFormat(
     val message: String = "형식이 올바르지 않습니다.",
-    val groups: Array<KClass<*>> = [],
-    val payload: Array<KClass<out Payload>> = []
+    val groups: Array<KClass<*>> = [], // 어떤 타입의 데이터든 포함할 수 있다
+    val payload: Array<KClass<out Payload>> = [] //Payload 타입을 상속받는 어떤 클래스든 수용할 수 있음을 의미
 )
