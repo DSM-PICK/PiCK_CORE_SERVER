3f04c7d845ed824041d18ad6fbbadd5d0879c8c9
도경 <dokyungkim2926@gmail.com>
2024-08-29 17:25:19 +0900

    add :: reqeust annotation

--- src/main/kotlin/dsm/pick2024/global/annotation/ValidFormat.kt
+++ src/main/kotlin/dsm/pick2024/global/annotation/ValidFormat.kt
@@ -1,6 +1,5 @@
 package dsm.pick2024.global.annotation
 
-import dsm.pick2024.domain.application.enums.ApplicationType
 import dsm.pick2024.global.annotation.common.FormatValidator
 import javax.validation.Constraint
 import javax.validation.Payload
@@ -11,7 +10,6 @@ import kotlin.reflect.KClass
 @Retention(AnnotationRetention.RUNTIME)
 annotation class ValidFormat(
     val message: String = "형식이 올바르지 않습니다.",
-    val type: ApplicationType,
     val groups: Array<KClass<*>> = [],
     val payload: Array<KClass<out Payload>> = []
 )
