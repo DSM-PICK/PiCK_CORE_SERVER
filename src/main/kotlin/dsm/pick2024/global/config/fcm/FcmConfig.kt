0abb57f477e4e66a2b05f3e973ade3e3165444f0
도경 <dokyungkim2926@gmail.com>
2024-09-03 19:25:42 +0900

    add :: config

---
+++ src/main/kotlin/dsm/pick2024/global/config/fcm/FcmConfig.kt
@@ -0,0 +1,32 @@
+package dsm.pick2024.global.config.fcm
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import java.io.IOException
+
+@Configuration
+class FcmConfig {
+
+    private val firebaseResource = ClassPathResource(System.getenv("FIREBASE_ACCESS_KEY"))
+
+    @Bean
+    @Throws(IOException::class)
+    fun firebaseApp(): FirebaseApp {
+        val options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(firebaseResource.inputStream))
+            .build()
+
+        return FirebaseApp.initializeApp(options)
+    }
+
+    @Bean
+    @Throws(IOException::class)
+    fun firebaseMessaging(): FirebaseMessaging {
+        return FirebaseMessaging.getInstance(firebaseApp())
+    }
+}
