940ea722fe4f72ea8b10f9b7aae3905fe7268f21
도경 <dokyungkim2926@gmail.com>
2024-09-03 23:07:49 +0900

    modify :: fcm config

--- src/main/kotlin/dsm/pick2024/global/config/fcm/FcmConfig.kt
+++ src/main/kotlin/dsm/pick2024/global/config/fcm/FcmConfig.kt
@@ -4,15 +4,18 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
 import java.io.IOException
 
 @Configuration
-class FcmConfig {
-
-    private val firebaseResource = ClassPathResource(System.getenv("FIREBASE_ACCESS_KEY"))
+class FcmConfig(
+    @Value("\${firebase.accessKey}")
+    private val resource: String
+) {
+    private val firebaseResource = ClassPathResource(resource)
 
     @Bean
     @Throws(IOException::class)
