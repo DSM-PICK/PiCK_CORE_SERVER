059a0a71598c766fcdee01713986c488f5811217
도경 <dokyungkim2926@gmail.com>
2024-09-04 10:40:49 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/global/config/fcm/FcmConfig.kt
+++ src/main/kotlin/dsm/pick2024/global/config/fcm/FcmConfig.kt
@@ -3,11 +3,9 @@ package dsm.pick2024.global.config.fcm
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
-import com.google.firebase.messaging.FirebaseMessaging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
 import java.io.File
 import java.io.IOException
 import java.net.URL
