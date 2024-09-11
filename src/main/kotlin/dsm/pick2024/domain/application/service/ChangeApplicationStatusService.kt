e59c4b3bdc7daaedae9e8e0d7ef60cbfbb92f2a0
도경 <dokyungkim2926@gmail.com>
2024-09-11 12:22:34 +0900

    refactor

--- src/main/kotlin/dsm/pick2024/domain/application/service/ChangeApplicationStatusService.kt
+++ src/main/kotlin/dsm/pick2024/domain/application/service/ChangeApplicationStatusService.kt
@@ -1,124 +1,27 @@
 package dsm.pick2024.domain.application.service
 
 import dsm.pick2024.domain.admin.port.`in`.AdminFacadeUseCase
-import dsm.pick2024.domain.application.domain.Application
 import dsm.pick2024.domain.application.enums.ApplicationKind
 import dsm.pick2024.domain.application.enums.Status
-import dsm.pick2024.domain.application.exception.ApplicationNotFoundException
+import dsm.pick2024.domain.application.facade.ApplicationFacade
 import dsm.pick2024.domain.application.port.`in`.ChangeApplicationStatusUseCase
-import dsm.pick2024.domain.application.port.out.DeleteApplicationPort
-import dsm.pick2024.domain.application.port.out.QueryApplicationPort
-import dsm.pick2024.domain.application.port.out.SaveApplicationPort
 import dsm.pick2024.domain.application.presentation.dto.request.ApplicationStatusRequest
-import dsm.pick2024.domain.applicationstory.domain.ApplicationStory
-import dsm.pick2024.domain.applicationstory.enums.Type
-import dsm.pick2024.domain.applicationstory.port.out.SaveAllApplicationStoryPort
-import dsm.pick2024.domain.attendance.domain.service.AttendanceService
-import dsm.pick2024.domain.attendance.port.out.QueryAttendancePort
-import dsm.pick2024.domain.attendance.port.out.SaveAttendancePort
-import dsm.pick2024.domain.event.Topic
-import dsm.pick2024.domain.event.application.SendMessageToApplicationEventPort
-import dsm.pick2024.domain.user.port.out.QueryUserPort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.UUID
 
 @Service
 class ChangeApplicationStatusService(
     private val adminFacadeUseCase: AdminFacadeUseCase,
-    private val queryApplicationPort: QueryApplicationPort,
-    private val saveApplicationPort: SaveApplicationPort,
-    private val applicationStorySaveAllPort: SaveAllApplicationStoryPort,
-    private val deleteApplicationPort: DeleteApplicationPort,
-    private val saveAttendancePort: SaveAttendancePort,
-    private val queryAttendancePort: QueryAttendancePort,
-    private val sendMessageToApplicationEventPort: SendMessageToApplicationEventPort,
-    private val queryUserPort: QueryUserPort,
-    private val attendanceService: AttendanceService
+    private val applicationFacade: ApplicationFacade
 ) : ChangeApplicationStatusUseCase {
 
     @Transactional
     override fun changeStatusApplication(request: ApplicationStatusRequest) {
         val admin = adminFacadeUseCase.currentAdmin()
-
         if (request.status == Status.OK) {
-            handleStatusOk(request.idList, admin.name)
+            applicationFacade.handleStatusOk(request.idList, admin.name, ApplicationKind.APPLICATION)
         } else {
-            handleStatusNo(request.idList)
+            applicationFacade.handleStatusNo(request.idList, ApplicationKind.APPLICATION)
         }
-
-    }
-
-    private fun handleStatusOk(ids: List<UUID>, adminName: String) {
-        val updateApplicationList = ids.map { id ->
-            val application = findApplicationById(id)
-            val user = queryUserPort.findByXquareId(application.userId)!!
-            sendMessageToApplicationEventPort.send(
-                user.deviceToken!!, Topic.APPLICATION,
-                Status.OK, ApplicationKind.APPLICATION,
-                application
-            )
-            updateApplication(application, adminName)
-        }
-
-        val applicationStory = updateApplicationList.map { application ->
-            createApplicationStory(application)
-        }
-
-        val attendance = updateApplicationList.map { application ->
-            val attendanceId = queryAttendancePort.findByUserId(application.userId)
-            attendanceService.updateAttendanceToApplication(
-                application.start, application.end!!,
-                application.applicationType, attendanceId!!
-            )
-        }.toMutableList()
-
-        saveApplicationPort.saveAll(updateApplicationList)
-        applicationStorySaveAllPort.saveAll(applicationStory)
-        saveAttendancePort.saveAll(attendance)
-    }
-
-    private fun handleStatusNo(ids: List<UUID>) {
-
-        ids.forEach { id ->
-            val application = findApplicationById(id)
-            val user = queryUserPort.findByXquareId(application.userId)!!
-            sendMessageToApplicationEventPort.send(
-                user.deviceToken!!, Topic.APPLICATION,
-                Status.NO, ApplicationKind.APPLICATION,
-                null
-            )
-            deleteApplicationPort.deleteByIdAndApplicationKind(application.id!!, ApplicationKind.APPLICATION)
-        }
-
-    }
-
-    private fun findApplicationById(id: UUID): Application {
-        return queryApplicationPort.findByIdAndApplicationKind(id, ApplicationKind.APPLICATION)
-            ?: throw ApplicationNotFoundException
-    }
-
-    private fun updateApplication(application: Application, adminName: String): Application {
-        return application.copy(
-            teacherName = adminName,
-            status = Status.OK
-        )
-    }
-
-    private fun createApplicationStory(application: Application): ApplicationStory {
-        val startAndEnd = attendanceService.translateApplication(
-            application.start,
-            application.end!!,
-            application.applicationType
-        )
-        return ApplicationStory(
-            reason = application.reason,
-            userName = application.userName,
-            start = startAndEnd.first(),
-            end = startAndEnd.last(),
-            date = application.date,
-            type = Type.APPLICATION,
-            userId = application.userId
-        )
     }
 }
--- src/main/kotlin/dsm/pick2024/domain/earlyreturn/service/ChangeEarlyReturnStatusService.kt
+++ src/main/kotlin/dsm/pick2024/domain/earlyreturn/service/ChangeEarlyReturnStatusService.kt
@@ -1,42 +1,18 @@
 package dsm.pick2024.domain.earlyreturn.service
 
 import dsm.pick2024.domain.admin.port.`in`.AdminFacadeUseCase
-import dsm.pick2024.domain.application.domain.Application
 import dsm.pick2024.domain.application.enums.ApplicationKind
-import dsm.pick2024.domain.application.enums.ApplicationType
 import dsm.pick2024.domain.application.enums.Status
-import dsm.pick2024.domain.application.exception.ApplicationNotFoundException
-import dsm.pick2024.domain.application.port.out.DeleteApplicationPort
-import dsm.pick2024.domain.application.port.out.QueryApplicationPort
-import dsm.pick2024.domain.application.port.out.SaveApplicationPort
-import dsm.pick2024.domain.applicationstory.domain.ApplicationStory
-import dsm.pick2024.domain.applicationstory.enums.Type
-import dsm.pick2024.domain.applicationstory.port.out.SaveAllApplicationStoryPort
-import dsm.pick2024.domain.attendance.domain.service.AttendanceService
-import dsm.pick2024.domain.attendance.port.out.QueryAttendancePort
-import dsm.pick2024.domain.attendance.port.out.SaveAttendancePort
+import dsm.pick2024.domain.application.facade.ApplicationFacade
 import dsm.pick2024.domain.earlyreturn.port.`in`.ChangeEarlyReturnStatusUseCase
 import dsm.pick2024.domain.earlyreturn.presentation.dto.request.StatusEarlyReturnRequest
-import dsm.pick2024.domain.event.Topic
-import dsm.pick2024.domain.event.application.SendMessageToApplicationEventPort
-import dsm.pick2024.domain.user.port.out.QueryUserPort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.UUID
 
 @Service
 class ChangeEarlyReturnStatusService(
     private val adminFacadeUseCase: AdminFacadeUseCase,
-    private val saveApplicationPort: SaveApplicationPort,
-    private val queryApplicationPort: QueryApplicationPort,
-    private val applicationStorySaveAllPort: SaveAllApplicationStoryPort,
-    private val deleteApplicationPort: DeleteApplicationPort,
-    private val saveAttendancePort: SaveAttendancePort,
-    private val queryAttendancePort: QueryAttendancePort,
-    private val queryUserPort: QueryUserPort,
-    private val sendMessageToApplicationEventPort: SendMessageToApplicationEventPort,
-    private val attendanceService: AttendanceService
-
+    private val applicationFacade: ApplicationFacade
 ) : ChangeEarlyReturnStatusUseCase {
 
     @Transactional
@@ -44,72 +20,9 @@ class ChangeEarlyReturnStatusService(
         val admin = adminFacadeUseCase.currentAdmin()
 
         if (request.status == Status.NO) {
-            handleStatusNo(request.idList)
+            applicationFacade.handleStatusNo(request.idList, ApplicationKind.EARLY_RETURN)
         } else {
-            handleStatusOk(request.idList, admin.name)
+            applicationFacade.handleStatusOk(request.idList, admin.name, ApplicationKind.EARLY_RETURN)
         }
-    }
-
-    private fun handleStatusOk(ids: List<UUID>, adminName: String) {
-        val updateEarlyReturnList = ids.map { id ->
-            val application = findApplicationById(id)
-            val user = queryUserPort.findByXquareId(application.userId)!!
-            sendMessageToApplicationEventPort.send(
-                user.deviceToken!!, Topic.APPLICATION,
-                Status.OK, ApplicationKind.EARLY_RETURN,
-                application
-            )
-            updateEarlyReturn(application, adminName)
-        }
-
-        val applicationStoryList = updateEarlyReturnList.map { earlyReturn ->
-            createApplicationStory(earlyReturn)
-        }
-
-        val attendanceList = updateEarlyReturnList.map { it ->
-            val attendanceId = queryAttendancePort.findByUserId(it.userId)
-            attendanceService.updateAttendanceToEarlyReturn(it.start, attendanceId!!)
-        }.toMutableList()
-
-        saveApplicationPort.saveAll(updateEarlyReturnList)
-        applicationStorySaveAllPort.saveAll(applicationStoryList)
-        saveAttendancePort.saveAll(attendanceList)
-    }
-
-    private fun handleStatusNo(ids: List<UUID>) {
-        ids.forEach { id ->
-            val application = findApplicationById(id)
-            val user = queryUserPort.findByXquareId(application.userId)!!
-            sendMessageToApplicationEventPort.send(
-                user.deviceToken!!, Topic.APPLICATION,
-                Status.NO, ApplicationKind.EARLY_RETURN,
-                null
-            )
-            deleteApplicationPort.deleteByIdAndApplicationKind(id, ApplicationKind.EARLY_RETURN)
-        }
-    }
-
-    private fun updateEarlyReturn(application: Application, adminName: String): Application {
-        return application.copy(
-            teacherName = adminName,
-            status = Status.OK
-        )
-    }
-
-    private fun findApplicationById(id: UUID): Application {
-        return queryApplicationPort.findByIdAndApplicationKind(id, ApplicationKind.EARLY_RETURN)
-            ?: throw ApplicationNotFoundException
-    }
-
-    private fun createApplicationStory(application: Application): ApplicationStory {
-        val start = attendanceService.translateApplication(application.start, null, ApplicationType.TIME)
-        return ApplicationStory(
-            reason = application.reason,
-            userName = application.userName,
-            start = start.first(),
-            date = application.date,
-            type = Type.EARLY_RETURN,
-            userId = application.userId
-        )
     }
 }
