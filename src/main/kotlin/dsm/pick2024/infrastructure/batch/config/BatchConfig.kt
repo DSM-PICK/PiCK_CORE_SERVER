ff31a25f935fed3f3588644ba201454eb4f003aa
도경 <dokyungkim2926@gmail.com>
2024-09-12 23:16:17 +0900

    add :: batchConfig

---
+++ src/main/kotlin/dsm/pick2024/infrastructure/batch/config/BatchConfig.kt
@@ -0,0 +1,47 @@
+package dsm.pick2024.infrastructure.batch.config
+
+import dsm.pick2024.domain.meal.domain.Meal
+import dsm.pick2024.infrastructure.batch.processor.NeisMealProcessor
+import dsm.pick2024.infrastructure.batch.reader.NeisMealReader
+import dsm.pick2024.infrastructure.batch.writer.MealItemWriter
+import dsm.pick2024.infrastructure.feign.client.dto.response.NeisMealRow
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.core.launch.support.RunIdIncrementer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableBatchProcessing
+class BatchConfig(
+    private val jobBuilderFactory: JobBuilderFactory,
+    private val stepBuilderFactory: StepBuilderFactory,
+    private val neisMealReader: NeisMealReader,
+    private val neisMealProcessor: NeisMealProcessor,
+    private val mealItemWriter: MealItemWriter
+) {
+
+    @Bean
+    fun neisMealJob(): Job {
+        return jobBuilderFactory.get("neisMealJob")
+            .incrementer(RunIdIncrementer())
+            .flow(neisMealStep())
+            .end()
+            .build()
+    }
+
+    @Bean
+    @StepScope
+    fun neisMealStep(): Step {
+        return stepBuilderFactory.get("neisMealStep")
+            .chunk<NeisMealRow, Meal>(10)
+            .reader(neisMealReader)
+            .processor(neisMealProcessor)
+            .writer(mealItemWriter)
+            .build()
+    }
+}
