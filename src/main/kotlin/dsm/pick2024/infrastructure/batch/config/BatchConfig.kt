f19fc141a7c820ea7a39c4dc1da897cd175c3f49
도경 <dokyungkim2926@gmail.com>
2024-09-13 10:05:48 +0900

    modify :: batchConfig

--- src/main/kotlin/dsm/pick2024/infrastructure/batch/config/BatchConfig.kt
+++ src/main/kotlin/dsm/pick2024/infrastructure/batch/config/BatchConfig.kt
@@ -1,9 +1,12 @@
 package dsm.pick2024.infrastructure.batch.config
 
 import dsm.pick2024.domain.meal.domain.Meal
+import dsm.pick2024.domain.schedule.domain.Schedule
 import dsm.pick2024.infrastructure.batch.processor.NeisMealProcessor
 import dsm.pick2024.infrastructure.batch.reader.NeisMealReader
+import dsm.pick2024.infrastructure.batch.reader.NeisScheduleReader
 import dsm.pick2024.infrastructure.batch.writer.MealItemWriter
+import dsm.pick2024.infrastructure.batch.writer.NeisScheduleItemWriter
 import dsm.pick2024.infrastructure.feign.client.dto.response.NeisMealRow
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
@@ -15,13 +18,34 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableBatchProcessing
 class BatchConfig(
     private val jobBuilderFactory: JobBuilderFactory,
     private val stepBuilderFactory: StepBuilderFactory,
     private val neisMealReader: NeisMealReader,
     private val neisMealProcessor: NeisMealProcessor,
-    private val mealItemWriter: MealItemWriter
+    private val mealItemWriter: MealItemWriter,
+    private val neisScheduleReader: NeisScheduleReader,
+    private val neisScheduleItemWriter: NeisScheduleItemWriter
 ) {
+
+    @Bean
+    fun neisScheduleJob(): Job {
+        return jobBuilderFactory.get("neisScheduleJob")
+            .incrementer(RunIdIncrementer())
+            .flow(neisMealStep())
+            .end()
+            .build()
+    }
+
+    @Bean
+    fun neisScheduleStep(): Step {
+        return stepBuilderFactory.get("neisScheduleStep")
+            .chunk<List<Schedule>, List<Schedule>>(100)
+            .reader(neisScheduleReader)
+            .writer(neisScheduleItemWriter)
+            .build()
+    }
 
     @Bean
     fun neisMealJob(): Job {
