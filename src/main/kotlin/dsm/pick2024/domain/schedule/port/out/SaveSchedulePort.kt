b2d67fca5d1187406a0e9814f4c2737ff1687b30
도경 <dokyungkim2926@gmail.com>
2024-09-13 10:05:30 +0900

    modify :: schedule save port

--- src/main/kotlin/dsm/pick2024/domain/schedule/port/out/SaveSchedulePort.kt
+++ src/main/kotlin/dsm/pick2024/domain/schedule/port/out/SaveSchedulePort.kt
@@ -3,7 +3,7 @@ package dsm.pick2024.domain.schedule.port.out
 import dsm.pick2024.domain.schedule.domain.Schedule
 
 interface SaveSchedulePort {
-    fun saveFeignSchedule(schedule: MutableList<Schedule>)
+    fun saveFeignSchedule(schedule: List<Schedule>)
 
     fun save(schedule: Schedule)
 }
