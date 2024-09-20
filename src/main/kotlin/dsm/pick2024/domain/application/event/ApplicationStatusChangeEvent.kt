255016d67ba7ec30780fc07f7fac1c8ed4690b3e
도경 <dokyungkim2926@gmail.com>
2024-09-21 01:21:13 +0900

    add :: event class

---
+++ src/main/kotlin/dsm/pick2024/domain/application/event/ApplicationStatusChangeEvent.kt
@@ -0,0 +1,9 @@
+package dsm.pick2024.domain.application.event
+
+import org.springframework.context.ApplicationEvent
+import java.util.*
+
+class ApplicationStatusChangeEvent (
+    source: Any,
+    val userIdList: List<UUID>
+) : ApplicationEvent(source)
