555e31921b2e9903475195e4d989c3c5f7370bca
도경 <dokyungkim2926@gmail.com>
2024-09-21 22:30:55 +0900

    modify :: MainWebSocketHandler

--- src/main/kotlin/dsm/pick2024/global/config/socket/MainWebSocketHandler.kt
+++ src/main/kotlin/dsm/pick2024/global/config/socket/MainWebSocketHandler.kt
@@ -2,6 +2,7 @@ package dsm.pick2024.global.config.socket
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import dsm.pick2024.domain.main.MainService
+import dsm.pick2024.global.security.jwt.JwtTokenProvider
 import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.TextMessage
@@ -10,22 +11,31 @@ import org.springframework.web.socket.handler.TextWebSocketHandler
 
 @Component
 class MainWebSocketHandler(
-    private val mainService: MainService
+    private val mainService: MainService,
+    private val jwtTokenProvider: JwtTokenProvider
 ) : TextWebSocketHandler(), ApplicationListener<WebSocketStatusUpdateEvent> {
 
     private val objectMapper = ObjectMapper()
-
-    fun sendStatusUpdate(session: WebSocketSession, status: Any?) {
-        val jsonResponse = objectMapper.writeValueAsString(status)
-        session.sendMessage(TextMessage(jsonResponse))
-    }
-
-    override fun onApplicationEvent(event: WebSocketStatusUpdateEvent) {
-        if (event.session.isOpen) sendStatusUpdate(event.session, event.status)
-    }
+    private val sessions = HashMap<String, WebSocketSession>()
 
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         val authorization = session.handshakeHeaders.getFirst("Authorization")
-        mainService.main(session, authorization.toString())
+        val token = authorization!!.removePrefix("Bearer ")
+        val claims = jwtTokenProvider.getClaimsToken(token)
+        val userId = claims.get("sub", String::class.java)
+        sessions[userId] = session
+        mainService.main(userId, session)
+    }
+
+    private fun sendStatusUpdate(userId: String, status: Any?) {
+        val session = sessions[userId]
+        if (session?.isOpen == true) {
+            val jsonResponse = objectMapper.writeValueAsString(status)
+            session.sendMessage(TextMessage(jsonResponse))
+        }
+    }
+
+    override fun onApplicationEvent(event: WebSocketStatusUpdateEvent) {
+        sendStatusUpdate(event.userId, event.status)
     }
 }
