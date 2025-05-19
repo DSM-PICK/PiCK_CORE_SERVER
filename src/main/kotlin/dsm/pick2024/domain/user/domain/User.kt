46a07c4557f46da7e591645d5e6d88480a497e0a
imhanseong <hans_hanseong@dsm.hs.kr>
2025-05-19 11:05:08 +0900

    feat :: user 도메인 수정

--- src/main/kotlin/dsm/pick2024/domain/user/domain/User.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/domain/User.kt
@@ -7,19 +7,32 @@ import java.util.UUID
 
 @Aggregate
 data class User(
-    val id: UUID,
+    val id: UUID? = null,
     val accountId: String,
-    val password: String,
     val name: String,
-    val grade: Int,
-    val classNum: Int,
-    val num: Int,
-    val birthDay: LocalDate,
-    val profile: String ? = null,
+    val grade: Int = 0,
+    val classNum: Int = 0,
+    val num: Int = 0,
+    val profile: String? = null,
     val role: Role,
-    val deviceToken: String ? = null
-) {
+    val deviceToken: String? = null
+)
+ {
     fun updateProfileFileName(fileName: String?): User {
         return this.copy(profile = fileName)
     }
+
+    companion object {
+        fun fromGoogle(accountId: String, name: String, profile: String?): User {
+            return User(
+                id = null,
+                accountId = accountId,
+                name = name,
+                profile = profile,
+                role = Role.STU,
+                deviceToken = null
+            )
+        }
+    }
+
 }
