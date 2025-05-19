ee0915c2c9c4028606c5bd86af6d3db8a1547c82
imhanseong <hans_hanseong@dsm.hs.kr>
2025-05-19 11:08:26 +0900

    feat :: GoogleLoginUseCase 추가

--- src/main/kotlin/dsm/pick2024/domain/user/port/in/GoogleLoginUseCase.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/port/in/GoogleLoginUseCase.kt
@@ -3,6 +3,7 @@ package dsm.pick2024.domain.user.port.`in`
 import dsm.pick2024.domain.user.presentation.dto.request.UserLoginRequest
 import dsm.pick2024.global.security.jwt.dto.TokenResponse
 
-interface LoginUseCase {
-    fun login(userLoginRequest: UserLoginRequest): TokenResponse
+interface GoogleLoginUseCase {
+    fun loginByGoogle(code: String): TokenResponse
+
 }
