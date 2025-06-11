6c5d542d0af07ef39feb9f88fc2a62afcda8b761
dohwi <dohwe0978@gmail.com>
2025-06-11 19:57:40 +0900

    feat :: 조기귀가 status 변경 시 알림 추가

--- src/main/kotlin/dsm/pick2024/domain/earlyreturn/service/ChangeEarlyReturnStatusService.kt
+++ src/main/kotlin/dsm/pick2024/domain/earlyreturn/service/ChangeEarlyReturnStatusService.kt
@@ -18,6 +18,8 @@ import dsm.pick2024.domain.attendance.port.out.QueryAttendancePort
 import dsm.pick2024.domain.attendance.port.out.SaveAttendancePort
 import dsm.pick2024.domain.earlyreturn.port.`in`.ChangeEarlyReturnStatusUseCase
 import dsm.pick2024.domain.earlyreturn.presentation.dto.request.StatusEarlyReturnRequest
+import dsm.pick2024.domain.fcm.port.`in`.FcmSendMessageUseCase
+import dsm.pick2024.domain.user.port.`in`.UserFacadeUseCase
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -33,12 +35,31 @@ class ChangeEarlyReturnStatusService(
     private val saveAttendancePort: SaveAttendancePort,
     private val queryAttendancePort: QueryAttendancePort,
     private val attendanceService: AttendanceService,
-    private val eventPublisher: ApplicationEventPublisher
+    private val eventPublisher: ApplicationEventPublisher,
+    private val fcmSendMessageUseCase: FcmSendMessageUseCase,
+    private val userFacadeUseCase: UserFacadeUseCase
 ) : ChangeEarlyReturnStatusUseCase {
 
     @Transactional
     override fun statusEarlyReturn(request: StatusEarlyReturnRequest) {
         val admin = adminFacadeUseCase.currentAdmin()
+
+        val deviceTokens = request.idList.mapNotNull {
+            userFacadeUseCase.getUserByXquareId(
+                findApplicationById(it).userId
+            ).deviceToken
+        }.filter { it.isNotBlank() }
+
+        when (request.status) {
+            Status.NO -> {
+                fcmSendMessageUseCase.execute(deviceTokens, "조귀귀가 신청 반려 안내", "${admin.name} 선생님이 조귀귀가 신청을 반려하셨습니다.")
+            }
+            Status.OK -> {
+                fcmSendMessageUseCase.execute(deviceTokens, "조귀귀가 신청 승인 안내", "${admin.name} 선생님이 조귀귀가 신청을 승인하셨습니다.")
+            }
+            else -> {
+            }
+        }
 
         if (request.status == Status.NO) {
             handleStatusNo(request.idList)
@@ -66,7 +87,7 @@ class ChangeEarlyReturnStatusService(
     }
 
     private fun handleStatusNo(idList: List<UUID>) {
-        eventPublisher.publishEvent(ChangeStatusRequest(this, idList))
+        eventPublisher.publishEvent(ChangeStatusRequest(this, idList.map { findApplicationById(it).userId }))
         idList.forEach { id ->
             deleteApplicationPort.deleteByIdAndApplicationKind(id, ApplicationKind.EARLY_RETURN)
         }
@@ -80,7 +101,7 @@ class ChangeEarlyReturnStatusService(
     }
 
     private fun findApplicationById(id: UUID): Application {
-        return queryApplicationPort.findByIdAndApplicationKind(id, ApplicationKind.EARLY_RETURN)
+        return queryApplicationPort.findById(id)
             ?: throw ApplicationNotFoundException
     }
 
