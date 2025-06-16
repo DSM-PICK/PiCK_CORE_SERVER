9a572dce820f21deb9342c2dcacec919e9d9361d
imhanseong <hans_hanseong@dsm.hs.kr>
2025-06-16 23:38:14 +0900

    feat :: UserSignUp 로직 추가

--- src/main/kotlin/dsm/pick2024/domain/user/presentation/UserController.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/presentation/UserController.kt
@@ -1,20 +1,24 @@
 package dsm.pick2024.domain.user.presentation
 
-import dsm.pick2024.domain.user.port.`in`.QueryUserAllUseCase
-import dsm.pick2024.domain.user.port.`in`.QueryUserDetailsInfoUseCase
-import dsm.pick2024.domain.user.port.`in`.QueryUserSimpleInfoUseCase
-import dsm.pick2024.domain.user.port.`in`.UploadUserProfileUseCase
-import dsm.pick2024.domain.user.port.`in`.UserTokenRefreshUseCase
+import dsm.pick2024.domain.user.port.`in`.*
+import dsm.pick2024.domain.user.presentation.dto.request.UserSignUpRequest
+import dsm.pick2024.global.security.jwt.JwtTokenProvider
+import dsm.pick2024.global.security.jwt.dto.TokenResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+import java.util.UUID
 
 @Tag(name = "user API")
 @RestController
@@ -24,7 +28,9 @@ class UserController(
     private val queryUserSimpleInfoUseCase: QueryUserSimpleInfoUseCase,
     private val queryUserDetailsInfoUseCase: QueryUserDetailsInfoUseCase,
     private val queryUserAllUseCase: QueryUserAllUseCase,
-    private val uploadUserProfileUseCase: UploadUserProfileUseCase
+    private val uploadUserProfileUseCase: UploadUserProfileUseCase,
+    private val createUserUseCase: CreateUserUseCase,
+    private val jwtTokenProvider: JwtTokenProvider
 ) {
 
     @Operation(summary = "유저 토큰 재발급 API")
@@ -48,4 +54,15 @@ class UserController(
     @Operation(summary = "유저 프로필 업로드 API")
     @PatchMapping("/profile")
     fun upload(@RequestParam("file")file: MultipartFile) = uploadUserProfileUseCase.uploadUserProfile(file)
+
+    @Operation(summary = "유저 회원가입 API")
+    @PostMapping("/signup")
+    fun signUp(@RequestBody request: UserSignUpRequest) : ResponseEntity<TokenResponse> {
+        val userId = createUserUseCase.createUser(request)
+
+        val tokenResponse = jwtTokenProvider.generateToken(userId.toString(), request.role.name)
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(tokenResponse)
+    }
 }
