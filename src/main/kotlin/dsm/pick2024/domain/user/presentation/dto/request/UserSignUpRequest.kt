4e7a8796ea1613cc926c45922d902af1b282a700
imhanseong <hans_hanseong@dsm.hs.kr>
2025-06-16 23:37:38 +0900

    feat :: UserSignUpRequest 생성

---
+++ src/main/kotlin/dsm/pick2024/domain/user/presentation/dto/request/UserSignUpRequest.kt
@@ -0,0 +1,14 @@
+package dsm.pick2024.domain.user.presentation.dto.request
+
+import dsm.pick2024.domain.user.entity.enums.Role
+
+data class UserSignUpRequest (
+    val accountId : String,
+    val name : String,
+    val grade : Int,
+    val classNum : Int,
+    val num : Int,
+    val role : Role,
+    val deviceToken : String
+){
+}
