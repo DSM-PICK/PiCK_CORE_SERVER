6f1f7b26d83212b14a64e5765c074f4f3e4a82ed
imhanseong <hans_hanseong@dsm.hs.kr>
2025-06-17 09:03:55 +0900

    feat :: service password 추가

--- src/main/kotlin/dsm/pick2024/domain/user/service/UserSignUpService.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/service/UserSignUpService.kt
@@ -4,24 +4,28 @@ import dsm.pick2024.domain.user.domain.User
 import dsm.pick2024.domain.user.port.`in`.CreateUserUseCase
 import dsm.pick2024.domain.user.port.out.UserSavePort
 import dsm.pick2024.domain.user.presentation.dto.request.UserSignUpRequest
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class UserSignUpService (
-    private val savePort: UserSavePort
+    private val savePort: UserSavePort,
+    private val passwordEncoder: PasswordEncoder
 ) : CreateUserUseCase{
 
     override fun createUser(request: UserSignUpRequest): UUID {
+        val encodedPassword = passwordEncoder.encode(request.password)
+
         val user = User(
             accountId = request.accountId,
+            password = encodedPassword,
             name = request.name,
             grade = request.grade,
             classNum = request.classNum,
             num = request.num,
             profile = null,
-            role = request.role,
-            deviceToken = request.deviceToken
+            role = request.role
         )
 
         return savePort.save(user).id!!
