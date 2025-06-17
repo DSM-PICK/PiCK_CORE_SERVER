d05decd85009929c3efd4b59595bde5fbb4fc669
imhanseong <hans_hanseong@dsm.hs.kr>
2025-06-17 09:03:02 +0900

    feat :: request password 추가

--- src/main/kotlin/dsm/pick2024/domain/user/presentation/dto/request/UserSignUpRequest.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/presentation/dto/request/UserSignUpRequest.kt
@@ -4,11 +4,11 @@ import dsm.pick2024.domain.user.entity.enums.Role
 
 data class UserSignUpRequest (
     val accountId : String,
+    val password : String,
     val name : String,
     val grade : Int,
     val classNum : Int,
     val num : Int,
-    val role : Role,
-    val deviceToken : String
+    val role : Role
 ){
 }
