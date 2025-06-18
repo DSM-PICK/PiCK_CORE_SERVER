8b56bb01e9d2b74616014e611191f982bcf211b8
dohwi <dohwe0978@gmail.com>
2025-06-18 17:23:59 +0900

    fix :: google oauth path 환경변수로 수정

--- src/main/kotlin/dsm/pick2024/infrastructure/googleoauth/service/GoogleOauthService.kt
+++ src/main/kotlin/dsm/pick2024/infrastructure/googleoauth/service/GoogleOauthService.kt
@@ -11,7 +11,8 @@ import java.net.URL
 @Service
 class GoogleOauthService(
     private val redisUtil: RedisUtil,
-    @Value("\${firebase.url}") private val url: URL
+    @Value("\${firebase.messaging.url.key}") private val key: URL,
+    @Value("\${firebase.messaging.url.credentials}") private val credentials: String
 ) : GoogleOauthServicePort {
     companion object {
         private const val REDIS_KEY = "fcm:access-token"
@@ -22,8 +23,8 @@ class GoogleOauthService(
 
         return try {
             val credentials = GoogleCredentials
-                .fromStream(url.openStream())
-                .createScoped("https://www.googleapis.com/auth/firebase.messaging")
+                .fromStream(key.openStream())
+                .createScoped(credentials)
 
             credentials.refreshIfExpired()
             val token = credentials.accessToken.tokenValue
