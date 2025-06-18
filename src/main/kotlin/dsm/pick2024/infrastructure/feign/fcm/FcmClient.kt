968a0de176962d8de4135388970d2b6ed32f7832
dohwi <dohwe0978@gmail.com>
2025-06-18 17:24:49 +0900

    fix :: lint

--- src/main/kotlin/dsm/pick2024/infrastructure/feign/fcm/FcmClient.kt
+++ src/main/kotlin/dsm/pick2024/infrastructure/feign/fcm/FcmClient.kt
@@ -1,14 +1,13 @@
 package dsm.pick2024.infrastructure.feign.fcm
 
 import dsm.pick2024.domain.fcm.domain.FcmMessage
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 
 @FeignClient(name = "fcm", url = "\${firebase.messaging.url.host}")
-interface FcmClient{
+interface FcmClient {
 
     @PostMapping("\${firebase.messaging.url.path}")
     fun sendMessage(
