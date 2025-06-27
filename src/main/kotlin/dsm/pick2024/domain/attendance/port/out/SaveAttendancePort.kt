eceee251638685dde6b78ca14b8e3b291c2ef9d2
도경 <dokyungkim2926@gmail.com>
2025-06-27 21:52:31 +0900

    refactor :: mutableList->List

--- src/main/kotlin/dsm/pick2024/domain/attendance/port/out/SaveAttendancePort.kt
+++ src/main/kotlin/dsm/pick2024/domain/attendance/port/out/SaveAttendancePort.kt
@@ -3,6 +3,6 @@ package dsm.pick2024.domain.attendance.port.out
 import dsm.pick2024.domain.attendance.domain.Attendance
 
 interface SaveAttendancePort {
-    fun saveAll(attendance: MutableList<Attendance>)
+    fun saveAll(attendance: List<Attendance>)
     fun save(attendance: Attendance)
 }
