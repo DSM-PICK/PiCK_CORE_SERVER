f14b10449ae970787a9ce25cfaa9ccdd6f4e7bb9
도경 <dokyungkim2926@gmail.com>
2025-06-27 22:00:04 +0900

    ktlint

--- src/main/kotlin/dsm/pick2024/domain/application/service/ChangeApplicationStatusService.kt
+++ src/main/kotlin/dsm/pick2024/domain/application/service/ChangeApplicationStatusService.kt
@@ -1,31 +1,15 @@
 package dsm.pick2024.domain.application.service
 
 import dsm.pick2024.domain.admin.port.`in`.AdminFacadeUseCase
-import dsm.pick2024.domain.application.domain.Application
-import dsm.pick2024.domain.application.enums.ApplicationKind
 import dsm.pick2024.domain.application.enums.Status
-import dsm.pick2024.domain.event.dto.ChangeStatusRequest
 import dsm.pick2024.domain.application.port.`in`.ApplicationFinderUseCase
 import dsm.pick2024.domain.application.port.`in`.ChangeApplicationStatusUseCase
-import dsm.pick2024.domain.application.port.out.DeleteApplicationPort
-import dsm.pick2024.domain.application.port.out.QueryApplicationPort
-import dsm.pick2024.domain.application.port.out.SaveApplicationPort
 import dsm.pick2024.domain.application.presentation.dto.request.ApplicationStatusRequest
 import dsm.pick2024.domain.application.service.processor.ApplicationApprovalProcessor
 import dsm.pick2024.domain.application.service.processor.ApplicationRejectionProcessor
-import dsm.pick2024.domain.applicationstory.domain.ApplicationStory
-import dsm.pick2024.domain.applicationstory.enums.Type
-import dsm.pick2024.domain.applicationstory.port.out.SaveAllApplicationStoryPort
-import dsm.pick2024.domain.attendance.domain.service.AttendanceService
-import dsm.pick2024.domain.attendance.port.out.QueryAttendancePort
-import dsm.pick2024.domain.attendance.port.out.SaveAttendancePort
-import dsm.pick2024.domain.event.enums.EventTopic
-import dsm.pick2024.domain.fcm.port.`in`.FcmSendMessageUseCase
 import dsm.pick2024.domain.user.port.`in`.UserFacadeUseCase
-import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.UUID
 
 @Service
 class ChangeApplicationStatusService(
