a23a045225ed54e23f37997e039e943a176b86a1
dohwi <dohwe0978@gmail.com>
2025-07-07 11:02:32 +0900

    fix :: 중복 Redis 클래스 삭제

--- src/main/kotlin/dsm/pick2024/infrastructure/util/port/RedisUtilPort.kt
+++
@@ -1,7 +0,0 @@
-package dsm.pick2024.infrastructure.util.port
-
-interface RedisUtilPort {
-    fun getData(key: String): String?
-    fun setDataExpire(key: String, value: String, duration: Long)
-    fun deleteData(key: String)
-}
