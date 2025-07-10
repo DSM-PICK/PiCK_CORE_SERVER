2f79bd097a2ba713efd846b9c2c00323fc0f1264
imhanseong <hans_hanseong@dsm.hs.kr>
2025-07-10 17:01:57 +0900

    fix :: 회원가입 검증 어노테이션 추가

--- src/main/kotlin/dsm/pick2024/domain/user/presentation/dto/request/UserSignUpRequest.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/presentation/dto/request/UserSignUpRequest.kt
@@ -1,11 +1,29 @@
 package dsm.pick2024.domain.user.presentation.dto.request
 
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
 data class UserSignUpRequest(
+    @field:NotBlank
     val accountId: String,
+    @field:NotBlank
+    @field:Size(min = 8, max = 30)
     val password: String,
+    @field:NotBlank
+    @field:Size(min = 1, max = 10)
     val name: String,
+    @field:Min(1)
+    @field:Max(3)
     val grade: Int,
+    @field:Min(1)
+    @field:Max(4)
     val classNum: Int,
+    @field:Min(1)
+    @field:Max(20)
     val num: Int,
+    @field:NotBlank
+    @field:Size(min = 6, max = 6)
     val code: String
 )
