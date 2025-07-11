41bc7f54e005f6d2ebd9a76be58db12a8d94c523
imhanseong <hans_hanseong@dsm.hs.kr>
2025-07-11 11:09:59 +0900

    fix :: password 정규식 수정

--- src/main/kotlin/dsm/pick2024/domain/user/presentation/dto/request/PasswordResetRequest.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/presentation/dto/request/PasswordResetRequest.kt
@@ -8,7 +8,7 @@ data class PasswordResetRequest(
     @field:NotBlank(message = "비밀번호는 필수입니다")
     @field:Size(min = 8, max = 30, message = "비밀번호 8-30자여야 합니다")
     @field:Pattern(
-        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()])[A-Za-z\\d!@#$%^&*()]{8,}$",
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()])[A-Za-z\\d!@#$%^&*()]{8,30}$",
         message = "비밀번호는 영문자, 숫자, 특수문자를 포함해야 합니다"
     )
     val password: String,
