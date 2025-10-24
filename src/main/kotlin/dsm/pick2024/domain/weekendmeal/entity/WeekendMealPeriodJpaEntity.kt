c2dd3db9d5af52925a84a5954e42e11123cd23fc
dohwi <dohwe0978@gmail.com>
2025-10-24 15:35:48 +0900

    feat :: unique key 추가

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/entity/WeekendMealPeriodJpaEntity.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/entity/WeekendMealPeriodJpaEntity.kt
@@ -4,12 +4,15 @@ import dsm.pick2024.global.base.BaseUUIDEntity
 import java.time.LocalDate
 import java.time.Month
 import java.util.UUID
+import javax.persistence.Column
 import javax.persistence.Entity
 
 @Entity(name = "tbl_weekend_meal_period")
 class WeekendMealPeriodJpaEntity(
     id: UUID?,
+    @Column(unique = true, nullable = false)
     val start: LocalDate,
+    @Column(unique = true, nullable = false)
     val end: LocalDate,
     val month: Month,
     val adminId: UUID
