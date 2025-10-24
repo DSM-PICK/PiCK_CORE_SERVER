f904d5b52295b63fac433c61c11a86996c32b47a
dohwi <dohwe0978@gmail.com>
2025-10-24 15:36:30 +0900

    feat :: queryWeekendMealPeriodByDate

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/port/out/QueryWeekendMealPeriodPort.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/port/out/QueryWeekendMealPeriodPort.kt
@@ -1,11 +1,12 @@
 package dsm.pick2024.domain.weekendmeal.port.out
 
 import dsm.pick2024.domain.weekendmeal.domain.WeekendMealPeriod
+import java.time.LocalDate
 import java.util.UUID
 
 interface QueryWeekendMealPeriodPort {
     fun queryWeekendMealById(id: UUID): WeekendMealPeriod?
     fun queryAllWeekendMeal(): List<WeekendMealPeriod>
-
     fun queryWeekendMealByAdminId(id: UUID): WeekendMealPeriod?
+    fun queryWeekendPeriodByDate(date: LocalDate): WeekendMealPeriod?
 }
