782f5782c7c9d8259d4ca3f4dd62729cd9b45628
dohwi <dohwe0978@gmail.com>
2025-10-28 19:12:43 +0900

    fix :: usecase 메서드 수정

--- src/main/kotlin/dsm/pick2024/domain/admin/port/in/AdminFacadeUseCase.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/port/in/AdminFacadeUseCase.kt
@@ -3,5 +3,5 @@ package dsm.pick2024.domain.admin.port.`in`
 import dsm.pick2024.domain.admin.domain.Admin
 
 interface AdminFacadeUseCase {
-    fun getAdminByAdminId(adminId: String): Admin?
+    fun currentAdmin(): Admin
 }
