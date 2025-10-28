af1f08a769c42a906fb72aaa5791dff61cf94316
dohwi <dohwe0978@gmail.com>
2025-10-28 17:19:20 +0900

    feat :: userfinder 추가

---
+++ src/main/kotlin/dsm/pick2024/domain/user/port/in/UserFinderUseCase.kt
@@ -0,0 +1,18 @@
+package dsm.pick2024.domain.user.port.`in`
+
+import dsm.pick2024.domain.user.domain.User
+import java.util.*
+
+interface UserFinderUseCase {
+    fun findByAccountIdOrThrow(accountId: String): User
+
+    fun findByStudentNumOrThrow(
+        grade: Int,
+        classNum: Int,
+        num: Int
+    ): User?
+
+    fun userAllOrThrow(): List<User>
+
+    fun findByUserIdOrThrow(userId: UUID): User
+}
