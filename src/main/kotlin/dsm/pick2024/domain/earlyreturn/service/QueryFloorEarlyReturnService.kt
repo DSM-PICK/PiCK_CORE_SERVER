a06fdf41f70462f0a34d6d2a5e7d89b44d98f18a
dohwi <dohwe0978@gmail.com>
2025-10-28 20:13:39 +0900

    refactor :: findByFloorAndApplicationKind로 변환

--- src/main/kotlin/dsm/pick2024/domain/earlyreturn/service/QueryFloorEarlyReturnService.kt
+++ src/main/kotlin/dsm/pick2024/domain/earlyreturn/service/QueryFloorEarlyReturnService.kt
@@ -2,7 +2,7 @@ package dsm.pick2024.domain.earlyreturn.service
 
 import dsm.pick2024.domain.application.enums.ApplicationKind
 import dsm.pick2024.domain.application.enums.Status
-import dsm.pick2024.domain.application.port.out.QueryApplicationPort
+import dsm.pick2024.domain.application.port.`in`.ApplicationFinderUseCase
 import dsm.pick2024.domain.earlyreturn.port.`in`.QueryFloorEarlyReturnUseCase
 import dsm.pick2024.domain.earlyreturn.presentation.dto.response.QueryEarlyReturnResponse
 import org.springframework.stereotype.Service
@@ -10,15 +10,15 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QueryFloorEarlyReturnService(
-    private val queryApplicationPort: QueryApplicationPort
+    private val applicationFinderUseCase: ApplicationFinderUseCase
 ) : QueryFloorEarlyReturnUseCase {
 
     @Transactional(readOnly = true)
     override fun queryFloorEarlyReturn(floor: Int) =
 
-        queryApplicationPort.findByFloorAndApplicationKind(floor, ApplicationKind.EARLY_RETURN)
+        applicationFinderUseCase.findByFloorAndApplicationKindOrThrow(floor, ApplicationKind.EARLY_RETURN)
             .filter { it.status == Status.QUIET }
-            .map { it ->
+            .map {
                 QueryEarlyReturnResponse(it)
             }
 }
