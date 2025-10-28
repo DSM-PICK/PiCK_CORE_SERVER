c7e5daa3fa45c5b73fdb03bdffd5a34140f203b7
dohwi <dohwe0978@gmail.com>
2025-10-28 18:57:46 +0900

    feat :: admin finder

--- src/main/kotlin/dsm/pick2024/domain/admin/finder/AdminFinder.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/finder/AdminFinder.kt
@@ -1,4 +1,24 @@
 package dsm.pick2024.domain.admin.finder
 
-class AdminFinder {
+import dsm.pick2024.domain.admin.domain.Admin
+import dsm.pick2024.domain.admin.exception.AdminNotFoundException
+import dsm.pick2024.domain.admin.port.`in`.AdminFinderUseCase
+import dsm.pick2024.domain.admin.port.out.QueryAdminPort
+import org.springframework.stereotype.Component
+
+@Component
+class AdminFinder(
+    private val queryAdminPort: QueryAdminPort
+) : AdminFinderUseCase {
+    override fun findByGradeAndClassNumOrThrow(grade: Int, classNum: Int) =
+        queryAdminPort.findByGradeAndClassNum(grade, classNum) ?: throw AdminNotFoundException
+
+    override fun findAllOrThrow(): List<Admin> =
+        queryAdminPort.findAll() ?: throw AdminNotFoundException
+
+    override fun findByAdminIdOrThrow(adminId: String) =
+        queryAdminPort.findByAdminId(adminId) ?: throw AdminNotFoundException
+
+    override fun findByAdminByNameOrThrow(name: String) =
+        queryAdminPort.findByAdminByName(name) ?: throw AdminNotFoundException
 }
--- src/main/kotlin/dsm/pick2024/domain/admin/port/in/AdminFinderUseCase.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/port/in/AdminFinderUseCase.kt
@@ -1,4 +1,13 @@
 package dsm.pick2024.domain.admin.port.`in`
 
+import dsm.pick2024.domain.admin.domain.Admin
+
 interface AdminFinderUseCase {
+    fun findByGradeAndClassNumOrThrow(grade: Int, classNum: Int): Admin
+
+    fun findAllOrThrow(): List<Admin>
+
+    fun findByAdminIdOrThrow(adminId: String): Admin
+
+    fun findByAdminByNameOrThrow(name: String): Admin
 }
