b64ea89d69183fa853d822abbff6a3a2e59ebef5
dohwi <dohwe0978@gmail.com>
2025-10-28 17:23:21 +0900

    style :: lint

--- src/main/kotlin/dsm/pick2024/domain/user/finder/UserFinder.kt
+++ src/main/kotlin/dsm/pick2024/domain/user/finder/UserFinder.kt
@@ -1,6 +1,5 @@
 package dsm.pick2024.domain.user.finder
 
-import dsm.pick2024.domain.user.domain.User
 import dsm.pick2024.domain.user.exception.UserNotFoundException
 import dsm.pick2024.domain.user.port.`in`.UserFinderUseCase
 import dsm.pick2024.domain.user.port.out.QueryUserPort
@@ -10,16 +9,16 @@ import java.util.*
 @Component
 class UserFinder(
     private val queryUserPort: QueryUserPort
-): UserFinderUseCase {
+) : UserFinderUseCase {
     override fun findByAccountIdOrThrow(accountId: String) =
         queryUserPort.findByAccountId(accountId) ?: throw UserNotFoundException
 
-    override fun findByStudentNumOrThrow(grade: Int, classNum: Int, num: Int)=
-        queryUserPort.findByStudentNum(grade,classNum,num) ?: throw UserNotFoundException
+    override fun findByStudentNumOrThrow(grade: Int, classNum: Int, num: Int) =
+        queryUserPort.findByStudentNum(grade, classNum, num) ?: throw UserNotFoundException
 
     override fun userAllOrThrow() =
         queryUserPort.userAll() ?: throw UserNotFoundException
 
-    override fun findByUserIdOrThrow(userId: UUID)=
+    override fun findByUserIdOrThrow(userId: UUID) =
         queryUserPort.findByUserId(userId) ?: throw UserNotFoundException
 }
