d03f95b032752b6ecc392b742636847a0f73c211
dohwi <dohwe0978@gmail.com>
2025-10-28 19:15:31 +0900

    refactor :: findByAdminByNameOrThrow로 변환

--- src/main/kotlin/dsm/pick2024/domain/admin/facade/AdminFacade.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/facade/AdminFacade.kt
@@ -2,17 +2,17 @@ package dsm.pick2024.domain.admin.facade
 
 import dsm.pick2024.domain.admin.domain.Admin
 import dsm.pick2024.domain.admin.port.`in`.AdminFacadeUseCase
-import dsm.pick2024.domain.admin.port.out.QueryAdminPort
+import dsm.pick2024.domain.admin.port.`in`.AdminFinderUseCase
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 
 @Component
 class AdminFacade(
-    private val queryAdminPort: QueryAdminPort
+    private val adminFinderUseCase: AdminFinderUseCase
 ) : AdminFacadeUseCase {
 
     override fun currentAdmin(): Admin {
         val name = SecurityContextHolder.getContext().authentication.name
-        return getAdminByAdminId(name)
+        return adminFinderUseCase.findByAdminByNameOrThrow(name)
     }
 }
