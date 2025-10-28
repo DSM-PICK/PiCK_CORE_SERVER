1cf7ac2b606b8d38b23f292d32ab449cce0a3228
dohwi <dohwe0978@gmail.com>
2025-10-28 18:59:20 +0900

    refactor :: findAllOrThrow로 변환

--- src/main/kotlin/dsm/pick2024/domain/admin/service/QueryAdminAllService.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/service/QueryAdminAllService.kt
@@ -1,13 +1,14 @@
 package dsm.pick2024.domain.admin.service
 
+import dsm.pick2024.domain.admin.port.`in`.AdminFinderUseCase
 import dsm.pick2024.domain.admin.port.`in`.QueryAdminAllUseCase
 import dsm.pick2024.domain.admin.port.out.QueryAdminPort
 import org.springframework.stereotype.Service
 
 @Service
 class QueryAdminAllService(
-    private val queryAdminPort: QueryAdminPort
+    private val adminFinderUseCase: AdminFinderUseCase
 ) : QueryAdminAllUseCase {
     override fun queryAdminAll(): List<String> =
-        queryAdminPort.findAll().map { it.name }
+        adminFinderUseCase.findAllOrThrow().map { it.name }
 }
