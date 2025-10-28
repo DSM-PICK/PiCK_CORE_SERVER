5553dad0fb179d856fa610e41e399fd36122a668
imhanseong <hans_hanseong@dsm.hs.kr>
2025-10-28 19:46:11 +0900

    refactor :: Admin - WeekendMealPeriod 매핑

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/mapper/WeekendMealPeriodMapper.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/mapper/WeekendMealPeriodMapper.kt
@@ -1,19 +1,23 @@
 package dsm.pick2024.domain.weekendmeal.mapper
 
+import dsm.pick2024.domain.admin.entity.AdminJpaEntity
+import dsm.pick2024.domain.admin.mapper.AdminMapper
 import dsm.pick2024.domain.weekendmeal.domain.WeekendMealPeriod
 import dsm.pick2024.domain.weekendmeal.entity.WeekendMealPeriodJpaEntity
 import org.springframework.stereotype.Component
 
 @Component
-class WeekendMealPeriodMapper {
-    fun toEntity(domain: WeekendMealPeriod) =
+class WeekendMealPeriodMapper(
+    private val adminMapper: AdminMapper
+) {
+    fun toEntity(domain: WeekendMealPeriod, admin: AdminJpaEntity) =
         domain.run {
             WeekendMealPeriodJpaEntity(
                 id = id,
                 start = start,
                 end = end,
                 month = month,
-                adminId = adminId
+                admin = admin
             )
         }
 
@@ -24,7 +28,7 @@ class WeekendMealPeriodMapper {
                 start = start,
                 end = end,
                 month = month,
-                adminId = adminId
+                adminId = admin.id!!
             )
         }
 }
