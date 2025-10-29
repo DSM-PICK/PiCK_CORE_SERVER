015649f43fbcaa4ffb0b8d0be27b8647ef215533
imhanseong <hans_hanseong@dsm.hs.kr>
2025-10-29 14:32:09 +0900

    fix :: 의존성 제거

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/mapper/WeekendMealPeriodMapper.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/mapper/WeekendMealPeriodMapper.kt
@@ -1,15 +1,12 @@
 package dsm.pick2024.domain.weekendmeal.mapper
 
 import dsm.pick2024.domain.admin.entity.AdminJpaEntity
-import dsm.pick2024.domain.admin.mapper.AdminMapper
 import dsm.pick2024.domain.weekendmeal.domain.WeekendMealPeriod
 import dsm.pick2024.domain.weekendmeal.entity.WeekendMealPeriodJpaEntity
 import org.springframework.stereotype.Component
 
 @Component
-class WeekendMealPeriodMapper(
-    private val adminMapper: AdminMapper
-) {
+class WeekendMealPeriodMapper {
     fun toEntity(domain: WeekendMealPeriod, admin: AdminJpaEntity) =
         domain.run {
             WeekendMealPeriodJpaEntity(
