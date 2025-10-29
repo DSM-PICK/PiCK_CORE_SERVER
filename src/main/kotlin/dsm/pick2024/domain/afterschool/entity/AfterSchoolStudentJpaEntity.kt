b4b7748c93bc71270d262dfc20a9a025e9ff6678
imhanseong <hans_hanseong@dsm.hs.kr>
2025-10-29 11:55:31 +0900

    refactor :: User - AfterSchoolStudent 매핑

--- src/main/kotlin/dsm/pick2024/domain/afterschool/entity/AfterSchoolStudentJpaEntity.kt
+++ src/main/kotlin/dsm/pick2024/domain/afterschool/entity/AfterSchoolStudentJpaEntity.kt
@@ -1,19 +1,23 @@
 package dsm.pick2024.domain.afterschool.entity
 
 import dsm.pick2024.domain.attendance.enums.AttendanceStatus
+import dsm.pick2024.domain.user.entity.UserJpaEntity
 import dsm.pick2024.global.base.BaseUUIDEntity
 import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
+import javax.persistence.JoinColumn
+import javax.persistence.OneToOne
 
 @Entity(name = "tbl_after_school")
 class AfterSchoolStudentJpaEntity(
     id: UUID?,
 
-    @Column(name = "user_id", nullable = false, columnDefinition = "BINARY(16)")
-    val userId: UUID,
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    val user: UserJpaEntity,
 
     @Column(name = "user_name", nullable = false, columnDefinition = "VARCHAR(10)")
     val userName: String,
