92c60963f8f44dcf9dda592345ca58c50843b211
dohwi <dohwe0978@gmail.com>
2025-10-29 20:01:15 +0900

    refactor :: findAllOrThrow로 변환

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/service/QueryAllWeekendMealStatus.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/service/QueryAllWeekendMealStatus.kt
@@ -1,22 +1,22 @@
 package dsm.pick2024.domain.weekendmeal.service
 
 import dsm.pick2024.domain.weekendmeal.port.`in`.QueryAllWeekendMealStatusUseCase
-import dsm.pick2024.domain.weekendmeal.port.out.QueryWeekendMealPort
+import dsm.pick2024.domain.weekendmeal.port.`in`.WeekendMealFinderUseCase
 import dsm.pick2024.domain.weekendmeal.presentation.dto.response.QueryWeekendMealResponse
 import org.springframework.stereotype.Service
 
 @Service
 class QueryAllWeekendMealStatus(
-    private val queryWeekendMealPort: QueryWeekendMealPort
+    private val weekendMealFinderUseCase: WeekendMealFinderUseCase
 ) : QueryAllWeekendMealStatusUseCase {
 
     override fun findAll(): List<QueryWeekendMealResponse> {
-        val weekendMeal = queryWeekendMealPort.findAll()
+        val weekendMeal = weekendMealFinderUseCase.findAllOrThrow()
 
         val responseList =
             weekendMeal.map { meal ->
                 QueryWeekendMealResponse(
-                    id = meal.id!!,
+                    id = meal.id,
                     userName = meal.userName,
                     status = meal.status,
                     grade = meal.grade,
