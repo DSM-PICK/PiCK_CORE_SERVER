a76c587f8451e7d56df593af33a4d147da2d30ec
imhanseong <hans_hanseong@dsm.hs.kr>
2025-10-29 11:51:31 +0900

    refactor :: User - Application 매핑

--- src/main/kotlin/dsm/pick2024/domain/application/persistence/repository/ApplicationRepository.kt
+++ src/main/kotlin/dsm/pick2024/domain/application/persistence/repository/ApplicationRepository.kt
@@ -1,46 +1,46 @@
 package dsm.pick2024.domain.application.persistence.repository
 
-import dsm.pick2024.domain.application.entity.ApplicationJapEntity
+import dsm.pick2024.domain.application.entity.ApplicationJpaEntity
 import dsm.pick2024.domain.application.enums.ApplicationKind
 import dsm.pick2024.domain.application.enums.Status
 import org.springframework.data.repository.Repository
 import java.util.UUID
 
-interface ApplicationRepository : Repository<ApplicationJapEntity, UUID> {
-    fun existsByUserId(userId: UUID): Boolean
+interface ApplicationRepository : Repository<ApplicationJpaEntity, UUID> {
+    fun existsByUser_Id(userId: UUID): Boolean
 
-    fun existsByUserIdAndApplicationKind(
+    fun existsByUser_IdAndApplicationKind(
         userId: UUID,
         applicationKind: ApplicationKind
     ): Boolean
 
-    fun existsByStatusAndUserIdAndApplicationKind(
+    fun existsByStatusAndUser_IdAndApplicationKind(
         status: Status,
         userId: UUID,
         applicationKind: ApplicationKind
     ): Boolean
 
-    fun saveAll(entity: Iterable<ApplicationJapEntity>)
+    fun saveAll(entity: Iterable<ApplicationJpaEntity>)
 
-    fun findById(id: UUID): ApplicationJapEntity?
+    fun findById(id: UUID): ApplicationJpaEntity?
 
-    fun findByUserId(userId: UUID): ApplicationJapEntity
+    fun findByUser_Id(userId: UUID): ApplicationJpaEntity?
 
     fun deleteByIdAndApplicationKind(id: UUID, applicationKind: ApplicationKind)
 
-    fun save(entity: ApplicationJapEntity)
+    fun save(entity: ApplicationJpaEntity)
 
-    fun findAllByApplicationKind(applicationKind: ApplicationKind): List<ApplicationJapEntity>
+    fun findAllByApplicationKind(applicationKind: ApplicationKind): List<ApplicationJpaEntity>
 
-    fun findAllByStatus(status: Status): List<ApplicationJapEntity>
+    fun findAllByStatus(status: Status): List<ApplicationJpaEntity>
 
     fun deleteAll()
 
     fun deleteAllByApplicationKind(applicationKind: ApplicationKind)
 
-    fun findByUserIdAndStatusAndApplicationKind(
+    fun findByUser_IdAndStatusAndApplicationKind(
         id: UUID,
         status: Status,
         applicationKind: ApplicationKind
-    ): ApplicationJapEntity
+    ): ApplicationJpaEntity?
 }
