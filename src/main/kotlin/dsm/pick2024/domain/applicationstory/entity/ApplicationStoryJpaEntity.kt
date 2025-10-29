02c9ad3b25f7a0049bcc8b51166e38f9e297ae9b
imhanseong <hans_hanseong@dsm.hs.kr>
2025-10-29 11:50:52 +0900

    refactor :: User - ApplicationStory 매핑

--- src/main/kotlin/dsm/pick2024/domain/applicationstory/entity/ApplicationStoryJpaEntity.kt
+++ src/main/kotlin/dsm/pick2024/domain/applicationstory/entity/ApplicationStoryJpaEntity.kt
@@ -1,6 +1,7 @@
 package dsm.pick2024.domain.applicationstory.entity
 
 import dsm.pick2024.domain.applicationstory.enums.Type
+import dsm.pick2024.domain.user.entity.UserJpaEntity
 import dsm.pick2024.global.base.BaseUUIDEntity
 import java.time.LocalDate
 import java.util.UUID
@@ -8,6 +9,8 @@ import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 
 @Entity(name = "tbl_application_story")
 class ApplicationStoryJpaEntity(
@@ -16,8 +19,9 @@ class ApplicationStoryJpaEntity(
     @Column(name = "reason", nullable = false, columnDefinition = "TEXT")
     val reason: String,
 
-    @Column(name = "user_id", nullable = false, columnDefinition = "BINARY(16)")
-    val userId: UUID,
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user: UserJpaEntity,
 
     @Column(name = "start", nullable = false)
     val start: String,
