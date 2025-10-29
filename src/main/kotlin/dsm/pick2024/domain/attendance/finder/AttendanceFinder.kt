8ae3f2868dafa0c95d8583aaa5691b022796b7ea
dohwi <dohwe0978@gmail.com>
2025-10-29 10:23:05 +0900

    feat :: attendance finder

---
+++ src/main/kotlin/dsm/pick2024/domain/attendance/finder/AttendanceFinder.kt
@@ -0,0 +1,31 @@
+package dsm.pick2024.domain.attendance.finder
+
+import dsm.pick2024.domain.admin.exception.AdminNotFoundException
+import dsm.pick2024.domain.attendance.domain.Attendance
+import dsm.pick2024.domain.attendance.port.`in`.AttendanceFinderUseCase
+import dsm.pick2024.domain.attendance.port.out.QueryAttendancePort
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class AttendanceFinder(
+    private val queryAttendancePort: QueryAttendancePort
+) : AttendanceFinderUseCase {
+    override fun findAllOrThrow(): List<Attendance> =
+        queryAttendancePort.findAll() ?: throw AdminNotFoundException
+
+    override fun findByGradeAndClassNumOrThrow(grade: Int, classNum: Int) =
+        queryAttendancePort.findByGradeAndClassNum(grade, classNum) ?: throw AdminNotFoundException
+
+    override fun findByStudentNumOrThrow(grade: Int, classNum: Int, num: Int) =
+        queryAttendancePort.findByStudentNum(grade, classNum, num) ?: throw AdminNotFoundException
+
+    override fun findByClubOrThrow(club: String) =
+        queryAttendancePort.findByClub(club) ?: throw AdminNotFoundException
+
+    override fun findByUserIdOrThrow(userId: UUID) =
+        queryAttendancePort.findByUserId(userId) ?: throw AdminNotFoundException
+
+    override fun findByFloorOrThrow(floor: Int) =
+        queryAttendancePort.findByFloor(floor) ?: throw AdminNotFoundException
+}
