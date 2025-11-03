27a2873ad828679582f5643d1c4b5778f870cc6e
imhanseong <hans_hanseong@dsm.hs.kr>
2025-11-03 22:25:45 +0900

    fix :: NoticeMapper 수정

--- src/main/kotlin/dsm/pick2024/domain/notice/mapper/NoticeMapper.kt
+++ src/main/kotlin/dsm/pick2024/domain/notice/mapper/NoticeMapper.kt
@@ -1,32 +1,39 @@
 package dsm.pick2024.domain.notice.mapper
 
-import dsm.pick2024.domain.admin.mapper.AdminMapper
+import dsm.pick2024.domain.admin.entity.AdminJpaEntity
 import dsm.pick2024.domain.notice.domain.Notice
 import dsm.pick2024.domain.notice.entity.NoticeJpaEntity
 import dsm.pick2024.global.base.GenericMapper
 import org.springframework.stereotype.Component
 
 @Component
-class NoticeMapper(private val adminMapper: AdminMapper) : GenericMapper<NoticeJpaEntity, Notice> {
-    override fun toEntity(domain: Notice) = domain.run {
-        NoticeJpaEntity(
-            id = id,
-            title = title,
-            content = content,
-            createAt = createAt,
-            admin = adminMapper.toEntity(admin),
-            teacherName = teacherName
-        )
-    }
+class NoticeMapper : GenericMapper<NoticeJpaEntity, Notice> {
 
-    override fun toDomain(entity: NoticeJpaEntity) = entity.run {
-        Notice(
-            id = id!!,
-            title = title,
-            content = content,
-            createAt = createAt,
-            admin = adminMapper.toDomain(admin),
-            teacherName = teacherName
-        )
+    fun toEntity(domain: Notice, admin: AdminJpaEntity): NoticeJpaEntity =
+        domain.run {
+            NoticeJpaEntity(
+                id = id,
+                title = title,
+                content = content,
+                createAt = createAt,
+                admin = admin,
+                teacherName = teacherName
+            )
+        }
+
+    override fun toDomain(entity: NoticeJpaEntity): Notice =
+        entity.run {
+            Notice(
+                id = id!!,
+                title = title,
+                content = content,
+                createAt = createAt,
+                adminId = admin.id!!,
+                teacherName = teacherName
+            )
+        }
+
+    override fun toEntity(domain: Notice): NoticeJpaEntity {
+        throw UnsupportedOperationException("AdminJpaEntity는 반드시 명시적으로 전달되어야 합니다.")
     }
 }
