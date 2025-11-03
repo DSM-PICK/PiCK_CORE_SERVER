6fedce5c8612ec00386acb2ab2cdfecfcb5520f4
imhanseong <hans_hanseong@dsm.hs.kr>
2025-11-03 10:56:46 +0900

    fix :: WeekendMeal 도메인 적용

--- src/main/kotlin/dsm/pick2024/domain/weekendmeal/domain/WeekendMealPeriod.kt
+++ src/main/kotlin/dsm/pick2024/domain/weekendmeal/domain/WeekendMealPeriod.kt
@@ -1,5 +1,6 @@
 package dsm.pick2024.domain.weekendmeal.domain
 
+import dsm.pick2024.domain.admin.domain.Admin
 import dsm.pick2024.global.annotation.Aggregate
 import java.time.LocalDate
 import java.time.Month
@@ -8,7 +9,7 @@ import java.util.UUID
 @Aggregate
 data class WeekendMealPeriod(
     val id: UUID = UUID(0, 0),
-    val adminId: UUID,
+    val admin: Admin,
     var start: LocalDate,
     var end: LocalDate,
     var month: Month
