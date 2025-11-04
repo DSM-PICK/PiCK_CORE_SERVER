2bf3f9d877420f3dbdde9e5e7d9c028c500a4bf2
dohwi <dohwe0978@gmail.com>
2025-11-04 21:52:55 +0900

    feat :: 에러코드 추가

---
+++ src/main/kotlin/dsm/pick2024/domain/admin/exception/RegisteredClassAndGrade.kt
@@ -0,0 +1,8 @@
+package dsm.pick2024.domain.admin.exception
+
+import dsm.pick2024.global.error.exception.ErrorCode
+import dsm.pick2024.global.error.exception.PickException
+
+object RegisteredClassAndGrade : PickException(
+    ErrorCode.REGISTERED_CLASS_AND_GRADE
+)
