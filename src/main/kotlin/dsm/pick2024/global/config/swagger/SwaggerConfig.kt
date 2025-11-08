3f315ba79ad0385290fab445e97e9f0874f566b1
root <1210thatman@gmail.com>
2025-11-08 14:28:20 +0900

    add :: 로그인 기능 설정

--- src/main/kotlin/dsm/pick2024/global/config/swagger/SwaggerConfig.kt
+++ src/main/kotlin/dsm/pick2024/global/config/swagger/SwaggerConfig.kt
@@ -1,7 +1,10 @@
 package dsm.pick2024.global.config.swagger
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,7 +14,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class SwaggerConfig : WebMvcConfigurer {
     @Bean
     fun openApi(): OpenAPI {
+        val apiKey = SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .`in`(SecurityScheme.In.HEADER)
+            .name("Authorization")
+            .scheme("bearer")
+            .bearerFormat("JWT")
+
+        val securityRequirement = SecurityRequirement()
+            .addList("Bearer Token")
+
         return OpenAPI()
+            .components(Components().addSecuritySchemes("Bearer Token", apiKey))
+            .addSecurityItem(securityRequirement)
             .addServersItem(Server().url("/dsm-pick"))
             .info(
                 Info().title("PiCK API")
