2c6ac0f21464edc826e6e3bf10878f63c0c18850
dohwi <dohwe0978@gmail.com>
2025-11-13 11:34:00 +0900

    feat :: deviceToken에 nullable 속성 추가

--- src/main/kotlin/dsm/pick2024/domain/admin/presentation/dto/request/AdminLoginRequest.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/presentation/dto/request/AdminLoginRequest.kt
@@ -3,5 +3,5 @@ package dsm.pick2024.domain.admin.presentation.dto.request
 data class AdminLoginRequest(
     val adminId: String,
     val password: String,
-    val deviceToken: String
+    val deviceToken: String?
 )
--- src/main/kotlin/dsm/pick2024/domain/admin/presentation/dto/request/AdminSignUpRequest.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/presentation/dto/request/AdminSignUpRequest.kt
@@ -19,7 +19,7 @@ data class AdminSignUpRequest(
     @field:NotBlank
     val code: String,
     @field:NotBlank
-    val deviceToken: String,
+    val deviceToken: String?,
     @field:NotBlank
     @field:Size(min = 6, max = 6)
     val secretKey: String
