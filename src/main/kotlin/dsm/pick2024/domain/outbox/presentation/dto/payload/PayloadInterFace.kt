3ca7ac876890cd9692333b1904c756d9857c36d0
dohwi <dohwe0978@gmail.com>
2025-11-19 21:29:44 +0900

    feat :: Gson 객체 companion object으로 변경

--- src/main/kotlin/dsm/pick2024/domain/outbox/presentation/dto/payload/PayloadInterFace.kt
+++ src/main/kotlin/dsm/pick2024/domain/outbox/presentation/dto/payload/PayloadInterFace.kt
@@ -3,7 +3,11 @@ package dsm.pick2024.domain.outbox.presentation.dto.payload
 import com.google.gson.Gson
 
 abstract class PayloadInterFace {
+    companion object {
+        val gson = Gson()
+    }
+
     fun toJson(): String {
-        return Gson().toJson(this)
+        return gson.toJson(this)
     }
 }
