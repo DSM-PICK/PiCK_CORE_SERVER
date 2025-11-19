2c097c5b65a3588bffb18b0edc3c33c04e976757
도경 <dokyungkim2926@gmail.com>
2025-11-19 23:00:33 +0900

    refactor ::  DiscordBugClient

--- src/main/kotlin/dsm/pick2024/infrastructure/feign/client/DiscordBugClient.kt
+++ src/main/kotlin/dsm/pick2024/infrastructure/feign/client/DiscordBugClient.kt
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
-@FeignClient(name = "discord-bug-message-client", url = "\${discord.webhook.message}")
+@FeignClient(name = "discord-bug-message-client", url = "\${discord.webhook.bugMessage}")
 interface DiscordBugClient {
     @PostMapping
     fun sendMessage(
