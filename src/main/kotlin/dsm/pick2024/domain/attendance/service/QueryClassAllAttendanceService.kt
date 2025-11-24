4acb4d1b3c9681faa52a4e03e9691e484f14fd00
dohwi <dohwe0978@gmail.com>
2025-11-24 23:18:45 +0900

    feat :: finder 삭제

--- src/main/kotlin/dsm/pick2024/domain/attendance/service/QueryClassAllAttendanceService.kt
+++ src/main/kotlin/dsm/pick2024/domain/attendance/service/QueryClassAllAttendanceService.kt
@@ -3,7 +3,7 @@ package dsm.pick2024.domain.attendance.service
 import dsm.pick2024.domain.attendance.port.`in`.QueryClassAllAttendanceUseCase
 import dsm.pick2024.domain.attendance.port.out.QueryAttendancePort
 import dsm.pick2024.domain.attendance.presentation.dto.response.QueryAllAttendanceResponse
-import dsm.pick2024.domain.classroom.port.`in`.ClassroomFinderUseCase
+import dsm.pick2024.domain.classroom.port.out.QueryClassroomPort
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,25 +11,23 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class QueryClassAllAttendanceService(
     private val queryAttendancePort: QueryAttendancePort,
-    private val classroomFinderUseCase: ClassroomFinderUseCase
+    private val queryClassroomPort: QueryClassroomPort
 ) : QueryClassAllAttendanceUseCase {
     @Transactional(readOnly = true)
     override fun queryClassAllAttendance(grade: Int, classNum: Int) =
         queryAttendancePort.findByGradeAndClassNum(grade, classNum)
-            .map { it ->
+            .map {
                 val userId = it.userId
                 val classroomName = try {
-                    val classroom = classroomFinderUseCase.findOKClassroomOrThrow(userId)
-                    classroom.classroomName ?: ""
+                    val classroom = queryClassroomPort.findOKClassroom(userId)
+                    classroom?.classroomName ?: ""
                 } catch (e: EmptyResultDataAccessException) {
                     ""
                 }
 
-                with(it) {
-                    QueryAllAttendanceResponse(
-                        it,
-                        classroomName
-                    )
-                }
+                QueryAllAttendanceResponse(
+                    it,
+                    classroomName
+                )
             }.distinctBy { it.id }.sortedWith(compareBy { it.num })
 }
