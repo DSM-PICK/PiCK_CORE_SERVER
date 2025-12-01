a97a804461093656d86b7dac805956794920e056
kangeunchan <kangeunchan080310@gmail.com>
2025-12-01 11:29:02 +0900

    refactor :: ktlint

--- src/main/kotlin/dsm/pick2024/global/config/restemplate/RestTemplateConfig.kt
+++ src/main/kotlin/dsm/pick2024/global/config/restemplate/RestTemplateConfig.kt
@@ -1,11 +1,9 @@
 package dsm.pick2024.global.config.restemplate
 
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
-import java.time.Duration
 
 @Configuration
 class RestTemplateConfig {
