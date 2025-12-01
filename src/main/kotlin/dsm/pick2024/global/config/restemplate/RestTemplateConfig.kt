bc2e064e34640eaf40c77cfcb7119841f1ad9c43
kangeunchan <kangeunchan080310@gmail.com>
2025-12-01 11:26:16 +0900

    feat :: add timeout

--- src/main/kotlin/dsm/pick2024/global/config/restemplate/RestTemplateConfig.kt
+++ src/main/kotlin/dsm/pick2024/global/config/restemplate/RestTemplateConfig.kt
@@ -1,11 +1,21 @@
 package dsm.pick2024.global.config.restemplate
 
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
+import java.time.Duration
 
 @Configuration
 class RestTemplateConfig {
     @Bean
-    fun restTemplate(): RestTemplate = RestTemplate()
+    fun restTemplate(): RestTemplate {
+        val factory = HttpComponentsClientHttpRequestFactory().apply {
+            setConnectTimeout(5000)
+            setReadTimeout(10000)
+        }
+
+        return RestTemplate(factory)
+    }
 }
