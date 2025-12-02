9c45b5e3e3e74f3b2c45b6fa37c8e12f1546c278
root <1210thatman@gmail.com>
2025-12-02 21:46:22 +0900

    feat :: 변경된 로직 적용 완료#531

--- src/main/kotlin/dsm/pick2024/domain/devicetoken/port/out/QueryUserDeviceTokenPort.kt
+++ src/main/kotlin/dsm/pick2024/domain/devicetoken/port/out/QueryUserDeviceTokenPort.kt
@@ -1,8 +1,8 @@
 package dsm.pick2024.domain.devicetoken.port.out
 
 import dsm.pick2024.domain.devicetoken.domain.UserDeviceToken
-import dsm.pick2024.domain.user.domain.User
+import java.util.UUID
 
 interface QueryUserDeviceTokenPort {
-    fun findAllByUserId(userId: User): List<UserDeviceToken>
+    fun findAllByUserId(userId: UUID): List<UserDeviceToken>
 }
