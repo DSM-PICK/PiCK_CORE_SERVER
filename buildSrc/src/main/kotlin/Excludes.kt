96d92b95e30298a11eb7a379298db619d42ad469
dohwi <dohwe0978@gmail.com>
2025-12-02 11:01:28 +0900

    feat :: log4j 의존성

---
+++ buildSrc/src/main/kotlin/Excludes.kt
@@ -0,0 +1,7 @@
+object Excludes {
+    const val SPRING_BOOT_LOGGING_GROUP = "org.springframework.boot"
+    const val SPRING_BOOT_LOGGING_MODULE = "spring-boot-starter-logging"
+
+    const val LOG4J_TO_SLF4J_GROUP = "org.apache.logging.log4j"
+    const val LOG4J_TO_SLF4J_MODULE = "log4j-to-slf4j"
+}
