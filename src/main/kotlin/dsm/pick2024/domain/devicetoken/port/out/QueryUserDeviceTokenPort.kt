f664e6ee14fcc033a0999f66c15ae6d5a649677a
root <1210thatman@gmail.com>
2025-12-03 10:24:33 +0900

    Revert "feat :: 변경된 로직 적용 완료#531"
    This reverts commit 9c45b5e3e3e74f3b2c45b6fa37c8e12f1546c278.

--- src/main/kotlin/dsm/pick2024/domain/devicetoken/port/out/QueryUserDeviceTokenPort.kt
+++ src/main/kotlin/dsm/pick2024/domain/devicetoken/port/out/QueryUserDeviceTokenPort.kt
@@ -1,8 +1,8 @@
 package dsm.pick2024.domain.devicetoken.port.out
 
 import dsm.pick2024.domain.devicetoken.domain.UserDeviceToken
-import java.util.UUID
+import dsm.pick2024.domain.user.domain.User
 
 interface QueryUserDeviceTokenPort {
-    fun findAllByUserId(userId: UUID): List<UserDeviceToken>
+    fun findAllByUserId(userId: User): List<UserDeviceToken>
 }
