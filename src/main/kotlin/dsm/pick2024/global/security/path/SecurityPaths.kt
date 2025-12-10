37f452e4c3422f52c13f99c5d06b73ef392e584f
dohwi <dohwe0978@gmail.com>
2025-12-10 14:21:28 +0900

    feat :: security path 추가

---
+++ src/main/kotlin/dsm/pick2024/global/security/path/SecurityPaths.kt
@@ -0,0 +1,25 @@
+package dsm.pick2024.global.security.path
+
+object SecurityPaths {
+    val PERMITALLPATHS = listOf(
+        "/admin/login",
+        "/admin/refresh",
+        "/user/login",
+        "/user/refresh",
+        "/main",
+        "/swagger-ui/**",
+        "/v3/api-docs/**",
+        "/swagger-resources/**",
+        "/webjars/**",
+        "/dsm-pick/swagger-ui/index.html",
+        "/dsm-pick/swagger-ui/index.html/**",
+        "/user/signup",
+        "/mail/send",
+        "/mail/check",
+        "/admin/signup",
+        "/PiCK_Logo.png",
+        "/user/password",
+        "/admin/key",
+        "/admin/password"
+    )
+}
