81ec9fa55eb314ef435e9247711f4a1dfffce052
kangeunchan <kangeunchan080310@gmail.com>
2025-12-14 17:55:46 +0900

    fix: Debezium configuration properties bean 등록 문제 해결

--- src/main/kotlin/dsm/pick2024/global/config/debezium/DebeziumRestTemplateConfig.kt
+++ src/main/kotlin/dsm/pick2024/global/config/debezium/DebeziumRestTemplateConfig.kt
@@ -1,12 +1,18 @@
 package dsm.pick2024.global.config.debezium
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
 
 @Configuration
+@EnableConfigurationProperties(
+    DebeziumProperties::class,
+    DebeziumClientProperties::class,
+    DebeziumRetryProperties::class
+)
 @ConditionalOnProperty(
     prefix = "debezium",
     name = ["enabled"],
