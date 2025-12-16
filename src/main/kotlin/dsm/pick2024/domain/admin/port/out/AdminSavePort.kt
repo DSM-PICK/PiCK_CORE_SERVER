2f16ec923f4c8445c91d739b37f2fec7e4c12a80
imhanseong <hans.hanseong@gmail.com>
2025-12-16 16:22:15 +0900

    fix :: 반환타입 수정

--- src/main/kotlin/dsm/pick2024/domain/admin/port/out/AdminSavePort.kt
+++ src/main/kotlin/dsm/pick2024/domain/admin/port/out/AdminSavePort.kt
@@ -3,5 +3,5 @@ package dsm.pick2024.domain.admin.port.out
 import dsm.pick2024.domain.admin.domain.Admin
 
 interface AdminSavePort {
-    fun save(admin: Admin)
+    fun save(admin: Admin): Admin
 }
