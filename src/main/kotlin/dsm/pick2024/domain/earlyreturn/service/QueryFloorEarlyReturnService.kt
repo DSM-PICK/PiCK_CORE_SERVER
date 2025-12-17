59d56402a1da4ccb7a2d0e0b2726907a0b8ea270
imhanseong <hans.hanseong@gmail.com>
2025-12-17 22:49:20 +0900

    fix :: 층별 조기귀가자까지 조회로 수정

--- src/main/kotlin/dsm/pick2024/domain/earlyreturn/service/QueryFloorEarlyReturnService.kt
+++ src/main/kotlin/dsm/pick2024/domain/earlyreturn/service/QueryFloorEarlyReturnService.kt
@@ -2,23 +2,43 @@ package dsm.pick2024.domain.earlyreturn.service
 
 import dsm.pick2024.domain.application.enums.ApplicationKind
 import dsm.pick2024.domain.application.enums.Status
+import dsm.pick2024.domain.application.port.out.QueryAllApplicationPort
 import dsm.pick2024.domain.application.port.out.QueryApplicationPort
-import dsm.pick2024.domain.earlyreturn.port.`in`.QueryFloorEarlyReturnUseCase
+import dsm.pick2024.domain.classroom.exception.FloorNotFoundException
+import dsm.pick2024.domain.earlyreturn.port.`in`.QueryFloorAndStatusEarlyReturnUseCase
 import dsm.pick2024.domain.earlyreturn.presentation.dto.response.QueryEarlyReturnResponse
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QueryFloorEarlyReturnService(
-    private val queryApplicationPort: QueryApplicationPort
-) : QueryFloorEarlyReturnUseCase {
+    private val queryApplicationPort: QueryApplicationPort,
+    private val queryAllApplicationPort: QueryAllApplicationPort
+) : QueryFloorAndStatusEarlyReturnUseCase {
 
     @Transactional(readOnly = true)
-    override fun queryFloorEarlyReturn(floor: Int) =
+    override fun queryFloorAndStatusEarlyReturn(
+        floor: Int,
+        status: Status
+    ): List<QueryEarlyReturnResponse> {
 
-        queryApplicationPort.findByFloorAndApplicationKind(floor, ApplicationKind.EARLY_RETURN)
-            .filter { it.status == Status.QUIET }
-            .map {
-                QueryEarlyReturnResponse(it)
+        val earlyReturns = when (floor) {
+            2, 3, 4 -> {
+                val filterClassRoomList =
+                queryApplicationPort
+                    .findByFloorAndApplicationKind(floor, ApplicationKind.EARLY_RETURN)
+                filterClassRoomList.filter { it.status == status }
             }
+            5 -> {
+                queryAllApplicationPort
+                    .findAllByStatusAndApplicationKind(status, ApplicationKind.EARLY_RETURN)
+            }
+            else -> throw FloorNotFoundException
+        }
+
+        return earlyReturns
+            .map { QueryEarlyReturnResponse(it) }
+            .distinctBy { it.id }
+            .sortedWith(compareBy({ it.grade }, { it.classNum }, { it.num }))
+    }
 }
