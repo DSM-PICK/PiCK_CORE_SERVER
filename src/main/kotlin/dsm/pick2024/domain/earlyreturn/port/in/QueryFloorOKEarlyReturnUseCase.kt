bec04189a5766ecab7aa31df3442429ddebb454e
imhanseong <hans.hanseong@gmail.com>
2025-12-17 15:20:53 +0900

    feat :: QueryFloorOKEarlyReturnUseCase 생성

---
+++ src/main/kotlin/dsm/pick2024/domain/earlyreturn/port/in/QueryFloorOKEarlyReturnUseCase.kt
@@ -0,0 +1,7 @@
+package dsm.pick2024.domain.earlyreturn.port.`in`
+
+import dsm.pick2024.domain.earlyreturn.presentation.dto.response.QueryEarlyReturnResponse
+
+interface QueryFloorOKEarlyReturnUseCase {
+    fun queryFloorOkEarlyReturn(floor: Int): List<QueryEarlyReturnResponse>
+}
