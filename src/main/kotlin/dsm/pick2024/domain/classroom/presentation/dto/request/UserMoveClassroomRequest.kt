738e4ebe7961d75801c5c06aba7dba1851e4bac0
imhanseong <hans.hanseong@gmail.com>
2025-12-23 20:29:29 +0900

    fix :: move(시작위치) 추가

--- src/main/kotlin/dsm/pick2024/domain/classroom/presentation/dto/request/UserMoveClassroomRequest.kt
+++ src/main/kotlin/dsm/pick2024/domain/classroom/presentation/dto/request/UserMoveClassroomRequest.kt
@@ -1,6 +1,7 @@
 package dsm.pick2024.domain.classroom.presentation.dto.request
 
 data class UserMoveClassroomRequest(
+    val move: String?,
     val floor: Int,
     val classroomName: String,
     val start: Int,
