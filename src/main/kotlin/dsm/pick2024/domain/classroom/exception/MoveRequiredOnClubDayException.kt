358bb006564c6c7d3ae80572095ad377dff9c770
imhanseong <hans.hanseong@gmail.com>
2025-12-26 19:39:52 +0900

    fix :: class로 수정

--- src/main/kotlin/dsm/pick2024/domain/classroom/exception/MoveRequiredOnClubDayException.kt
+++ src/main/kotlin/dsm/pick2024/domain/classroom/exception/MoveRequiredOnClubDayException.kt
@@ -3,6 +3,6 @@ package dsm.pick2024.domain.classroom.exception
 import dsm.pick2024.global.error.exception.ErrorCode
 import dsm.pick2024.global.error.exception.PickException
 
-object MoveRequiredOnClubDayException : PickException(
+class MoveRequiredOnClubDayException : PickException(
     ErrorCode.MOVE_REQUIRED_ON_CLUB_DAY
 )
