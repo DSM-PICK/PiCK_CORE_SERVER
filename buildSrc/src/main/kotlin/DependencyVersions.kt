9e9707fc86c1558730570cb900b5968cc828d7c0
dohwi <dohwe0978@gmail.com>
2025-12-26 16:02:47 +0900

    fix :: admin sdk 의존성 삭제

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -11,7 +11,6 @@ object DependencyVersions {
     const val APACHE_POI = "5.2.5"
     const val GSON = "2.8.7"
     const val AWS = "1.12.281"
-    const val FCM = "8.1.0"
     const val GOOGLE_OAUTH2 = "1.17.0"
     const val KAFKA = "2.9.13"
     const val APACHE_HTTP_CLIENT = "4.5.14"
