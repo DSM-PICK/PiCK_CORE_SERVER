6d93c6c7845a0abf0a35706ca96a12ea49b84985
dohwi <dohwe0978@gmail.com>
2025-12-29 19:53:43 +0900

    feat :: feign client error decoder에 로깅 추가

--- src/main/kotlin/dsm/pick2024/infrastructure/feign/FeignClientErrorDecoder.kt
+++ src/main/kotlin/dsm/pick2024/infrastructure/feign/FeignClientErrorDecoder.kt
@@ -7,9 +7,12 @@ import dsm.pick2024.infrastructure.feign.exception.FeignUnAuthorizedException
 import feign.FeignException
 import feign.Response
 import feign.codec.ErrorDecoder
+import org.slf4j.LoggerFactory
 
 class FeignClientErrorDecoder : ErrorDecoder {
+    private val log = LoggerFactory.getLogger(this::class.java)
     override fun decode(methodKey: String?, response: Response?): Exception {
+        response?.let { log.error("‼️error reason: ${response.reason()} error body: ${response.body()}") }
         if (response!!.status() >= 400) {
             when (response.status()) {
                 400 -> throw FeignBadRequestException
