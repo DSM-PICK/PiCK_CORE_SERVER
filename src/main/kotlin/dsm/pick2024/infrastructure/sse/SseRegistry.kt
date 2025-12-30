3f985a0d7c0e53090a6b6b95394c3573bbfb1c5b
dohwi <dohwe0978@gmail.com>
2025-12-30 19:19:52 +0900

    feat :: logging 추가

--- src/main/kotlin/dsm/pick2024/infrastructure/sse/SseRegistry.kt
+++ src/main/kotlin/dsm/pick2024/infrastructure/sse/SseRegistry.kt
@@ -1,6 +1,7 @@
 package dsm.pick2024.infrastructure.sse
 
 import dsm.pick2024.infrastructure.sse.port.out.SseRegistryPort
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import java.util.UUID
@@ -10,6 +11,7 @@ import java.util.concurrent.CopyOnWriteArrayList
 @Component
 class SseRegistry : SseRegistryPort {
     private val emitters: MutableMap<UUID, MutableList<SseEmitter>> = ConcurrentHashMap()
+    private val logger = LoggerFactory.getLogger(SseRegistry::class.java)
 
     override fun add(userId: UUID): SseEmitter {
         val emitter = SseEmitter(60 * 60 * 1000L * 2) // 2시간
@@ -35,6 +37,7 @@ class SseRegistry : SseRegistryPort {
             try {
                 emitter.send(SseEmitter.event().data(data ?: "."))
             } catch (e: Exception) {
+                logger.error("User Event Send Failed ${e.message}", e)
                 remove(userId, emitter)
             }
         }
