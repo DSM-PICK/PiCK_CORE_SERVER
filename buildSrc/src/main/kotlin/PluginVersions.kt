7c037c09e730cc605d80e9989c8fcb16d1f3dac4
imhanseong <hans.hanseong@gmail.com>
2026-01-06 23:27:52 +0900

    refactor :: 버전 수정

--- buildSrc/src/main/kotlin/PluginVersions.kt
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -1,9 +1,9 @@
 object PluginVersions {
     const val SPRING_BOOT_VERSION = "2.7.16"
     const val DEPENDENCY_MANAGER_VERSION = "1.0.11.RELEASE"
-    const val JVM_VERSION = "1.7.20"
-    const val SPRING_PLUGIN_VERSION = "1.7.20"
-    const val JPA_PLUGIN_VERSION = "1.7.20"
-    const val KAPT_VERSION = "1.7.20"
+    const val JVM_VERSION = "1.9.24"
+    const val SPRING_PLUGIN_VERSION = "1.9.24"
+    const val JPA_PLUGIN_VERSION = "1.9.24"
+    const val KAPT_VERSION = "1.9.24"
     const val KLINT_VERSION = "11.5.1"
 }
