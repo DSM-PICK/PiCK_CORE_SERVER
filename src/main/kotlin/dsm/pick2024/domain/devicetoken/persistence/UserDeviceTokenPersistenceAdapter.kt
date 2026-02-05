102b2db60d2cbf77df6f8d518c258ff5ea42cbf8
그분 <1210thatman@gmail.com>
2026-02-05 21:36:38 +0900

    modify: ChangeEarlyReturnStatusService에서 deviceToken 관련 로직 수정#629

--- src/main/kotlin/dsm/pick2024/domain/devicetoken/persistence/UserDeviceTokenPersistenceAdapter.kt
+++ src/main/kotlin/dsm/pick2024/domain/devicetoken/persistence/UserDeviceTokenPersistenceAdapter.kt
@@ -5,9 +5,11 @@ import dsm.pick2024.domain.devicetoken.entity.UserDeviceTokenJpaEntity
 import dsm.pick2024.domain.devicetoken.mapper.UserDeviceTokenMapper
 import dsm.pick2024.domain.devicetoken.persistence.repository.UserDeviceTokenRepository
 import dsm.pick2024.domain.devicetoken.port.out.UserDeviceTokenPort
+import org.springframework.stereotype.Component
 import java.util.*
 
-class UserDeviceTokenPersistencePort(
+@Component
+class UserDeviceTokenPersistenceAdapter(
     private val userDeviceTokenRepository: UserDeviceTokenRepository,
     private val userDeviceTokenMapper: UserDeviceTokenMapper
 ) : UserDeviceTokenPort {
