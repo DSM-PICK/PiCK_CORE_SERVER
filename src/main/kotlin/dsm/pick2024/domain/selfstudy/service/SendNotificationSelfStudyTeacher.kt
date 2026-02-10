b637b952f11b9488d9f9b7084af365fe64c0ea64
그분 <1210thatman@gmail.com>
2026-02-10 19:01:12 +0900

    add: Timezone을 KST로 설정하는 로직 추가

--- src/main/kotlin/dsm/pick2024/domain/selfstudy/service/SendNotificationSelfStudyTeacher.kt
+++ src/main/kotlin/dsm/pick2024/domain/selfstudy/service/SendNotificationSelfStudyTeacher.kt
@@ -4,6 +4,7 @@ import dsm.pick2024.domain.admin.port.`in`.AdminFinderUseCase
 import dsm.pick2024.domain.outbox.port.`in`.OutboxFacadeUseCase
 import dsm.pick2024.domain.selfstudy.port.`in`.SelfStudyFinderUseCase
 import dsm.pick2024.domain.selfstudy.port.`in`.SendNotificationSelfStudyTeacherUseCase
+import dsm.pick2024.global.common.TimeUtils
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
@@ -14,15 +15,15 @@ class SendNotificationSelfStudyTeacher(
     private val outboxFacadeUseCase: OutboxFacadeUseCase
 ) : SendNotificationSelfStudyTeacherUseCase {
     override fun execute() {
-        val selfStudies = selfStudyFinderUseCase.findByDaySelfStudyOrThrow(LocalDate.now()).map { it }
+        val selfStudies = selfStudyFinderUseCase.findByDaySelfStudyOrThrow(TimeUtils.nowLocalDate())
 
-        selfStudies.map {
-            val admin = adminFinderUseCase.findByAdminNameOrThrow(it.teacherName)
+        selfStudies.forEach { selfStudy ->
+            val admin = adminFinderUseCase.findByAdminNameOrThrow(selfStudy.teacherName)
             admin.deviceToken?.let { token ->
                 outboxFacadeUseCase.sendNotification(
                     deviceToken = token,
                     title = "[PiCK] 자습감독 알림",
-                    body = "${admin.name}선생님은 오늘 ${it.floor}층 자습감독 선생님입니다."
+                    body = "${admin.name}선생님은 오늘 ${selfStudy.floor}층 자습감독 선생님입니다."
                 )
             }
         }
